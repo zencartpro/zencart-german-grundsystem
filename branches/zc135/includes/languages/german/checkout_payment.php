70a54d4192a0be4e9db16307048af62cc5146dbe
wflohr <wflohr@d52c303f-c418-0410-a0d3-be95aab480a1>
2006-09-11 10:46:10 +0000

    wfh:: bug fix missing semicolon

--- branches/zc135/includes/languages/german/checkout_payment.php
+++ branches/zc135/includes/languages/german/checkout_payment.php
@@ -14,13 +14,13 @@ define('HEADING_TITLE', 'Schritt 2 von 3 , Zahlungsart');
 
 define('TABLE_HEADING_BILLING_ADDRESS', 'Rechnungsanschrift');
 define('TEXT_SELECTED_BILLING_DESTINATION', 'Ihre Rechnungsanschrift steht links. Sie k&ouml;nnen Ihre Rechnungsanschrift &auml;ndern indem Sie auf <em>Anschrift &auml;ndern</em> klicken.');
-define('TITLE_BILLING_ADDRESS', 'Rechnungsanschrift:')
+define('TITLE_BILLING_ADDRESS', 'Rechnungsanschrift:');
 
 define('TABLE_HEADING_PAYMENT_METHOD', 'Zahlungsart');
 define('TEXT_SELECT_PAYMENT_METHOD', 'Bitte w&auml;hlen Sie eine Zahlungsart f&uuml;r diese Bestellung.');
 define('TITLE_PLEASE_SELECT', 'Bitte w&auml;hlen Sie');
 define('TEXT_ENTER_PAYMENT_INFORMATION', 'Dies ist zur Zeit die einzig m&ouml;gliche Zahlungsart.');
-define('TABLE_HEADING_COMMENTS', 'Anmerkungen oder Hinweise')
+define('TABLE_HEADING_COMMENTS', 'Anmerkungen oder Hinweise');
 
 define('TITLE_NO_PAYMENT_OPTIONS_AVAILABLE', 'Zur Zeit nicht verf&uuml;gbar');
 define('TEXT_NO_PAYMENT_OPTIONS_AVAILABLE','<span class="alert">Entschuldigung, aber wir k&ouml;nnen Zahlungen aus Ihrer Region nicht annehmen .</span><br />Bitte setzen Sie sich mit uns in Verbindung, um Alternativen zu suchen. ');
