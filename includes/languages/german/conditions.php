3f26058f9029616324013482757bfc3d09bf91f5
hugo13 <hugo13@d52c303f-c418-0410-a0d3-be95aab480a1>
2007-02-15 13:09:10 +0000

    changed file-header/lic

--- includes/languages/german/conditions.php
+++ includes/languages/german/conditions.php
@@ -1,24 +1,13 @@
 <?php
-//
-// +----------------------------------------------------------------------+
-// |zen-cart Open Source E-commerce                                       |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2003 The zen-cart developers                           |
-// |                                                                      |   
-// | http://www.zen-cart.com/index.php                                    |   
-// |                                                                      |   
-// | Portions Copyright (c) 2003 osCommerce                               |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the GPL license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available through the world-wide-web at the following url:           |
-// | http://www.zen-cart.com/license/2_0.txt.                             |
-// | If you did not receive a copy of the zen-cart license and are unable |
-// | to obtain it through the world-wide-web, please send a note to       |
-// | license@zen-cart.com so we can mail you a copy immediately.          |
-// +----------------------------------------------------------------------+
-// $Id: conditions.php 1969 2005-09-13 06:57:21Z drbyte $
-//
+/**
+ * @package languageDefines
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @translator: cyaneo/hugo13/wflohr    http://www.zen-cart.at    2007-01-03
+ * @version $Id: checkout_success.php 5407 2006-12-27 01:35:37Z drbyte $
+ */
+
 
 define('NAVBAR_TITLE','Allgemeine Gesch&auml;fstbedingungen');
 define('HEADING_TITLE','Allgemeine Gesch&auml;fstbedingungen');
