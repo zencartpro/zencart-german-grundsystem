b358a64a0068b56667a7ccc20726b48822dbf61b
cyaneo <cyaneo@d52c303f-c418-0410-a0d3-be95aab480a1>
2007-02-22 20:15:58 +0000

    Copyright und Powered by in zen-cart.at geändert versions_id.txt zeigt nun auf zen-cart.at zc_install: Demodaten teilweise in deutsch übersetzt zc_install: Pfad für navigation.php von "english" in "$language" geändert zc_install: mysql_zencart.sql: € in &euro; konvertiert zc_install: neue Datei "ueber_zencart.html" hinzugefügt zc_install: Pfad von "about_zencart.html" nach "ueber_zencart.html" geändert docs: einige Dokumente in deutsch übersetzt

--- branches/zc137/includes/languages/german.php
+++ branches/zc137/includes/languages/german.php
@@ -14,7 +14,7 @@
 //define('CUSTOM_KEYWORDS', 'ecommerce, open source, shop, online shopping');
 // END: moved to meta_tags.php
 
-define('FOOTER_TEXT_BODY', 'Copyright &copy; ' . date('Y') . ' <a href="http://www.zen-cart.com" target="_blank">Zen Cart</a>. Powered by <a href="http://www.zen-cart.com" target="_blank">Zen Cart</a>');
+define('FOOTER_TEXT_BODY', 'Copyright &copy; ' . date('Y') . ' <a href="http://www.zen-cart.at" target="_blank">Zen Cart</a>. Powered by <a href="http://www.zen-cart.at" target="_blank">Zen Cart</a>');
 
 // look in your $PATH_LOCALE/locale directory for available locales..
 // on RedHat try 'en_US'
--- branches/zc137/zc_install/includes/modules/pages/database_setup/header_php.php
+++ branches/zc137/zc_install/includes/modules/pages/database_setup/header_php.php
@@ -123,7 +123,7 @@ $is_upgrade = $_GET['is_upgrade'];
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
 <title>Zen Cart&trade; Installer</title>
 <link rel="stylesheet" type="text/css" href="includes/templates/template_default/css/stylesheet.css">
 </head>
