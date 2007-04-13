42bb590daf3ca117d18db4c37313ea8591cf5511
hugo13 <hugo13@d52c303f-c418-0410-a0d3-be95aab480a1>
2007-04-13 06:45:32 +0000

    Preis inkl. UST && zzgl.Versandkosten in den templates css & viennablood hinzugefuegt

---
+++ branches/zc137/includes/languages/english/extra_definitions/rl.vat_info.php
@@ -0,0 +1,24 @@
+<?php
+//
+// +----------------------------------------------------------------------+
+// |zen-cart Open Source E-commerce                                       |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2003 The zen-cart developers                           |
+// |                                                                      |
+// | http://www.zen-cart.com/index.php                                    |
+// |                                                                      |
+// | Portions Copyright (c) 2003 osCommerce                               |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.0 of the GPL license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available through the world-wide-web at the following url:           |
+// | http://www.zen-cart.com/license/2_0.txt.                             |
+// | If you did not receive a copy of the zen-cart license and are unable |
+// | to obtain it through the world-wide-web, please send a note to       |
+// | license@zen-cart.com so we can mail you a copy immediately.          |
+// +----------------------------------------------------------------------+
+// $Id:$
+//
+define('VAT_SHOW', true);
+define('VAT_SHOW_TEXT','<span id="vatinfo"><br/>Price incl. VAT.<br/><a href="index.php?main_page=shippinginfo" target="_self">plus forwarding expenses</a><br/></span>');
+?>
