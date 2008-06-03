f5a80df64ecc7969b72d0cfa9d151e3435386c2f
hugo13 <hugo13@d52c303f-c418-0410-a0d3-be95aab480a1>
2008-06-03 10:59:36 +0000

    webchills total-module

---
+++ module/ot_webchills/includes/languages/english/modules/order_total/ot_webchills.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * ot_total order-total module
+ *
+ * @package orderTotal
+ * @desc map_shop generates google_map entries at http://shops.zen-cart.at
+ * @copyright Copyright 2008 rainer langheiter
+ * @author rainer langheiter <zen-cart@langheiter.com http://edv.langheiter.com/zencart/>
+ * @license http://www.gnu.org/copyleft/gpl.html     
+ * @version $Id$
+ */
+
+define('MODULE_ORDER_TOTAL_WEBCHILLS_TITLE', 'Porto-contribution and expenses');
+define('MODULE_ORDER_TOTAL_WEBCHILLS_DESCRIPTION', 'Hier können Sie einen Zuschlag für Bestellungen unter einem bestimmten Bestellwert festlegen.');
