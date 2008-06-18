1ef5d2361f476653d5843d30bfde90a6905c4c27
hugo13 <hugo13@d52c303f-c418-0410-a0d3-be95aab480a1>
2008-06-18 09:22:35 +0000

    anzeige aller configurationsgruppen auch wenn keine deutsche uebersetzung vorhanden ist

--- admin/includes/boxes/configuration_dhtml.php
+++ admin/includes/boxes/configuration_dhtml.php
@@ -4,7 +4,7 @@
  * @copyright Copyright 2003-2006 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: configuration_dhtml.php 3009 2006-02-11 15:41:10Z wilt $
+ * @version $Id$
  */
 if (!defined('IS_ADMIN_FLAG')) {
   die('Illegal Access');
@@ -23,15 +23,31 @@ if (!defined('IS_ADMIN_FLAG')) {
   if (1 == 1) {
     $cfg_groups = '';
     /* r.l. multilanguage  added language_id */
+    $rl_groups = array();   
     $configuration_groups = $db->Execute("select configuration_group_id as cgID,
                                                        configuration_group_title as cgTitle
                                                 from " . TABLE_CONFIGURATION_GROUP . "
-                                 where visible = '1' AND language_id = '" . $_SESSION['languages_id'] . "'
+                                 where visible = '1' AND language_id = '1'
                                  order by sort_order");
 
     while (!$configuration_groups->EOF) {
-      $cfg_groups .= '<li><a href="' . zen_href_link(FILENAME_CONFIGURATION, 'gID=' . $configuration_groups->fields['cgID'], 'NONSSL') . '">' . $configuration_groups->fields['cgTitle'] . '</a></li>' . "\n";
-      $configuration_groups->MoveNext();
+        $rl_groups[$configuration_groups->fields['cgID']] = $configuration_groups->fields;
+        $configuration_groups->MoveNext();
+    }
+    if($_SESSION['languages_id'] != 1){                                                                                  
+        $configuration_groups = $db->Execute("select configuration_group_id as cgID,
+                                                           configuration_group_title as cgTitle
+                                                    from " . TABLE_CONFIGURATION_GROUP . "
+                                     where visible = '1' AND language_id = '" . $_SESSION['languages_id'] . "'
+                                     order by sort_order");
+        while (!$configuration_groups->EOF) {
+            $rl_groups[$configuration_groups->fields['cgID']] = $configuration_groups->fields;
+            $configuration_groups->MoveNext();
+        }
+    }                             
+    foreach ($rl_groups as $key => $rl_group) {
+        $cfg_groups .= '<li><a href="' . zen_href_link(FILENAME_CONFIGURATION, 'gID=' . $rl_group['cgID'], 'NONSSL') . '">' . $rl_group['cgTitle'] . '</a></li>' . "\n";
+    
     }
   }
 echo $cfg_groups;
