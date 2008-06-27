afdf84ae668e04f0e5fd5d3edc116ac44e3f1acc
hugo13 <hugo13@d52c303f-c418-0410-a0d3-be95aab480a1>
2008-06-27 12:42:43 +0000

    added: Dynamic Price Updater multilanguage !!! functions_prices.php angepasst, damit Mwst + versandkosten erhalten bleiben

---
+++ zc138_addon/MIN/Dynamic Price Updater multilanguage/includes/languages/english/ADD_ON/product_info.php
@@ -0,0 +1,5 @@
+<?php
+// $Id$
+
+define('UPDATER_PREFIX_TEXT', 'Your price: ');
+define('UPDATER_SB_TITLE', 'Price Breakdown: ');
---
+++ zc138_addon/MIN/Dynamic Price Updater multilanguage/includes/languages/german/ADD_ON/product_info.php
@@ -0,0 +1,5 @@
+<?php
+// $Id$
+
+define('UPDATER_PREFIX_TEXT', 'Ihr Preis: ');
+define('UPDATER_SB_TITLE', 'Preisberechnung: ');
