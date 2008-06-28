248872f56797f890f1d665bab91900e4e4255bdd
hugo13 <hugo13@d52c303f-c418-0410-a0d3-be95aab480a1>
2008-06-28 11:44:09 +0000

    SLIMBOX: ordner struktur korrigieren

---
+++ zc138_addon/MIN/fual_slimbox.v0.1.5/includes/modules/pages/product_reviews_write/jscript_slimbox.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ * @copyright Copyright 2007 FUAL
+ * @copyright Portions Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id$
+ */
+
+require_once( DIR_WS_CLASSES . 'fual_slimbox.php' );
+$fualSO = new FualSlimboxOptions();
+$fualSO->jscript();
+?>
