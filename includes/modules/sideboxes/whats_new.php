2007aef45564030f772f844e1e8fd0c14b70fa62
hugo13 <hugo13@d52c303f-c418-0410-a0d3-be95aab480a1>
2008-09-24 04:33:39 +0000

    Problem: Multiple Languages showing in What's New sidebox
    http://www.zen-cart.com/forum/showthread.php?p=616420#post616420

--- includes/modules/sideboxes/whats_new.php
+++ includes/modules/sideboxes/whats_new.php
@@ -13,12 +13,12 @@
 // $display_limit = zen_get_products_new_timelimit();
   $display_limit = zen_get_new_date_range();
   $random_whats_new_sidebox_product_query = "select p.products_id, p.products_image, p.products_tax_class_id, p.products_price, pd.products_name,
-                                              p.master_categories_id
+                            p.master_categories_id
                            from (" . TABLE_PRODUCTS . " p
                            left join " . TABLE_PRODUCTS_DESCRIPTION . " pd on p.products_id = pd.products_id )
                            where p.products_id = pd.products_id
+                           and pd.language_id = '" . (int)$_SESSION['languages_id'] . "'
                            and p.products_status = 1 " . $display_limit;
-
 //  $random_whats_new_sidebox_product = zen_random_select($random_whats_new_sidebox_product_query);
   $random_whats_new_sidebox_product = $db->ExecuteRandomMulti($random_whats_new_sidebox_product_query, MAX_RANDOM_SELECT_NEW);
 
@@ -28,4 +28,3 @@
     $title_link = FILENAME_PRODUCTS_NEW;
     require($template->get_template_dir($column_box_default, DIR_WS_TEMPLATE, $current_page_base,'common') . '/' . $column_box_default);
   }
-?>
