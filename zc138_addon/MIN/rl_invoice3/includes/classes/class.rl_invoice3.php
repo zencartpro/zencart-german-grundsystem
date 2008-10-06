b50fa15785b0e5dd72e893d99a38930928c5b10a
hugo13 <hugo13@d52c303f-c418-0410-a0d3-be95aab480a1>
2008-10-06 18:23:29 +0000

    rl_invoice :: zusaetzliche pdf-dateien können angegeben werden

--- zc138_addon/MIN/rl_invoice3/includes/classes/class.rl_invoice3.php
+++ zc138_addon/MIN/rl_invoice3/includes/classes/class.rl_invoice3.php
@@ -136,7 +136,8 @@ class rl_invoice3 extends fpdi{
                                 ('RL_INVOICE3_FONTS', 43, 'Schriftarten für Rechnung und Artikel', 'Welche Schriftarten wollen Sie verwenden? <br />1. Für Rechnungstexte <br >2. Für Artikel & Summe<br /><br />Standart: dejavusanscondensed|freemono<br />(Pfad/und Schriftart für Rechnung|Pfad/und Schriftart für Artikel+Summe<br />'),
                                 ('RL_INVOICE3_WITHOUTINVOICE', 43, 'Rechnungsadresse nicht drucken', 'Rechnungsadresse nicht drucken'),
                                 ('RL_INVOICE3_PDF_PATH', 43, 'Speicherort und -Name der PDF-Datei', '1. Wo sollen PDF-Dateien gespeichert werden?<br />2. speichern ja|nein (1|0)<br />Standard: " . DIR_FS_CATALOG . DIR_WS_INCLUDES . "pdf/|1<br />'),
-                                ('RL_INVOICE3_SEND_PDF', 43, 'Rechnung bei Bestellung', 'soll Rechnung bei Bestellung gesendet werden')
+                                ('RL_INVOICE3_SEND_PDF', 43, 'Rechnung bei Bestellung', 'soll Rechnung bei Bestellung gesendet werden'),
+                                ('RL_INVOICE3_SEND_ATTACH', 43, 'Anhänge', 'welche PDFs sollen noch angehängt werden; bei mehreren dateien | (pipe) als trenner verwenden)')
                                 ;";
             }
             
@@ -157,7 +158,8 @@ class rl_invoice3 extends fpdi{
                         ('fonts for invoice|products', 'RL_INVOICE3_FONTS', 'dejavusanscondensed|freemono', 'fonts for <br />1. invoice in general <br >2. products & total-table<br />', $group, 120, NULL),
                         ('do not print invoice address', 'RL_INVOICE3_WITHOUTINVOICE', 'false', 'do not print invoice address', $group, 160, \"zen_cfg_select_option(array('true', 'false'), \"),
                         ('filename and path to store the pdf-file', 'RL_INVOICE3_PDF_PATH', '" . DIR_FS_CATALOG . DIR_WS_INCLUDES . "pdf/|1', '1. path to store the pdf-file<br />Default: ../pdf/|1<br />', $group, 130, NULL),
-                        ('RL_INVOICE3_SEND_PDF', 'RL_INVOICE3_SEND_PDF', '1', 'RL_INVOICE3_SEND_PDF', $group, 130, NULL)
+                        ('RL_INVOICE3_SEND_PDF', 'RL_INVOICE3_SEND_PDF', '1', 'RL_INVOICE3_SEND_PDF', $group, 130, NULL),
+                        ('additional attachements', 'RL_INVOICE3_SEND_ATTACH', 'agb.pdf|widerruf.pdf', 'RL_INVOICE3_SEND_PDF', $group, 130, NULL)
                         ;";
             foreach ($sqlA as $key => $value) {
                 $db->execute($value);
@@ -500,6 +502,20 @@ class rl_invoice3 extends fpdi{
         $pdfName = $this -> oID . '_' . md5($this->order->customer['email_address']) . ".pdf";
         return $this->pdfPath['path'] . $pdfName;
     }
+    
+    function getPDFAttachments(){
+        $attachArray = array();
+        $attachArray[] = array('file'=>$this->getPDFFileName(), 'mime_type'=>'pdf');      
+        $attachements = explode('|', RL_INVOICE3_SEND_ATTACH);
+        foreach ($attachements as $value) {
+            $file = DIR_FS_CATALOG . DIR_WS_INCLUDES . 'pdf/' . $value;
+            if(file_exists($file)){
+                $attachArray[] = array('file'=>$file, 'mime_type'=>'pdf');
+            }
+        }
+        return $attachArray;
+    }
+        
     function writePDF($onlyFile = false){
         $pdfName = $this -> oID . ".pdf";
         if(!$onlyFile){
