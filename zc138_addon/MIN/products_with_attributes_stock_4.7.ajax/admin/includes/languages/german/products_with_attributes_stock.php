6dd9581d8fc88728a9d519cf9e59c6f83368ae4f
hugo13 <hugo13@d52c303f-c418-0410-a0d3-be95aab480a1>
2008-11-14 16:02:14 +0000

    products_with_attributes:: Amendments from 4.7 to 4.7ajax -added ajax functionality -search filter: "%ma" filters all produchts with "ma" in products-name or in products-model; the search value will be stored in a cookie  a click at the "quantity in stock" value adds an input-field & you can enter the new value; the store button saves all changed values -added files in /ajax folder -changed products_with_attributes_stock.php -added products_with_attributes_stock_ajax.php

---
+++ zc138_addon/MIN/products_with_attributes_stock_4.7.ajax/admin/includes/languages/german/products_with_attributes_stock.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package admin
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id$
+ */
+ 
+define('PWA_DELETE_VARIANT_CONFIRMATION', 'Are you sure you want to delete this product variant?');
+
+define('PWA_PRODUCT_ID', 'Artikel ID');
+define('PWA_PRODUCT_NAME', 'Produktname');
+define('PWA_PRODUCT_MODEL', 'Artikelnummer');
+define('PWA_QUANTITY', 'Anzahl: ');
+define('PWA_QUANTITY_FOR_ALL_VARIANTS', 'Menge fuer alle Varianten');
+define('PWA_ADD_QUANTITY', 'Menge fuer ArtikelVariante hinzufuegen');
+define('PWA_SYNC_QUANTITY', 'Mengen abgleichen');
+
+define('PWA_EDIT_QUANTITY', 'Mengen bearbeiten');
+define('PWA_DELETE_VARIANT', 'Variante loeschen');
+
+define('PWA_STOCK_ID', 'Mengen ID');
+define('PWA_VARIANT', 'Variante');
+define('PWA_QUANTITY_IN_STOCK', 'Menge auf Lager');
+define('PWA_EDIT', 'bearbeiten');
+define('PWA_DELETE', 'loeschen');
+define('PWA_SUBMIT','Abschicken');
+
+?>
---
+++ zc138_addon/MIN/products_with_attributes_stock_4.7.ajax/stock_attribute.sql
@@ -0,0 +1,23 @@
+CREATE TABLE products_with_attributes_stock (
+        stock_id INT NOT NULL AUTO_INCREMENT ,
+        products_id INT NOT NULL ,
+        stock_attributes VARCHAR( 255 ) NOT NULL ,
+        quantity FLOAT NOT NULL ,
+        PRIMARY KEY ( `stock_id` )
+        );
+
+INSERT INTO configuration (configuration_title, configuration_key, configuration_value, 
+       configuration_description, configuration_group_id, sort_order, 
+       last_modified, date_added, use_function, set_function) 
+       VALUES ('Show available stock level in cart when less than order', 'STOCK_SHOW_LOW_IN_CART', 'false', 
+               'When customer places more items in cart than are available, show the available amount on the shopping cart page:',
+               '9',
+               '6',
+               NULL,
+               now(),
+               NULL,
+               "zen_cfg_select_option(array('true', 'false'),"
+       );
+       
+       
+       
