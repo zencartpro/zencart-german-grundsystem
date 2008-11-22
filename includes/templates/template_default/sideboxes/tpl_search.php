28e06ae7e1282cf0b6beadc13afa4ef83aa17af4
maleborg <maleborg@d52c303f-c418-0410-a0d3-be95aab480a1>
2008-11-22 09:08:37 +0000

    BUGFIX CSS Buttons wurden immer angezeigt für die beiden Search Sideboxen.
    Siehe http://www.zen-cart.at/zcvb/forum/showthread.php?p=30752#post30752

--- includes/templates/template_default/sideboxes/tpl_search.php
+++ includes/templates/template_default/sideboxes/tpl_search.php
@@ -15,11 +15,11 @@
   $content .= zen_draw_hidden_field('search_in_description', '1') . zen_hide_session_id();
 
   if (strtolower(IMAGE_USE_CSS_BUTTONS) == 'yes') {
-    $content .= zen_draw_input_field('keyword', '', 'size="18" maxlength="100" style="width: ' . ($column_width-30) . 'px"') . '<br />' . zen_image_submit (BUTTON_IMAGE_SEARCH,HEADER_SEARCH_BUTTON);
-    $content .= '<br /><a href="' . zen_href_link(FILENAME_ADVANCED_SEARCH) . '">' . BOX_SEARCH_ADVANCED_SEARCH . '</a>';
-  } else {
     $content .= zen_draw_input_field('keyword', '', 'size="18" maxlength="100" style="width: ' . ($column_width-30) . 'px" value="' . HEADER_SEARCH_DEFAULT_TEXT . '" onfocus="if (this.value == \'' . HEADER_SEARCH_DEFAULT_TEXT . '\') this.value = \'\';" onblur="if (this.value == \'\') this.value = \'' . HEADER_SEARCH_DEFAULT_TEXT . '\';"') . '<br /><input type="submit" value="' . HEADER_SEARCH_BUTTON . '" style="width: 50px" />';
     $content .= '<br /><a href="' . zen_href_link(FILENAME_ADVANCED_SEARCH) . '">' . BOX_SEARCH_ADVANCED_SEARCH . '</a>';
+  } else {
+	$content .= zen_draw_input_field('keyword', '', 'size="18" maxlength="100" style="width: ' . ($column_width-30) . 'px" value="' . HEADER_SEARCH_DEFAULT_TEXT . '" onfocus="if (this.value == \'' . HEADER_SEARCH_DEFAULT_TEXT . '\') this.value = \'\';" onblur="if (this.value == \'\') this.value = \'' . HEADER_SEARCH_DEFAULT_TEXT . '\';"') . '<br />';
+	$content .= zen_image_submit (BUTTON_IMAGE_SEARCH,HEADER_SEARCH_BUTTON);
   }
 
   $content .= "</form>";
--- includes/templates/template_default/sideboxes/tpl_search_header.php
+++ includes/templates/template_default/sideboxes/tpl_search_header.php
@@ -13,7 +13,7 @@
   $content .= zen_draw_hidden_field('main_page',FILENAME_ADVANCED_SEARCH_RESULT);
   $content .= zen_draw_hidden_field('search_in_description', '1') . zen_hide_session_id();
 
-  if (strtolower(IMAGE_USE_CSS_BUTTONS) == 'yes') {
+  if (strtolower(IMAGE_USE_CSS_BUTTONS) == 'no') {
     $content .= zen_draw_input_field('keyword', '', 'size="6" maxlength="30" style="width: 100px" value="' . HEADER_SEARCH_DEFAULT_TEXT . '" onfocus="if (this.value == \'' . HEADER_SEARCH_DEFAULT_TEXT . '\') this.value = \'\';" onblur="if (this.value == \'\') this.value = \'' . HEADER_SEARCH_DEFAULT_TEXT . '\';"') . '&nbsp;' . zen_image_submit (BUTTON_IMAGE_SEARCH,HEADER_SEARCH_BUTTON);
   } else {
     $content .= zen_draw_input_field('keyword', '', 'size="6" maxlength="30" style="width: 100px" value="' . HEADER_SEARCH_DEFAULT_TEXT . '" onfocus="if (this.value == \'' . HEADER_SEARCH_DEFAULT_TEXT . '\') this.value = \'\';" onblur="if (this.value == \'\') this.value = \'' . HEADER_SEARCH_DEFAULT_TEXT . '\';"') . '&nbsp;<input type="submit" value="' . HEADER_SEARCH_BUTTON . '" style="width: 45px" />';
