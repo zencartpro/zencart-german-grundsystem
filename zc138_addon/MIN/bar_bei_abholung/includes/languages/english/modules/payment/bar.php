ef1922ba08d25948512861323227825902e1811a
webchills <webchills@d52c303f-c418-0410-a0d3-be95aab480a1>
2008-12-13 10:53:32 +0000

    bar_bei_abholung: Neue Version 1.4 mit Ergaenzung von gidex. Wird nun bei versandkostenfreien Artikeln nicht mehr angeboten ausser es wurde vorher Selbstabholung gewaehlt.

---
+++ zc138_addon/MIN/bar_bei_abholung/includes/languages/english/modules/payment/bar.php
@@ -0,0 +1,26 @@
+<?php
+//
+// +----------------------------------------------------------------------+
+// |zen-cart Open Source E-commerce                                       |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2003 The zen-cart developers                           |
+// |                                                                      |
+// | http://www.zen-cart.com/index.php                                    |
+// |                                                                      |
+// | Portions Copyright (c) 2003 osCommerce                               |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.0 of the GPL license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available through the world-wide-web at the following url:           |
+// | http://www.zen-cart.com/license/2_0.txt.                             |
+// | If you did not receive a copy of the zen-cart license and are unable |
+// | to obtain it through the world-wide-web, please send a note to       |
+// | license@zen-cart.com so we can mail you a copy immediately.          |
+// +----------------------------------------------------------------------+
+// $Id: bar.php,v 1.2 2005/05/21 15:21:35 cyaneo Exp $
+//
+
+define('MODULE_PAYMENT_BAR_TEXT_TITLE', 'Cash by fetch');
+define('MODULE_PAYMENT_BAR_TEXT_DESCRIPTION', 'For use on Cash by fetch');
+define('MODULE_PAYMENT_BAR_TEXT_EMAIL_FOOTER', 'For use on Cash by fetch');
+?>
---
+++ zc138_addon/MIN/bar_bei_abholung/includes/languages/german/modules/payment/bar.php
@@ -0,0 +1,31 @@
+<?php
+//
+// +----------------------------------------------------------------------+
+// |zen-cart Open Source E-commerce                                       |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2003 The zen-cart developers                           |
+// |                                                                      |
+// | http://www.zen-cart.com/index.php                                    |
+// |                                                                      |
+// | Portions Copyright (c) 2003 osCommerce                               |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.0 of the GPL license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available through the world-wide-web at the following url:           |
+// | http://www.zen-cart.com/license/2_0.txt.                             |
+// | If you did not receive a copy of the zen-cart license and are unable |
+// | to obtain it through the world-wide-web, please send a note to       |
+// | license@zen-cart.com so we can mail you a copy immediately.          |
+// +----------------------------------------------------------------------+
+// | Translator:           cyaneo                                         |
+// | Date of Translation:  21.03.05                                       |
+// | Homepage:             www.zen-cart.at                                |
+// +----------------------------------------------------------------------+
+// $Id: bar.php,v 1.2 2005/05/21 15:21:35 cyaneo Exp $
+// $Id: bar.php,v 1.3 2008/07/24 10:11:17 webchills $
+//
+
+define('MODULE_PAYMENT_BAR_TEXT_TITLE', 'Bar bei Abholung');
+define('MODULE_PAYMENT_BAR_TEXT_DESCRIPTION', 'Sie zahlen bar bei Abholung');
+define('MODULE_PAYMENT_BAR_TEXT_EMAIL_FOOTER', 'Sie zahlen bar bei Abholung');
+?>
