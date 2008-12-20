cae80ec9381faa2f114c03957da5ff94a9a99d36
hugo13 <hugo13@d52c303f-c418-0410-a0d3-be95aab480a1>
2008-12-20 19:35:27 +0000

    rl_invoice3 :: auftragsgemaess template umbenannt

---
+++ zc138_addon/MIN/rl_invoice3/includes/templates/YOUR_TEMPLATE/template_info.php
@@ -0,0 +1,16 @@
+<?php
+/**          
+ * Template Information File
+ *
+ * @copyright 2006 rainer@langheiter.com
+ * @copyright Portions 2003-2005 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: template_info.php by hugo13
+ */
+$template_name = 'YourTemplate';
+$template_version = 'Version 1.0';
+$template_author = 'zen-cart.at';
+$template_description = '...';
+$template_screenshot = 'screenshot.gif';
+?>
