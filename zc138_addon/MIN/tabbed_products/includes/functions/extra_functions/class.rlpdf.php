f261b0300320f569dec9701163e43bc0a70a615f
hugo13 <hugo13@d52c303f-c418-0410-a0d3-be95aab480a1>
2009-03-02 08:16:53 +0000

    TPP :: zusaetzlich werden noch alle products_model-dateien UND ordner aufgelistet

--- zc138_addon/MIN/tabbed_products/includes/functions/extra_functions/class.rlpdf.php
+++ zc138_addon/MIN/tabbed_products/includes/functions/extra_functions/class.rlpdf.php
@@ -8,51 +8,68 @@
  * 
  * @author rainer AT langheiter DOT com // http://www.filosofisch.com // http://edv.langheiter.com
  * example: <!--%PDF%--><!--###getPDF###-->
- * 
- * 
  * first part == name of tab ( <!--%PDF%--> )
  * second part == name of called function; param: products_id  ( <!--###getPDF###--> )
  */
 
 
 class rlPDF {
-    public $wpinstall;
+    private $db;
     private $pid;
+    private $files;
     
     function __construct($pid = 1){
         $this->pid = $pid;
+        global $db;
+        $this->db = $db;
+        $this->files = array();     
     }
     
     public function getPDFLink(){
         $content = '';
-        $files = array();
+        
+        $this->getFileFolder($this->pid);
+        $this->getFileFolder($this->getProductsModel());
+        
+        $content = '<div id="pdf-files"><h4>klick to view</h4><ul>';
+
+        foreach ($this->files as $key => $value) {
+            $content .= '<li><a href="' . $value . '" target="_blank">' . $key . '</a></li>';
+        }
+        $content .= '</ul></div>';
+        return $content ;
+    }
+    public function getProductsModel(){
+        $sql = "SELECT products_model FROM " . TABLE_PRODUCTS . " WHERE products_id='" . $this->pid . "' LIMIT 1";
+        $res = $this->db->Execute($sql);
+        if($res->RecordCount()!=0){
+            return $res->fields['products_model'];
+        }
+    }
+
+    public function getFileFolder($search){
         $path = DIR_WS_IMAGES . 'PDF/';
         // check single file
-        $singleFile = $path . $this->pid . '.pdf';
+        $singleFile = $path . $search . '.pdf';
         if(file_exists($singleFile)){
-            $files[$this->pid . '.pdf'] = $singleFile;
+            $this->files[$search . '.pdf'] = $singleFile;
         }
         
         // check pid-folder
-        $path = DIR_WS_IMAGES . 'PDF/' . $this->pid . '/';
+        $path = DIR_WS_IMAGES . 'PDF/' . $search . '/';
         if ($handle = @opendir($path)) {
             while (false !== ($file = readdir($handle))) {
                 if($file == '.' || $file == '..') {
                     // nothing todo
                 } else {
-                    $files[$file] .=  $path  . $file;
+                    $this->files[$file] .=  $path  . $file;
                 }
             }
             closedir($handle);
         } else {
             // nothing todo
         }
-        $content = '<div id="pdf-files"><h4>klick to view</h4><ul>';
-        foreach ($files as $key => $value) {
-            $content .= '<li><a href="' . $value . '" target="_blank">' . $key . '</a></li>';
-        }
-        $content .= '</ul></div>';
-        return $content ;
+        
     }
 }
 
