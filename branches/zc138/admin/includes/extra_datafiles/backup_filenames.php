3c6cee0d817db8f510811ba05682d8e82c5ce9d2
webchills <webchills@d52c303f-c418-0410-a0d3-be95aab480a1>
2009-03-28 17:34:21 +0000

    SQL Backup Tool von web28 integriert. See: http://www.zen-cart.at/zcvb/forum/downloads.php?action=ratelink&catid=8&linkid=156

---
+++ branches/zc138/admin/includes/extra_datafiles/backup_filenames.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package admin
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: music_type_filenames.php 3001 2006-02-09 21:45:06Z wilt $
+ */
+//if (!defined('IS_ADMIN_FLAG')) {
+  //die('Illegal Access');
+//}
+  define('FILENAME_BACKUP', 'backup');
+?>
