907814b77277cbe0f4d0c48562960eea54f6b468
hugo13 <hugo13@d52c303f-c418-0410-a0d3-be95aab480a1>
2010-04-29 11:00:52 +0000

    kleine anpassungen an layout controller zen_cr.sh == rechte setzen auf der console

--- zen_cr.sh
+++ zen_cr.sh
@@ -1,8 +1,15 @@
 chmod -R 777 cache
 chmod -R 777 images
 chmod -R 777 includes/languages/english/html_includes
+chmod -R 777 includes/languages/german/html_includes
 chmod -R 777 media
 chmod -R 777 pub
 chmod -R 777 admin/backups
 chmod -R 777 admin/images/graphs
+
+touch includes/configure.php
+touch admin/includes/configure.php
+chmod 777 includes/configure.php
+chmod 777 admin/includes/configure.php
+
 
