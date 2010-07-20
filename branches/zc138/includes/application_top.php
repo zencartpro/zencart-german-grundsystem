f884f3274b1f6defc20b74f2893783848824672e
webchills <webchills@d52c303f-c418-0410-a0d3-be95aab480a1>
2010-07-20 08:02:08 +0000

    vatinfo Anzeige aktualisiert diverse Sicherheitsdinge aus 1.3.9 Bundesland standardmaessig auf aus Bruttopreise standardmaeesig auf ein Aehnliche Artikel auf Empfohlene Artikel umbenannt Warnung bei nicht umbenanntem Admin integriert

--- branches/zc138/includes/application_top.php
+++ branches/zc138/includes/application_top.php
@@ -7,27 +7,49 @@
  * see {@link  http://www.zen-cart.com/wiki/index.php/Developers_API_Tutorials#InitSystem wikitutorials} for more details.
  *
  * @package initSystem
- * @copyright Copyright 2003-2007 Zen Cart Development Team
+ * @copyright Copyright 2003-2010 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: application_top.php 7470 2007-11-24 20:54:50Z drbyte $
+ * @version $Id: application_top.php 2010-07-20 09:34:50Z webchills $
  */
 /**
- * prevent hack attempts which waste CPU cycles
+ * inoculate against hack attempts which waste CPU cycles
  */
-$paramsToCheck = array('main_page', 'cPath', 'products_id', 'language', 'currency', 'action', 'manufacturers_id', 'pID', 'pid', 'reviews_id', 'filter_id', 'zenid', 'sort', 'number_of_uploads', 'notify', 'page_holder', 'chapter', 'alpha_filter_id', 'typefilter', 'disp_order', 'id', 'key', 'music_genre_id', 'record_company_id', 'set_session_login', 'faq_item', 'edit', 'delete', 'search_in_description', 'dfrom', 'pfrom', 'dto', 'pto', 'inc_subcat', 'payment_error', 'order', 'gv_no', 'pos', 'addr', 'error', 'count', 'error_message', 'info_message', 'cID', 'page');
-$contaminated = false;
-foreach($paramsToCheck as $key) {
-  if (isset($_GET[$key]) && substr($_GET[$key], 0, 4) == 'http') {
+$contaminated = (isset($_FILES['GLOBALS']) || isset($_REQUEST['GLOBALS'])) ? true : false;
+$paramsToAvoid = array('GLOBALS', '_COOKIE', '_ENV', '_FILES', '_GET', '_POST', '_REQUEST', '_SERVER', '_SESSION', 'HTTP_COOKIE_VARS', 'HTTP_ENV_VARS', 'HTTP_GET_VARS', 'HTTP_POST_VARS', 'HTTP_POST_FILES', 'HTTP_RAW_POST_DATA', 'HTTP_SERVER_VARS', 'HTTP_SESSION_VARS');
+$paramsToAvoid[] = 'autoLoadConfig';
+$paramsToAvoid[] = 'mosConfig_absolute_path';
+$paramsToAvoid[] = 'hash';
+$paramsToAvoid[] = 'main';
+foreach($paramsToAvoid as $key) {
+  if (isset($_GET[$key]) || isset($_POST[$key]) || isset($_COOKIE[$key])) {
     $contaminated = true;
     break;
   }
 }
-if ($contaminated || isset($_GET['autoLoadConfig']) || isset($_GET['mosConfig_absolute_path']) || isset($_FILES['GLOBALS']) || isset($_REQUEST['GLOBALS']) )
+$paramsToCheck = array('main_page', 'cPath', 'products_id', 'language', 'currency', 'action', 'manufacturers_id', 'pID', 'pid', 'reviews_id', 'filter_id', 'zenid', 'sort', 'number_of_uploads', 'notify', 'page_holder', 'chapter', 'alpha_filter_id', 'typefilter', 'disp_order', 'id', 'key', 'music_genre_id', 'record_company_id', 'set_session_login', 'faq_item', 'edit', 'delete', 'search_in_description', 'dfrom', 'pfrom', 'dto', 'pto', 'inc_subcat', 'payment_error', 'order', 'gv_no', 'pos', 'addr', 'error', 'count', 'error_message', 'info_message', 'cID', 'page', 'credit_class_error_code');
+if (!$contaminated) {
+  foreach($paramsToCheck as $key) {
+    if (isset($_GET[$key]) && !is_array($_GET[$key])) {
+      if (substr($_GET[$key], 0, 4) == 'http' || strstr($_GET[$key], '//')) {
+        $contaminated = true;
+        break;
+      }
+      if (isset($_GET[$key]) && strlen($_GET[$key]) > 43) {
+        $contaminated = true;
+        break;
+      }
+    }
+  }
+}
+unset($paramsToCheck, $paramsToAvoid, $key);
+if ($contaminated)
 {
   header('HTTP/1.1 406 Not Acceptable');
   exit(0);
 }
+unset($contaminated);
+/* *** END OF INNOCULATION *** */
 
 /**
  * boolean used to see if we are in the admin script, obviously set to false here.
