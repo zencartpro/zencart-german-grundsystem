bac7369572d3e854148d7a31e2603047b74f5e92
webchills <webchills@d52c303f-c418-0410-a0d3-be95aab480a1>
2010-08-03 14:53:34 +0000

    phpBB Setup entfernt

--- branches/zc139/zc_install/includes/templates/template_default/sideboxes/german/navigation.php
+++ branches/zc139/zc_install/includes/templates/template_default/sideboxes/german/navigation.php
@@ -2,7 +2,7 @@
 /**
  * @package Installer
  * @access private
- * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Copyright 2003-2010 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id$
@@ -14,7 +14,6 @@
   <li id="licenseaccept">GPL Lizenz</li>
   <li id="inspection">Systempr&uuml;fung</li>
   <li id="system">Servereinstellungen</li>
-  <li id="phpbb">phpBB Integration</li>
   <li id="database">Datenbankanbindung</li>
 <?php if ((isset($is_upgradable) && $is_upgradable) || (isset($is_upgrade) && $is_upgrade)) { ?>
   <li id="databaseupg">Datenbank Upgrade</li>
