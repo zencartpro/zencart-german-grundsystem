0241b9fd47a5b2d42c4a7ec0e75b670c689b69cc
maleborg <maleborg@d52c303f-c418-0410-a0d3-be95aab480a1>
2010-09-30 12:05:23 +0000

    Neue Datei languages/german/alert_page.php und ein paar Anpassungen

---
+++ admin/includes/languages/german/alert_page.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package admin
+ * @copyright Copyright 2003-2010 Zen Cart Development Team
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id$
+ */
+
+define('HEADING_TITLE', 'Warnung!');
+define('ALERT_PART1', 'Warnung: Sie können sich nicht in den Adminbereich einloggen bis Sie');
+define('ALERT_RENAME_ADMIN', 'das Adminverzeichnis umbenannt haben.');
+define('ALERT_REMOVE_ZCINSTALL', 'das Verzeichnis zc_install gelöscht haben.<br />(Benutzen Sie dazu Ihr FTP Programm oder den Adminbereich Ihres Webspace Anbieters.)');
+define('ADMIN_RENAME_FAQ_NOTE', 'Hilfe für das Umbenennen Ihres Adminverzeichnises finden Sie hier');
+define('ALERT_PART2', 'Danach erreichen Sie ihren Adminbereich unter der neuen URL im Browser, z.B.: <u>http://www.ihredomain.de/IhrUmbenannterAdminfolder/</u> ');
