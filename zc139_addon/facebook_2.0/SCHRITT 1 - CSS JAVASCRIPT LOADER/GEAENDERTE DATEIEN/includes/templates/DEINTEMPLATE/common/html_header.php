462b5e6338102847b21fe4bb2c26b93974a6edcd
webchills <webchills@d52c303f-c418-0410-a0d3-be95aab480a1>
2011-05-20 08:28:51 +0000

    Fehler in Ordnerstruktur und in html_header.php behoben

--- zc139_addon/facebook_2.0/SCHRITT 1 - CSS JAVASCRIPT LOADER/GEAENDERTE DATEIEN/includes/templates/DEINTEMPLATE/common/html_header.php
+++ zc139_addon/facebook_2.0/SCHRITT 1 - CSS JAVASCRIPT LOADER/GEAENDERTE DATEIEN/includes/templates/DEINTEMPLATE/common/html_header.php
@@ -5,10 +5,10 @@
  * outputs the html header. i,e, everything that comes before the \</head\> tag <br />
  * 
  * @package templateSystem
- * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Copyright 2003-2011 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: html_header.php 6948 2007-09-02 23:30:49Z drbyte $
+ * @version $Id: html_header.php for cssjs loader 2011-05-19 10:30:49Z webchills $
  */
 /**
  * load the module for generating page meta-tags
@@ -37,6 +37,9 @@ require(DIR_WS_MODULES . zen_get_module_directory('meta_tags.php'));
 <?php } //endif FAVICON ?>
 
 <base href="<?php echo (($request_type == 'SSL') ? HTTPS_SERVER . DIR_WS_HTTPS_CATALOG : HTTP_SERVER . DIR_WS_CATALOG ); ?>" />
+<?php if (isset($canonicalLink) && $canonicalLink != '') { ?>
+<link rel="canonical" href="<?php echo $canonicalLink; ?>" />
+<?php } ?>
 
 <?php
 /**
