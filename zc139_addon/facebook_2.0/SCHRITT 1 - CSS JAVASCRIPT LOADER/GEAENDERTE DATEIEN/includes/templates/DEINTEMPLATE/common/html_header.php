a84c74c2d394d0c507301804e7287b852b1595b4
webchills <webchills@d52c303f-c418-0410-a0d3-be95aab480a1>
2011-06-28 15:59:08 +0000

    canonical ergaenzt in html_header.php

--- zc139_addon/facebook_2.0/SCHRITT 1 - CSS JAVASCRIPT LOADER/GEAENDERTE DATEIEN/includes/templates/DEINTEMPLATE/common/html_header.php
+++ zc139_addon/facebook_2.0/SCHRITT 1 - CSS JAVASCRIPT LOADER/GEAENDERTE DATEIEN/includes/templates/DEINTEMPLATE/common/html_header.php
@@ -8,7 +8,7 @@
  * @copyright Copyright 2003-2011 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: html_header.php for cssjs loader 2011-05-19 10:30:49Z webchills $
+ * @version $Id: html_header.php for CSS/JS Loader ZC139 2011-06-28 17:30:49Z webchills $
  */
 /**
  * load the module for generating page meta-tags
@@ -40,7 +40,6 @@ require(DIR_WS_MODULES . zen_get_module_directory('meta_tags.php'));
 <?php if (isset($canonicalLink) && $canonicalLink != '') { ?>
 <link rel="canonical" href="<?php echo $canonicalLink; ?>" />
 <?php } ?>
-
 <?php
 /**
 * load the loader files
