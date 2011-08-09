8a56bd5b93f09ae3eb025fc77fe88f789ebdeaca
hugo13 <hugo13@d52c303f-c418-0410-a0d3-be95aab480a1>
2011-08-09 19:15:09 +0000

    install auf multilang umgestellt

---
+++ zc_install/includes/templates/template_default/sideboxes/german/navigation.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package Installer
+ * @access private
+ * @copyright Copyright 2003-2010 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id$
+ */
+
+?>
+<ul>
+  <li id="welcome">Willkommen</li>
+  <li id="licenseaccept">GPL Lizenz</li>
+  <li id="inspection">Systempr&uuml;fung</li>
+  <li id="system">Servereinstellungen</li>
+  <li id="database">Datenbankanbindung</li>
+<?php if ((isset($is_upgradable) && $is_upgradable) || (isset($is_upgrade) && $is_upgrade)) { ?>
+  <li id="databaseupg">Datenbank Upgrade</li>
+<?php } ?>
+  <li id="store">Shopkonfiguration</li>
+  <li id="admin">Administrator Konto</li>
+  <li id="finish">Fertigstellung</li>
+</ul>
