09c28826c0fb8032c89092a8241c6c3ea963eed9
hugo13 <hugo13@d52c303f-c418-0410-a0d3-be95aab480a1>
2011-08-12 07:58:36 +0000

    shop: templates hinzugefuegt & angepasst

---
+++ includes/templates/viennabright/templates/tpl_index_default.php
@@ -0,0 +1,105 @@
+<?php
+/**
+* Template designed by 12leaves.com
+* 12leaves.com - Free ecommerce templates and design services
+ * 
+ * @copyright Copyright 2008-2009 12leaves.com
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ */
+?>
+<div class="centerColumn" id="indexDefault">
+
+<!--<h1 id="indexDefaultHeading"><?php echo HEADING_TITLE; ?></h1>
+<?php if (SHOW_CUSTOMER_GREETING == 1) { ?>
+<?php echo zen_customer_greeting(); ?>
+<?php } ?>
+
+<?php if (TEXT_MAIN) { ?>
+<div id="" class="content"><?php echo TEXT_MAIN; ?></div>
+<?php } ?>-->
+
+<!-- deprecated - to use uncomment this section
+<?php if (TEXT_INFORMATION) { ?>
+<div id="" class="content"><?php echo TEXT_INFORMATION; ?></div>
+<?php } ?>-->
+
+<?php if (DEFINE_MAIN_PAGE_STATUS >= 1 and DEFINE_MAIN_PAGE_STATUS <= 2) { ?>
+<?php
+/**
+ * get the Define Main Page Text
+ */
+?>
+<div id="indexDefaultMainContent" class="content"><?php require($define_page); ?></div>
+<?php } ?>
+
+<!-- Home page banner 1 -->
+<?php
+  if (SHOW_BANNERS_GROUP_SET1 != '' && $banner = zen_banner_exists('dynamic', SHOW_BANNERS_GROUP_SET1)) {
+    if ($banner->RecordCount() > 0) {
+?>
+<div id="bannerOne" class="banners"><?php echo zen_display_banner('static', $banner); ?></div>
+<?php
+    }
+  }
+?>
+
+<!-- Home page banner 2 -->
+<?php
+  if (SHOW_BANNERS_GROUP_SET2 != '' && $banner = zen_banner_exists('dynamic', SHOW_BANNERS_GROUP_SET2)) {
+    if ($banner->RecordCount() > 0) {
+?>
+<div id="bannerTwo" class="banners"><?php echo zen_display_banner('static', $banner); ?></div>
+<?php
+    }
+  }
+?>
+
+
+<?php
+  $show_display_category = $db->Execute(SQL_SHOW_PRODUCT_INFO_MAIN);
+  while (!$show_display_category->EOF) {
+?>
+
+<?php if ($show_display_category->fields['configuration_key'] == 'SHOW_PRODUCT_INFO_MAIN_FEATURED_PRODUCTS') { ?>
+<?php
+/**
+ * display the Featured Products Center Box
+ */
+?>
+<?php require($template->get_template_dir('tpl_modules_featured_products.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_modules_featured_products.php'); ?>
+<?php } ?>
+
+<?php if ($show_display_category->fields['configuration_key'] == 'SHOW_PRODUCT_INFO_MAIN_SPECIALS_PRODUCTS') { ?>
+<?php
+/**
+ * display the Special Products Center Box
+ */
+?>
+<?php require($template->get_template_dir('tpl_modules_specials_default.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_modules_specials_default.php'); ?>
+<?php } ?>
+
+<?php if ($show_display_category->fields['configuration_key'] == 'SHOW_PRODUCT_INFO_MAIN_NEW_PRODUCTS') { ?>
+<?php
+/**
+ * display the New Products Center Box
+ */
+?>
+<?php require($template->get_template_dir('tpl_modules_whats_new.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_modules_whats_new.php'); ?>
+<?php } ?>
+
+<?php if ($show_display_category->fields['configuration_key'] == 'SHOW_PRODUCT_INFO_MAIN_UPCOMING') { ?>
+<?php
+/**
+ * display the Upcoming Products Center Box
+ */
+?>
+<?php include(DIR_WS_MODULES . zen_get_module_directory(FILENAME_UPCOMING_PRODUCTS)); ?><?php } ?>
+
+
+<?php
+  $show_display_category->MoveNext();
+} // !EOF
+?>
+</div>
