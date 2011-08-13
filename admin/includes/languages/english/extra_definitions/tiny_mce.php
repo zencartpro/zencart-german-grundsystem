752a268bf0ebef2b0a39ce2668b438a7a813b466
hugo13 <hugo13@d52c303f-c418-0410-a0d3-be95aab480a1>
2011-08-13 13:47:09 +0000

    admin: TINYMCE als editor hinzugefuegt

---
+++ admin/includes/languages/english/extra_definitions/tiny_mce.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package htmleditors
+ * @copyright Copyright 2009 Kuroi Web Design
+ * @copyright Portions Copyright 2003-2006 Zen Cart Development Team
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id$
+ */
+
+define('EDITOR_TINYMCE', 'TinyMCE');
