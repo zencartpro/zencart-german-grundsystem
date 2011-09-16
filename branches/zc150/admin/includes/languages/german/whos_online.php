c7139d907ae54d1101f818e1d13e0327c500678a
webchills <webchills@d52c303f-c418-0410-a0d3-be95aab480a1>
2011-09-16 14:36:38 +0000

    fehlende uebersetzung im admin bei whos online

--- branches/zc150/admin/includes/languages/german/whos_online.php
+++ branches/zc150/admin/includes/languages/german/whos_online.php
@@ -1,15 +1,12 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2003-2010 Zen Cart Development Team
+ * @copyright Copyright 2003-2011 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.at/license/2_0.txt GNU Public License V2.0
  * @translators: cyaneo/hugo13/wflohr/maleborg/webchills	http://www.zen-cart.at
- * @version $Id: whos_online.php 627 2010-08-30 15:05:14Z webchills $
+ * @version $Id: whos_online.php 628 2011-09-16 16:28:14Z webchills $
  */
-
-//  $Id: whos_online.php 627 2010-08-30 15:05:14Z webchills $
-//
 
 define('HEADING_TITLE','Wer ist online');
 define('TABLE_HEADING_ONLINE','Online');
@@ -37,22 +34,18 @@ define('WHOS_ONLINE_INACTIVE_LAST_CLICK_TEXT','Inaktiv ist letzter Klick >=');
 define('WHOS_ONLINE_INACTIVE_ARRIVAL_TEXT','Inaktiv seit Ankunft >');
 define('WHOS_ONLINE_REMOVED_TEXT','wird entfernt');
 
-// whos_online.php
-define('WHOIS_TIMER_REMOVE',1200);
-// seconds when removed from whos_online table - 1200 default = 20 minutes
-define('WHOIS_TIMER_INACTIVE',180);
-// seconds when considered inactive - 180 default = 3 minutes
-define('WHOIS_TIMER_DEAD',540);
-// seconds when considered dead - 540 default = 9 minutes
-define('WHOIS_SHOW_HOST','1');
-// show Last Clicked time and host name - 1 default
-define('WHOIS_REPEAT_LEGEND_BOTTOM','12');
-// show legend on bottom when more than how many entries - 12 default
+define('WHOIS_TIMER_REMOVE', 1200); // seconds when removed from whos_online table - 1200 default = 20 minutes
+define('WHOIS_TIMER_INACTIVE', 180); // seconds when considered inactive - 180 default = 3 minutes
+define('WHOIS_TIMER_DEAD', 540); // seconds when considered dead - 540 default = 9 minutes
+define('WHOIS_SHOW_HOST', '1'); // show Last Clicked time and host name - 1 default
+define('WHOIS_REPEAT_LEGEND_BOTTOM', '12'); // show legend on bottom when more than how many entries - 12 default
 define('OFFICE_IP_TO_HOST_ADDRESS', 'AUS');
 define('TEXT_SESSION_ID', '<strong>Session ID:</strong> ');
 define('TEXT_HOST', '<strong>Host:</strong> ');
 define('TEXT_USER_AGENT', '<strong>User Agent:</strong> ');
 define('TEXT_EMPTY_CART', '<strong>Leerer Warenkorb</strong>');
+define('TEXT_WHOS_ONLINE_FILTER_SPIDERS', 'Spiders ausschliessen');
+define('TEXT_WHOS_ONLINE_FILTER_ADMINS', 'Admin IP Addressen ausschliessen?');
 define('TEXT_WHOS_ONLINE_TIMER_UPDATING', 'Aktualisiere ');
 define('TEXT_WHOS_ONLINE_TIMER_EVERY', 'alle %s Sekunden.&nbsp;&nbsp;');
 define('TEXT_WHOS_ONLINE_TIMER_DISABLED', 'Manuell');
