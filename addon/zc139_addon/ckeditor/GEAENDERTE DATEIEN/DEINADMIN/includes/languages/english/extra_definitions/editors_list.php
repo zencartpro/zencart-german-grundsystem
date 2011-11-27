7a8e98e937a39eefda067e8ab90addec16844e20
webchills <webchills@d52c303f-c418-0410-a0d3-be95aab480a1>
2011-11-27 12:05:27 +0000

    ckeditor 3.6.2

---
+++ addon/zc139_addon/ckeditor/GEAENDERTE DATEIEN/DEINADMIN/includes/languages/english/extra_definitions/editors_list.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package htmleditors
+ * @copyright Copyright 2010 Kuroi Web Design
+ * @copyright Portions Copyright 2003-2011 Zen Cart Development Team
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: editors_list.php 2011-11-27 12:47:10Z webchills $
+ */
+
+/*
+ * File included to "reset" editors list
+ */
+
+  define('EDITOR_HTMLAREA', 'HTMLarea');
+  define('EDITOR_CKEDITOR', 'CKeditor');
+  // uncomment other editors if you are using them
+  //  define('EDITOR_TINYMCE', 'TinyMCE');
+  //  define('EDITOR_FCKEDITOR', 'FCKeditor');
---
+++ addon/zc139_addon/ckeditor/NEUE DATEIEN/DEINADMIN/includes/languages/german/extra_definitions/ckeditor.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package htmleditors
+ * @copyright Copyright 2010 Kuroi Web Design
+ * @copyright Portions Copyright 2003-2011 Zen Cart Development Team
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: ckeditor.php 2011-11-27 12:28:02Z webchills $
+ */
+
+  define('EDITOR_CKEDITOR', 'CKEditor');
