7529597c58588c55677ff813f4f642ad8bfa93bc
webchills <webchills@d52c303f-c418-0410-a0d3-be95aab480a1>
2011-11-29 18:22:41 +0000

    lightbox 1.6.3

---
+++ addon/150/lightbox/NEUE DATEIEN/includes/classes/zen_lightbox/autoload_default.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Zen Lightbox
+ *
+ * @author Alex Clarke (aclarke@ansellandclarke.co.uk)
+ * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: autoload_default.php 2008-12-09 aclarke $
+ */
+?>
+
+jQuery(function($) {
+        $("a[rel^='lightbox']").slimbox({<?php require_once(DIR_FS_CATALOG . DIR_WS_CLASSES . 'zen_lightbox/options.php'); ?>}, function(el) 
+        {
+                return [el.href, el.title /* + '<br /><a href="' + el.href + '">Download this image</a>'*/];
+        }, function(el) {
+                return (this == el) || ((this.rel.length > 8) && (this.rel == el.rel));
+        });
+});
+//--></script>
+
+<?php if (ZEN_LIGHTBOX_CLOSE_IMAGE == 'true' || ZEN_LIGHTBOX_PREV_NEXT == 'true') { echo '<style> #lbPrevLink, #lbNextLink {display: block; position: absolute; top: 0; width: 63px; height:32px!important; outline: none;} #lbNextLink {right: 0; background: transparent url(images/zen_lightbox/nextlabel.gif) no-repeat 100% 15%;} #lbPrevLink {left: 0; background: transparent url(images/zen_lightbox/prevlabel.gif) no-repeat 0 15%; } </style>';}?>
---
+++ addon/150/lightbox/NEUE DATEIEN/includes/functions/extra_functions/zen_lightbox.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Zen Lightbox
+ *
+ * @author Alex Clarke (aclarke@ansellandclarke.co.uk)
+ * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: zen_lightbox.php 2008-12-11 aclarke $
+ */
+
+function zen_lightbox($src, $alt = '', $width = '', $height = '', $parameters = '') {
+  global $template_dir;
+  
+  //auto replace with defined missing image
+  if ($src == DIR_WS_IMAGES and PRODUCTS_IMAGE_NO_IMAGE_STATUS == '1') {
+    $src = DIR_WS_IMAGES . PRODUCTS_IMAGE_NO_IMAGE;
+  }
+
+  if ((empty($src) || ($src == DIR_WS_IMAGES)) && (IMAGE_REQUIRED == 'false')) {
+    return false;
+  }
+
+  // if not in current template switch to template_default
+  if (!file_exists($src)) {
+    $src = str_replace(DIR_WS_TEMPLATES . $template_dir, DIR_WS_TEMPLATES . 'template_default', $src);
+  }
+
+  // hook for handle_image() function such as Image Handler etc
+  if (function_exists('handle_image')) {
+    $newimg = handle_image($src, $alt, $width, $height, $parameters);
+    list($src, $alt, $width, $height, $parameters) = $newimg; 
+  }
+
+  $image = zen_output_string($src);
+
+  return $image;
+}
+?>
