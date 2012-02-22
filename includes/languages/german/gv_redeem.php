f2a75fcfaf0712eac00fc0a09342e083845a878c
Jörg Schönberger <office@webchills.at>
2012-02-22 09:04:28 +0000

    Bugfixes Geschenkgutschein Sprachfiles

--- includes/languages/german/gv_redeem.php
+++ includes/languages/german/gv_redeem.php
@@ -1,27 +1,15 @@
 <?php
-//
-// +----------------------------------------------------------------------+
-// |zen-cart Open Source E-commerce                                       |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2003 The zen-cart developers                           |
-// |                                                                      |
-// | http://www.zen-cart.com/index.php                                    |
-// |                                                                      |
-// | Portions Copyright (c) 2003 osCommerce                               |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the GPL license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available through the world-wide-web at the following url:           |
-// | http://www.zen-cart.com/license/2_0.txt.                             |
-// | If you did not receive a copy of the zen-cart license and are unable |
-// | to obtain it through the world-wide-web, please send a note to       |
-// | license@zen-cart.com so we can mail you a copy immediately.          |
-// +----------------------------------------------------------------------+
-// $Id: gv_redeem.php 293 2008-05-28 21:10:40Z maleborg $
-//
+/**
+ * @package languageDefines
+ * @copyright Copyright 2003-2012 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @translators: cyaneo/hugo13/wflohr/maleborg/webchills	http://www.zen-cart-pro.at
+ * @version $Id: gv_redeem.php 628 2012-02-22 09:05:14Z webchills $
+ */
 
-define('NAVBAR_TITLE', TEXT_GV_NAME .' einlösen ');
-define('HEADING_TITLE', TEXT_GV_NAME .' einlösen ');
-define('TEXT_INFORMATION', 'Weitere Informationen zum Thema ' . TEXT_GV_NAME . ' erfahren Sie in unserer <br /><br /><strong><a href="' . zen_href_link(FILENAME_GV_FAQ, '', 'NONSSL').'">' . GV_FAQ . '.</a></strong>');
-define('TEXT_INVALID_GV','Der ' . TEXT_GV_NAME . 'code ist ungültig oder wurde bereits eingelöst. Wenn Sie Fragen haben, kontaktieren Sie uns bitte über unser Kontaktformular hier im Shop');
-define('TEXT_VALID_GV','Herzlichen Glückwunsch, Sie haben einen ' . TEXT_GV_NAME . ' im Wert von %s eingelöst.');
+define('NAVBAR_TITLE', 'Geschenkgutschein einlösen ');
+define('HEADING_TITLE', 'Geschenkgutschein einlösen ');
+define('TEXT_INFORMATION', 'Weitere Informationen zum Thema Geschenkgutscheine erhalten Sie in unserer <br /><br /><strong><a href="' . zen_href_link(FILENAME_GV_FAQ, '', 'NONSSL').'">' . GV_FAQ . '.</a></strong>');
+define('TEXT_INVALID_GV','Der Gutscheincode ist ungültig oder wurde bereits eingelöst. Wenn Sie Fragen haben, kontaktieren Sie uns bitte über unser Kontaktformular hier im Shop');
+define('TEXT_VALID_GV','Herzlichen Glückwunsch, Sie haben einen Geschenkgutschein im Wert von %s eingelöst.');
