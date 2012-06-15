4dff24377800e54d30c5066096c3c6d15100e672
webchills <webchills@d52c303f-c418-0410-a0d3-be95aab480a1>
2012-06-15 05:01:49 +0000

    Artikelmerkmale fuer Buttonloesung

---
+++ addon/150/artikelmerkmale-buttonloesung/SQL/install.sql
@@ -0,0 +1,5 @@
+##################################################################################
+# Zusatzfeld für Artikelmerkmale (Buttonlösung) - 2012-06-13 - webchills
+##################################################################################
+
+ALTER TABLE `products_description` ADD `products_merkmale` VARCHAR( 64 ) NOT NULL;
