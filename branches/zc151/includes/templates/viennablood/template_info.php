30471194abf2940d6d815826721c5e8819ada498
webchills <webchills@d52c303f-c418-0410-a0d3-be95aab480a1>
2013-08-18 08:53:52 +0000

    Aktualisierung Viennablood

--- branches/zc151/includes/templates/viennablood/template_info.php
+++ branches/zc151/includes/templates/viennablood/template_info.php
@@ -3,14 +3,14 @@
  * Template Information File
  *
  * @copyright 2006 swl web design
- * @copyright Portions 2003-2005 Zen Cart Development Team
+ * @copyright Portions 2003-2013 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
- * @version $Id: template_info.php by swl
+ * @version $Id: template_info.php 2013-08-17 09:58:36Z webchills $
  */
 $template_name = 'ViennaBlood';
-$template_version = 'Version 1.0';
-$template_author = 'zen-cart.at';
-$template_description = 'Simple red & white template for zen-cart.';
+$template_version = 'Version 1.1';
+$template_author = 'zen-cart-pro.at';
+$template_description = 'Simple red & white template for Zen Cart';
 $template_screenshot = 'screenshot.jpg';
 ?>
