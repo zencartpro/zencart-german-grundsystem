eca1ad0bb9b885214ad74ad1a96e5987166004f1
Jörg Schönberger <office@webchills.at>
2013-09-23 10:02:06 +0000

    Template Viennaorange

---
+++ includes/templates/viennaorange/sideboxes/tpl_search_header.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Template designed by 12leaves.com
+* 12leaves.com - Free ecommerce templates and design services
+* 
+* Side Box Template
+*
+* @package templateSystem
+* @copyright Copyright 2008-2009 12leaves.com
+* @copyright Copyright 2003-2013 Zen Cart Development Team
+* @copyright Portions Copyright 2003 osCommerce
+* @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
+* @version $Id: tpl_search_header.php 836 2012-01-01 20:33:58Z webchills $
+*/
+ 
+  $content = "";
+?>
