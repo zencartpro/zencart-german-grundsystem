a0d17312dbb7ffbf239d54bfbe15faea587fa186
Jörg Schönberger <office@webchills.at>
2013-09-28 06:20:54 +0000

    Aktualisierung Classic

--- UPLOAD/includes/templates/classic/template_info.php
+++ UPLOAD/includes/templates/classic/template_info.php
@@ -3,14 +3,14 @@
  * Template Information File
  *
  * @package templateSystem
- * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
- * @version $Id: template_info.php 729 2011-08-09 15:49:16Z hugo13 $
+ * @version $Id: template_info.php 730 2013-09-26 08:49:16Z webchills $
  */
-$template_name = 'Classic Contemporary Green';
-$template_version = 'Version 1.3.5';
-$template_author = 'Zen Cart Team (c) 2006';
+$template_name = 'Classic';
+$template_version = 'Version 1.5.1';
+$template_author = 'Zen Cart Team (c) 2006-2013';
 $template_description = 'This template set is designed to be easily modified using only the style sheet to change colors, fonts, and the store logo. Three images are required; logo.jpg, header_bg.jpg, and tile_back.gif.';
 $template_screenshot = 'scr_template_default.jpg';
 ?>
