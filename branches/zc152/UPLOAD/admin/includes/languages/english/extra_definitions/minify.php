79b9de9f6d04bc8c608481acdede18d7ecd572fc
webchills <webchills@d52c303f-c418-0410-a0d3-be95aab480a1>
2014-03-23 12:03:06 +0000

    CSS/JS Loader / Minify fix integriert

---
+++ branches/zc152/UPLOAD/admin/includes/languages/english/extra_definitions/minify.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * CSS/JS Loader - Minify
+ *
+ * @copyright Copyright 2003-2014 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
+ * @version $Id: minify.php 2014-03-23 09:34:05 webchills $
+ */
+
+define('BOX_CONFIGURATION_MINIFY', 'Minify');
--- branches/zc152/UPLOAD/includes/templates/template_default/common/html_header.php
+++ branches/zc152/UPLOAD/includes/templates/template_default/common/html_header.php
@@ -8,7 +8,7 @@
  * @copyright Copyright 2003-2014 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
- * @version $Id: html_header.php 847 2013-03-01 11:10:39Z webchills $
+ * @version $Id: html_header.php 848 2014-03-23 12:10:39Z webchills $
  */
 /**
  * load the module for generating page meta-tags
@@ -27,7 +27,7 @@ require(DIR_WS_MODULES . zen_get_module_directory('meta_tags.php'));
 <meta name="description" content="<?php echo META_TAG_DESCRIPTION; ?>" />
 <meta http-equiv="imagetoolbar" content="no" />
 <meta name="author" content="The Zen Cart&reg; Team and others" />
-<meta name="generator" content="Zen-Cart 1.5 - deutsche Version, http://www.zen-cart-pro.at" />
+<meta name="generator" content="Zen-Cart 1.5.2 - deutsche Version, http://www.zen-cart-pro.at" />
 <?php if (defined('ROBOTS_PAGES_TO_SKIP') && in_array($current_page_base,explode(",",constant('ROBOTS_PAGES_TO_SKIP'))) || $current_page_base=='down_for_maintenance' || $robotsNoIndex === true) { ?>
 <meta name="robots" content="noindex, nofollow" />
 <?php } ?>
@@ -43,99 +43,32 @@ require(DIR_WS_MODULES . zen_get_module_directory('meta_tags.php'));
 
 <?php
 /**
- * load all template-specific stylesheets, named like "style*.css", alphabetically
- */
-  $directory_array = $template->get_template_part($template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css'), '/^style/', '.css');
-  while(list ($key, $value) = each($directory_array)) {
-    echo '<link rel="stylesheet" type="text/css" href="' . $template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css') . '/' . $value . '" />'."\n";
-  }
-/**
- * load stylesheets on a per-page/per-language/per-product/per-manufacturer/per-category basis. Concept by Juxi Zoza.
- */
-  $manufacturers_id = (isset($_GET['manufacturers_id'])) ? $_GET['manufacturers_id'] : '';
-  $tmp_products_id = (isset($_GET['products_id'])) ? (int)$_GET['products_id'] : '';
-  $tmp_pagename = ($this_is_home_page) ? 'index_home' : $current_page_base;
-  if ($current_page_base == 'page' && isset($ezpage_id)) $tmp_pagename = $current_page_base . (int)$ezpage_id;
-  $sheets_array = array('/' . $_SESSION['language'] . '_stylesheet',
-                        '/' . $tmp_pagename,
-                        '/' . $_SESSION['language'] . '_' . $tmp_pagename,
-                        '/c_' . $cPath,
-                        '/' . $_SESSION['language'] . '_c_' . $cPath,
-                        '/m_' . $manufacturers_id,
-                        '/' . $_SESSION['language'] . '_m_' . (int)$manufacturers_id,
-                        '/p_' . $tmp_products_id,
-                        '/' . $_SESSION['language'] . '_p_' . $tmp_products_id
-                        );
-  while(list ($key, $value) = each($sheets_array)) {
-    //echo "<!--looking for: $value-->\n";
-    $perpagefile = $template->get_template_dir('.css', DIR_WS_TEMPLATE, $current_page_base, 'css') . $value . '.css';
-    if (file_exists($perpagefile)) echo '<link rel="stylesheet" type="text/css" href="' . $perpagefile .'" />'."\n";
-  }
+* load the loader files
+*/
 
-/**
- *  custom category handling for a parent and all its children ... works for any c_XX_XX_children.css  where XX_XX is any parent category
- */
-  $tmp_cats = explode('_', $cPath);
-  $value = '';
-  foreach($tmp_cats as $val) {
-    $value .= $val;
-    $perpagefile = $template->get_template_dir('.css', DIR_WS_TEMPLATE, $current_page_base, 'css') . '/c_' . $value . '_children.css';
-    if (file_exists($perpagefile)) echo '<link rel="stylesheet" type="text/css" href="' . $perpagefile .'" />'."\n";
-    $perpagefile = $template->get_template_dir('.css', DIR_WS_TEMPLATE, $current_page_base, 'css') . '/' . $_SESSION['language'] . '_c_' . $value . '_children.css';
-    if (file_exists($perpagefile)) echo '<link rel="stylesheet" type="text/css" href="' . $perpagefile .'" />'."\n";
-    $value .= '_';
-  }
-/**
- * load printer-friendly stylesheets -- named like "print*.css", alphabetically
- */
-  $directory_array = $template->get_template_part($template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css'), '/^print/', '.css');
-  sort($directory_array);
-  while(list ($key, $value) = each($directory_array)) {
-    echo '<link rel="stylesheet" type="text/css" media="print" href="' . $template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css') . '/' . $value . '" />'."\n";
-  }
-
-/**
- * load all site-wide jscript_*.js files from includes/templates/YOURTEMPLATE/jscript, alphabetically
- */
-  $directory_array = $template->get_template_part($template->get_template_dir('.js',DIR_WS_TEMPLATE, $current_page_base,'jscript'), '/^jscript_/', '.js');
-  while(list ($key, $value) = each($directory_array)) {
-    echo '<script type="text/javascript" src="' .  $template->get_template_dir('.js',DIR_WS_TEMPLATE, $current_page_base,'jscript') . '/' . $value . '"></script>'."\n";
-  }
-
-/**
- * load all page-specific jscript_*.js files from includes/modules/pages/PAGENAME, alphabetically
- */
-  $directory_array = $template->get_template_part($page_directory, '/^jscript_/', '.js');
-  while(list ($key, $value) = each($directory_array)) {
-    echo '<script type="text/javascript" src="' . $page_directory . '/' . $value . '"></script>' . "\n";
-  }
-
-/**
- * load all site-wide jscript_*.php files from includes/templates/YOURTEMPLATE/jscript, alphabetically
- */
-  $directory_array = $template->get_template_part($template->get_template_dir('.php',DIR_WS_TEMPLATE, $current_page_base,'jscript'), '/^jscript_/', '.php');
-  while(list ($key, $value) = each($directory_array)) {
-/**
- * include content from all site-wide jscript_*.php files from includes/templates/YOURTEMPLATE/jscript, alphabetically.
- * These .PHP files can be manipulated by PHP when they're called, and are copied in-full to the browser page
- */
-    require($template->get_template_dir('.php',DIR_WS_TEMPLATE, $current_page_base,'jscript') . '/' . $value); echo "\n";
-  }
-/**
- * include content from all page-specific jscript_*.php files from includes/modules/pages/PAGENAME, alphabetically.
- */
-  $directory_array = $template->get_template_part($page_directory, '/^jscript_/');
-  while(list ($key, $value) = each($directory_array)) {
-/**
- * include content from all page-specific jscript_*.php files from includes/modules/pages/PAGENAME, alphabetically.
- * These .PHP files can be manipulated by PHP when they're called, and are copied in-full to the browser page
- */
-    require($page_directory . '/' . $value); echo "\n";
-  }
-
-// DEBUG: echo '<!-- I SEE cat: ' . $current_category_id . ' || vs cpath: ' . $cPath . ' || page: ' . $current_page . ' || template: ' . $current_template . ' || main = ' . ($this_is_home_page ? 'YES' : 'NO') . ' -->';
-
-
+if($RI_CJLoader->get('status') && (!isset($Ajax) || !$Ajax->status())){
+	$RI_CJLoader->autoloadLoaders();
+	$RI_CJLoader->loadCssJsFiles();
+	$files = $RI_CJLoader->header();
+	foreach($files['css'] as $file)
+		if($file['include']) {
+      include($file['src']);
+    } else if (!$RI_CJLoader->get('minify_css') || $file['external']) {
+      echo "<link rel=\"stylesheet\" type=\"text/css\" href='{$file['src']}' />\n";
+    } else {
+      echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"extras/min/?f={$file['src']}&amp;".$RI_CJLoader->get('minify_time')."\" />\n";
+    }
+		
+	foreach($files['jscript'] as $file)
+		if($file['include']) {
+      include($file['src']);
+    } else if(!$RI_CJLoader->get('minify_js') || $file['external']) {
+      echo "<script type='text/javascript' src='{$file['src']}'></script>\n";
+    } else {
+      echo "<script type=\"text/javascript\" src=\"extras/min/?f={$file['src']}&amp;".$RI_CJLoader->get('minify_time')."\"></script>\n";
+    }
+}
+//DEBUG: echo '<!-- I SEE cat: ' . $current_category_id . ' || vs cpath: ' . $cPath . ' || page: ' . $current_page . ' || template: ' . $current_template . ' || main = ' . ($this_is_home_page ? 'YES' : 'NO') . ' -->';
 ?>
 </head>
 <?php // NOTE: Blank line following is intended: ?>
