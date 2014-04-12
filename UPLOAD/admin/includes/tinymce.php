7d8d467b4182a59c8f985d93f82eae07627f8783
Jörg Schönberger <office@webchills.at>
2014-04-12 14:17:07 +0000

    1.5.2: Update Tiny MCE Editor auf 4.021

--- UPLOAD/admin/includes/tinymce.php
+++ UPLOAD/admin/includes/tinymce.php
@@ -1,67 +1,39 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2010 Kuroi Web Design
- * @copyright Portions Copyright 2014 Zen Cart Development Team
+ * @copyright Copyright 2003-2014 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
- * @version $Id: tinymce.php 756 2014-03-30 18:47:09Z webchills $
+ * @version $Id: tinymce.php 757 2014-04-12 18:47:09Z webchills $
  */
 if (!defined('IS_ADMIN_FLAG')) {
   die('Illegal Access');
 }
 
-
-
-$var = zen_get_languages();
-$jsLanguageLookupArray = "var lang = new Array;\n";
-foreach ($var as $key)
-{
-  $jsLanguageLookupArray .= "  lang[" . $key['id'] . "] = '" . $key['code'] . "';\n";
-}
 ?>
 <script type="text/javascript" src="../<?php echo DIR_WS_EDITORS ?>tiny_mce/jquery-1.11.0.min.js"></script>
-<script type="text/javascript" src="../<?php echo DIR_WS_EDITORS ?>tiny_mce/jquery.tinymce.js"></script>
-<script type="text/javascript"><!--
-$(document).ready(function() {
-  <?php echo $jsLanguageLookupArray ?>
-  $('textarea').each(function() {
-    if ($(this).attr('name') != 'message' && $(this).attr('class') != 'noEditor')
-    {
-      index = $(this).attr('name').match(/\d+/);
-      if (index == null) index = <?php echo $_SESSION['languages_id'] ?>;
-      $(this).tinymce(
-        {
-          // Language for this editor instance
-          language : lang[index],
-
-          // Location of TinyMCE script
-          script_url : '../<?php echo DIR_WS_EDITORS ?>/tiny_mce/tiny_mce.js',
-          relative_urls : true,
-          remove_script_host : false,
-          // utf-8 please
-          entity_encoding : "raw",
-                  
-          
-          // General options
-          theme : "advanced",
-          plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
-
-          // Theme options
-          theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
-          theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-          theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-          theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak",
-          theme_advanced_toolbar_location : "top",
-          theme_advanced_toolbar_align : "left",
-          theme_advanced_statusbar_location : "bottom",
-          theme_advanced_resizing : true,
-
-          extended_valid_elements : "hr[class|width|size|noshade]",
-          file_browser_callback : "fileBrowserCallBack",
-          custom_undo_redo_levels : 10,
-          paste_use_dialog : false
-        });
-    }
-  });
+<script type="text/javascript" src="../<?php echo DIR_WS_EDITORS ?>tiny_mce/jquery.tinymce.min.js"></script>
+<script type="text/javascript" src="../<?php echo DIR_WS_EDITORS ?>tiny_mce/tinymce.min.js"></script>
+<script type="text/javascript">
+	
+tinymce.init({
+    selector: "textarea",
+    theme: "modern",
+    language : "de",
+    entity_encoding : "raw",
+    plugins: [
+        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+        "searchreplace wordcount visualblocks visualchars code fullscreen",
+        "insertdatetime media nonbreaking save table contextmenu directionality",
+        "emoticons template paste textcolor"
+    ],
+    toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+    toolbar2: "print preview media | forecolor backcolor emoticons",
+    image_advtab: true,
+    templates: [
+        {title: 'Test template 1', content: 'Test 1'},
+        {title: 'Test template 2', content: 'Test 2'}
+    ]
 });
-//--></script>
+</script>
+
