58bca0351e3c8a654dfcff3b150531531424746a
Jörg Schönberger <office@webchills.at>
2014-04-12 13:24:38 +0000

    1.5.2: Neue Define Page Zahlungsarten, VatAddon bei versandkostenfrei, fehlende Facebook Open Graph Loader

--- UPLOAD/includes/languages/english/extra_definitions/rl.vat_info.php
+++ UPLOAD/includes/languages/english/extra_definitions/rl.vat_info.php
@@ -4,10 +4,12 @@
  * @copyright Copyright 2003-2014 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
- * @version $Id: rl.vat_info.php 656 2012-11-18 17:45:57Z webchills $
+ * @version $Id: rl.vat_info.php 658 2014-04-12 15:45:57Z webchills $
  */
+ 
+define('VAT_SHOW_TEXT_VERSANDKOSTENFREI','<br/><span class="taxAddon">incl. %s VAT</span>');
 
 if(DISPLAY_PRICE_WITH_TAX=='true')
-define('VAT_SHOW_TEXT','<br/><span class="taxAddon">incl. %s VAT.<br/> plus <a href="' . zen_href_link(FILENAME_SHIPPING) . '">shipping and handling</a></span>');
+define('VAT_SHOW_TEXT','<br/><span class="taxAddon">incl. %s VAT<br/> plus <a href="' . zen_href_link(FILENAME_SHIPPING) . '">shipping and handling</a></span>');
 else
-define('VAT_SHOW_TEXT','<br/><span class="taxAddon">plus %s VAT.<br/> plus <a href="' . zen_href_link(FILENAME_SHIPPING) . '">shipping and handling</a></span>');
+define('VAT_SHOW_TEXT','<br/><span class="taxAddon">plus %s VAT<br/> plus <a href="' . zen_href_link(FILENAME_SHIPPING) . '">shipping and handling</a></span>');
---
+++ UPLOAD/includes/templates/template_default/templates/tpl_zahlungsarten_default.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Page Template
+ *
+ * Loaded automatically by index.php?main_page=zahlungsarten
+ * Displays zahlungsarten page.
+ *
+ * @package templateSystem
+ * @copyright Copyright 2003-2014 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
+ * @version $Id: tpl_zahlungsarten_default.php 1 2014-04-12 14:58:36Z hugo13 $
+ */
+
+?>
+<!-- bof tpl_zahlungsarten_default.php -->
+	<div class='centerColumn' id='zahlungsarten'>
+		<h1 id='zahlungsarten-heading'><?php echo HEADING_TITLE; ?></h1>
+		<div id='zahlungsarten-content' class='content'>
+		<?php
+		/**
+		* require the html_define for the zahlungsarten page
+		*/
+		require($define_page);
+		?>
+		</div>
+	</div>
+<!-- eof tpl_zahlungsarten_default.php -->
