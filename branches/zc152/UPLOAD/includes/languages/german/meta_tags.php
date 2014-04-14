060da3f32ce1c78ad142535f5b9746f719bb332d
webchills <webchills@d52c303f-c418-0410-a0d3-be95aab480a1>
2014-04-14 05:50:36 +0000

    1.5.2: Unterstuetzung von <meta name="language" content="xx" />

--- branches/zc152/UPLOAD/includes/languages/german/meta_tags.php
+++ branches/zc152/UPLOAD/includes/languages/german/meta_tags.php
@@ -4,7 +4,7 @@
  * @copyright Copyright 2003-2014 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
- * @version $Id: meta_tags.php 621 2014-02-09 18:48:28Z webchills $
+ * @version $Id: meta_tags.php 622 2014-02-09 18:48:28Z webchills $
  */
 
 // page title
@@ -16,28 +16,31 @@ define('SITE_TAGLINE', 'deutsche Version');
 // Custom Keywords
 define('CUSTOM_KEYWORDS', 'ecommerce, open source, shop, online shopping');
 
-// Home Page Only:
-  define('HOME_PAGE_META_DESCRIPTION', '');
-  define('HOME_PAGE_META_KEYWORDS', '');
+// Language
+define('META_TAG_LANGUAGE', 'de');
 
-  // NOTE: If HOME_PAGE_TITLE is left blank (default) then TITLE and SITE_TAGLINE will be used instead.
-  define('HOME_PAGE_TITLE', ''); // usually best left blank
+// Home Page Only:
+define('HOME_PAGE_META_DESCRIPTION', '');
+define('HOME_PAGE_META_KEYWORDS', '');
+
+// NOTE: If HOME_PAGE_TITLE is left blank (default) then TITLE and SITE_TAGLINE will be used instead.
+define('HOME_PAGE_TITLE', ''); // usually best left blank
 
 
 // EZ-Pages meta-tags.  Follow this pattern for all ez-pages for which you desire custom metatags. Replace the # with ezpage id.
 // If you wish to use defaults for any of the 3 items for a given page, simply do not define it. 
 // (ie: the Title tag is best not set, so that site-wide defaults can be used.)
 // repeat pattern as necessary
-  define('META_TAG_DESCRIPTION_EZPAGE_#','');
-  define('META_TAG_KEYWORDS_EZPAGE_#','');
-  define('META_TAG_TITLE_EZPAGE_#', '');
+define('META_TAG_DESCRIPTION_EZPAGE_#','');
+define('META_TAG_KEYWORDS_EZPAGE_#','');
+define('META_TAG_TITLE_EZPAGE_#', '');
 
 // Per-Page meta-tags. Follow this pattern for individual pages you wish to override. This is useful mainly for additional pages.
 // replace "page_name" with the UPPERCASE name of your main_page= value, such as ABOUT_US or SHIPPINGINFO etc.
 // repeat pattern as necessary
-  define('META_TAG_DESCRIPTION_page_name','');
-  define('META_TAG_KEYWORDS_page_name','');
-  define('META_TAG_TITLE_page_name', '');
+define('META_TAG_DESCRIPTION_page_name','');
+define('META_TAG_KEYWORDS_page_name','');
+define('META_TAG_TITLE_page_name', '');
 
 // Review Page can have a lead in:
 define('META_TAGS_REVIEW', 'Bewertungen: ');
