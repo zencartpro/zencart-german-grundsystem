ebd6fdf31fd889e1980d98d78f854a9570b22dae
webchills <webchills@d52c303f-c418-0410-a0d3-be95aab480a1>
2014-07-31 15:39:17 +0000

    1.5.3: Fehlendes Backup MySQL File, Bugfix Anrede in Bestellbestaetigung, Bugfix Email Archiv Manager fuer eingeschraenkte Admins

--- branches/zc153/UPLOAD/admin/includes/extra_datafiles/email_archive_manager_defines.php
+++ branches/zc153/UPLOAD/admin/includes/extra_datafiles/email_archive_manager_defines.php
@@ -1,3 +1,3 @@
 <?php
-define('FILENAME_EMAIL_HISTORY', 'email_archive_manager.php');
+define('FILENAME_EMAIL_HISTORY', 'email_archive_manager');
 define('BOX_TOOLS_EMAIL_ARCHIVE_MANAGER', 'Email Archiv Manager');
