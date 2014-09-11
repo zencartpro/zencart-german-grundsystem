edcea5af35bffbbe4cebad7fe9935f511b74de54
Jörg Schönberger <office@webchills.at>
2014-09-11 10:42:46 +0000

    Bugfix Iamge Handler

--- UPLOAD/includes/modules/pages/popup_image_additional/header_php.php
+++ UPLOAD/includes/modules/pages/popup_image_additional/header_php.php
@@ -6,7 +6,7 @@
  * @copyright Copyright 2003-2014 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
- * @version $Id: header_php.php 731 2013-01-26 13:49:16Z webchills $
+ * @version $Id: header_php.php 732 2014-09-11 12:41:16Z webchills $
  */
 // This should be first line of the script:
   $zco_notifier->notify('NOTIFY_HEADER_START_POPUP_IMAGES_ADDITIONAL');
@@ -30,12 +30,14 @@
   $products_image = $products_values->fields['products_image'];
 
   $products_image_extension = substr($products_image, strrpos($products_image, '.'));
-  $products_image_base = preg_replace('|'.$products_image_extension.'$|', '', $products_image);
-  $products_image_medium = $products_image_base . IMAGE_SUFFIX_MEDIUM . $products_image_extension;
-  $products_image_large = $products_image_base . IMAGE_SUFFIX_LARGE . $products_image_extension;
-
-  $_GET['products_image_large_additional'] = str_replace(' ', '+', stripslashes($_REQUEST['products_image_large_additional']));
-
-
+//Begin Image Handler changes 1 of 1
+//the next three lines are commented out for Image Handler 3
+//  $products_image_base = str_replace($products_image_extension, '', $products_image);
+//  $products_image_medium = $products_image_base . IMAGE_SUFFIX_MEDIUM . $products_image_extension;
+//  $products_image_large = $products_image_base . IMAGE_SUFFIX_LARGE . $products_image_extension;
+  $products_image_base = preg_replace('/'.$products_image_extension . '$/', '', $products_image);
+  $products_image_medium = DIR_WS_IMAGES . 'medium/' . $products_image_base . IMAGE_SUFFIX_MEDIUM . $products_image_extension;
+  $products_image_large  = DIR_WS_IMAGES . 'large/' . $products_image_base . IMAGE_SUFFIX_LARGE . $products_image_extension;
+//Begin Image Handler changes 1 of 1
   // This should be last line of the script:
   $zco_notifier->notify('NOTIFY_HEADER_END_POPUP_IMAGES_ADDITIONAL');
