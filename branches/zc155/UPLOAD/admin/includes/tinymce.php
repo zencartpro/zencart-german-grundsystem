8c94471a4f6f09f7212ec526ade8a3f2b97cc89a
webchills <webchills@d52c303f-c418-0410-a0d3-be95aab480a1>
2015-12-23 20:10:20 +0000

    1.5.5: Update TinyMCE Editor auf 4.3.2

--- branches/zc155/UPLOAD/admin/includes/tinymce.php
+++ branches/zc155/UPLOAD/admin/includes/tinymce.php
@@ -4,14 +4,14 @@
  * @copyright Copyright 2003-2016 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
- * @version $Id: tinymce.php 758 2015-05-02 12:47:09Z webchills $
+ * @version $Id: tinymce.php 759 2015-12-23 20:47:09Z webchills $
  */
 if (!defined('IS_ADMIN_FLAG')) {
   die('Illegal Access');
 }
 
 ?>
-<script type="text/javascript" src="../<?php echo DIR_WS_EDITORS ?>tiny_mce/jquery-1.11.2.min.js"></script>
+<script type="text/javascript" src="../<?php echo DIR_WS_EDITORS ?>tiny_mce/jquery-1.11.3.min.js"></script>
 <script type="text/javascript" src="../<?php echo DIR_WS_EDITORS ?>tiny_mce/jquery.tinymce.min.js"></script>
 <script type="text/javascript" src="../<?php echo DIR_WS_EDITORS ?>tiny_mce/tinymce.min.js"></script>
 <script type="text/javascript">
