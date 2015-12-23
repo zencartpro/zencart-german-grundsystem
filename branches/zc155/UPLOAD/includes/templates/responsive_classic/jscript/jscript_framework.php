c7476e9391754ddbad4c459233adb83232d2075e
webchills <webchills@d52c303f-c418-0410-a0d3-be95aab480a1>
2015-12-23 21:24:12 +0000

    1.5.5: Templateanpassungen responsive_classic

--- branches/zc155/UPLOAD/includes/templates/responsive_classic/jscript/jscript_framework.php
+++ branches/zc155/UPLOAD/includes/templates/responsive_classic/jscript/jscript_framework.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * @package admin
  * @copyright Copyright 2003-2016 Zen Cart Development Team
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version GIT: $Id: Author: Ian Wilson  New in v1.5.4 $
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
+ * @version $Id: jscript_framework.php 2015-12-23 22:10:39Z webchills $
  */
 ?>
-<script>
+<script type="text/javascript">
 if (typeof zcJS == "undefined" || !zcJS) {
   window.zcJS = { name: 'zcJS', version: '0.1.0.0' };
 };
