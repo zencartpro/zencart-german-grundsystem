ed3347d6df8b52481e118e76455e6ee7c6115071
webchills <webchills@d52c303f-c418-0410-a0d3-be95aab480a1>
2016-02-17 08:01:38 +0000

    1.5.5: Artikelbestand products_low_stock durch erweiterten Lagerbestandbericht (Inventory Report) ersetzt

--- branches/zc155/UPLOAD/admin/includes/languages/english/stats_products_lowstock.php
+++ branches/zc155/UPLOAD/admin/includes/languages/english/stats_products_lowstock.php
@@ -4,11 +4,19 @@
  * @copyright Copyright 2003-2016 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
- * @version $Id: stats_products_lowstock.php 730 2015-12-22 15:49:16Z webchills $
+ * @version $Id: stats_products_lowstock.php 731 2016-02-17 08:49:16Z webchills $
  */
-
-define('HEADING_TITLE', 'Product Stock Report');
-
+ 
+define('HEADING_TITLE', 'Inventory Report');
 define('TABLE_HEADING_NUMBER', 'ID#');
+define('TABLE_HEADING_MODEL', 'Model');
 define('TABLE_HEADING_PRODUCTS', 'Products');
-define('TABLE_HEADING_VIEWED', 'Quantity');
+define('TABLE_HEADING_QUANTITY', 'Qty');
+define('TABLE_HEADING_PRICE', 'Price');
+define('TABLE_HEADING_TOTAL', 'Total');
+define('TABLE_HEADING_MINIMUM_QUANTITY','Minimum Qty per Order');
+define('TABLE_HEADING_MANUFACTURER', 'Manufacturer');
+define('INVENTORY_REPORT_TEXT_CSV', 'Generate CSV');
+define('TABLE_HEADING_MASTERCATEGORY', 'Master Category');
+define('INVENTORY_SHOW_ALL', 'show all');
+define('INVENTORY_SHOW_PAGINATED', 'show paginated');
