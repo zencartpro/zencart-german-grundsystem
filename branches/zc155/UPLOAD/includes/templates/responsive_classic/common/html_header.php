044fc3cbb3d1520574493dff9f9edcd63ee4d5c9
webchills <webchills@d52c303f-c418-0410-a0d3-be95aab480a1>
2016-02-28 15:43:19 +0000

    1.5.5: Bugfixes im responsiven Template, fehlende Sprachdefinitionen im Admin

--- branches/zc155/UPLOAD/includes/templates/responsive_classic/common/html_header.php
+++ branches/zc155/UPLOAD/includes/templates/responsive_classic/common/html_header.php
@@ -8,7 +8,7 @@
  * @copyright Copyright 2003-2016 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
- * @version $Id: html_header.php 4 2016-02-17 12:10:39Z webchills $
+ * @version $Id: html_header.php 5 2016-02-28 16:10:39Z webchills $
  */
 $zco_notifier->notify('NOTIFY_HTML_HEAD_START', $current_page_base, $template_dir);
 
@@ -18,9 +18,12 @@ header('X-Frame-Options:SAMEORIGIN');
  * load the module for generating page meta-tags
  */
 require(DIR_WS_MODULES . zen_get_module_directory('meta_tags.php'));
+/**
+ * output main page HEAD tag and related headers/meta-tags, etc
+ */
 ?>
-
 <?php
+
 // ZCAdditions.com, ZCA Responsive Template Default (BOF-addition 1 of 2)
 if (!class_exists('Mobile_Detect')) {
   include_once(DIR_WS_CLASSES . 'Mobile_Detect.php');
@@ -36,13 +39,13 @@ if (!class_exists('Mobile_Detect')) {
 ?>
 <!DOCTYPE html>
 <html <?php echo HTML_PARAMS; ?>>
-<head>
-<meta charset="<?php echo CHARSET; ?>">
+  <head>
+  <meta charset="<?php echo CHARSET; ?>">
   <title><?php echo META_TAG_TITLE; ?></title>
   <meta name="keywords" content="<?php echo META_TAG_KEYWORDS; ?>" />
   <meta name="description" content="<?php echo META_TAG_DESCRIPTION; ?>" />
   <meta name="author" content="<?php echo STORE_NAME ?>" />
-<meta name="generator" content="Zen-Cart 1.5.5 - deutsche Version, http://www.zen-cart-pro.at" />
+  <meta name="generator" content="Zen-Cart 1.5.5 - deutsche Version, http://www.zen-cart-pro.at" />
 <?php if (defined('ROBOTS_PAGES_TO_SKIP') && in_array($current_page_base,explode(",",constant('ROBOTS_PAGES_TO_SKIP'))) || $current_page_base=='down_for_maintenance' || $robotsNoIndex === true) { ?>
   <meta name="robots" content="noindex, nofollow" />
 <?php } ?>
@@ -50,12 +53,13 @@ if (!class_exists('Mobile_Detect')) {
   <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes"/>
 
 <?php if (defined('FAVICON')) { ?>
-<link rel="icon" href="<?php echo FAVICON; ?>" type="image/x-icon" />
-<link rel="shortcut icon" href="<?php echo FAVICON; ?>" type="image/x-icon" />
+  <link rel="icon" href="<?php echo FAVICON; ?>" type="image/x-icon" />
+  <link rel="shortcut icon" href="<?php echo FAVICON; ?>" type="image/x-icon" />
 <?php } //endif FAVICON ?>
-<base href="<?php echo (($request_type == 'SSL') ? HTTPS_SERVER . DIR_WS_HTTPS_CATALOG : HTTP_SERVER . DIR_WS_CATALOG ); ?>" />
+
+  <base href="<?php echo (($request_type == 'SSL') ? HTTPS_SERVER . DIR_WS_HTTPS_CATALOG : HTTP_SERVER . DIR_WS_CATALOG ); ?>" />
 <?php if (isset($canonicalLink) && $canonicalLink != '') { ?>
-<link rel="canonical" href="<?php echo $canonicalLink; ?>" />
+  <link rel="canonical" href="<?php echo $canonicalLink; ?>" />
 <?php } ?>
 <?php
   // BOF hreflang for multilingual sites
@@ -105,10 +109,10 @@ if($RI_CJLoader->get('status') && (!isset($Ajax) || !$Ajax->status())){
 //DEBUG: echo '<!-- I SEE cat: ' . $current_category_id . ' || vs cpath: ' . $cPath . ' || page: ' . $current_page . ' || template: ' . $current_template . ' || main = ' . ($this_is_home_page ? 'YES' : 'NO') . ' -->';
 ?>
 
-<?php // ZCAdditions.com, ZCA Responsive Template Default (BOF-addition 3 of 3)
-$responsive_mobile = '<link rel="stylesheet" type="text/css" href="' . $template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css') . '/' . 'responsive_mobile.css' . '" /><link rel="stylesheet" type="text/css" href="' . $template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css') . '/' . 'jquery.mmenu.all.css' . '" />'; 
-$responsive_tablet = '<link rel="stylesheet" type="text/css" href="' . $template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css') . '/' . 'responsive_tablet.css' . '" /><link rel="stylesheet" type="text/css" href="' . $template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css') . '/' . 'jquery.mmenu.all.css' . '" />'; 
-$responsive_default = '<link rel="stylesheet" type="text/css" href="' . $template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css') . '/' . 'responsive_default.css' . '" />'; 
+<?php // ZCAdditions.com, ZCA Responsive Template Default (BOF-addition 2 of 2)
+$responsive_mobile = '<link rel="stylesheet" type="text/css" href="' . $template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css') . '/' . 'responsive_mobile.css' . '" /><link rel="stylesheet" type="text/css" href="' . $template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css') . '/' . 'jquery.mmenu.all.css' . '" />';
+$responsive_tablet = '<link rel="stylesheet" type="text/css" href="' . $template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css') . '/' . 'responsive_tablet.css' . '" /><link rel="stylesheet" type="text/css" href="' . $template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css') . '/' . 'jquery.mmenu.all.css' . '" />';
+$responsive_default = '<link rel="stylesheet" type="text/css" href="' . $template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css') . '/' . 'responsive_default.css' . '" />';
 
 if (in_array($current_page_base,explode(",",'popup_image,popup_image_additional')) ) {
   echo '';
