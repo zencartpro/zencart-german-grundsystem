33960f7a436f197d30502cc954396cd7188e578d
webchills <webchills@d52c303f-c418-0410-a0d3-be95aab480a1>
2016-02-29 20:21:51 +0000

    1.5.5: Bestelldetails auf checkout success Seite

--- branches/zc155/UPLOAD/includes/languages/german/checkout_success.php
+++ branches/zc155/UPLOAD/includes/languages/german/checkout_success.php
@@ -4,8 +4,8 @@
  * @copyright Copyright 2003-2016 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
- * @translator: cyaneo/hugo13/wflohr/maleborg	http://www.zen-cart-pro.at	2007-01-03
- * @version $Id: checkout_success.php 627 2010-08-30 15:05:14Z webchills $
+ 
+ * @version $Id: checkout_success.php 628 2016-02-29 15:05:14Z webchills $
  */
 
 define('NAVBAR_TITLE_1','Kasse');
@@ -27,3 +27,26 @@ define('TEXT_YOUR_ORDER_NUMBER', '<strong>Ihre Bestellnummer lautet:</strong> ')
 
 define('TEXT_CHECKOUT_LOGOFF_GUEST', '<p><strong>ANMERKUNG:</strong> Um Ihren Auftrag durchzuführen, wurde ein temporäres Konto erstellt. Sie können dieses Konto schließen, indem Sie auf Abmelden klicken. Das Abmelden stellt auch sicher, daß die Informationen über Ihren Aufenhalt in unserem Shop nicht der nächsten Person sichtbar sind, die diesen Computer verwendet. Wenn Sie mit Ihrem Einkauf fortfahren möchten, denken Sie bitte daran, vor dem Verlassen unseres Shops auf Abmelden zu klicken.</p>');
 define('TEXT_CHECKOUT_LOGOFF_CUSTOMER', 'Vielen Dank für Ihren Einkauf! Bitte melden Sie sich vor Verlassen des Shops ab, um sicherzugehen das Informationen über Ihren Aufenthalt in unserem Shop nicht für die nächste Person sichtbar sind, die diesen Computer verwendet.');
+define('HEADING_ORDER_NUMBER', 'Bestellnummer %s');
+define('HEADING_ORDER_DATE', 'Bestelldatum:');
+define('HEADING_ORDER_TOTAL', 'Gesamtsumme:');
+
+define('HEADING_DELIVERY_ADDRESS', 'Lieferadresse');
+define('HEADING_SHIPPING_METHOD', 'Versandart');
+
+define('HEADING_PRODUCTS', 'Artikel');
+define('HEADING_TAX', 'Steuer');
+define('HEADING_TOTAL', 'Gesamtsumme');
+define('HEADING_QUANTITY', 'Menge');
+
+define('HEADING_BILLING_ADDRESS', 'Rechnungsadresse');
+define('HEADING_PAYMENT_METHOD', 'Zahlungsart');
+
+define('HEADING_ORDER_HISTORY', 'Bestellhistorie &amp; Kommentare');
+define('TEXT_NO_COMMENTS_AVAILABLE', 'keine Kommentare');
+define('TABLE_HEADING_STATUS_DATE', 'Datum');
+define('TABLE_HEADING_STATUS_ORDER_STATUS', 'Bestellstatus');
+define('TABLE_HEADING_STATUS_COMMENTS', 'Kommentare');
+define('QUANTITY_SUFFIX', '&nbsp;  ');
+define('ORDER_HEADING_DIVIDER', '&nbsp;-&nbsp;');
+define('TEXT_OPTION_DIVIDER', '&nbsp;-&nbsp;');
