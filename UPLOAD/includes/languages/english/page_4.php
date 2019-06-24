2dbe67381de1f6d985c99b09ca79216e3379a84d
webchills <office@webchills.com>
2019-06-24 17:55:52 +0200

    Typo in englischem page_4 Sprachfile

--- UPLOAD/includes/languages/english/page_4.php
+++ UPLOAD/includes/languages/english/page_4.php
@@ -1,14 +1,12 @@
 <?php
-<?php
 /**
  * @package languageDefines
- * @copyright Copyright 2003-2016 Zen Cart Development Team
+ * @copyright Copyright 2003-2019 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
- * @version $Id: page_4.php 730 2015-12-22 17:49:16Z webchills $
+ * @version $Id: page_4.php 731 2019-06-24 17:50:16Z webchills $
  */
  
 define('NAVBAR_TITLE', 'Page 4');
 define('HEADING_TITLE', 'Page 4');
-
-define('TEXT_INFORMATION', 'Page 4 information goes here.');
+define('TEXT_INFORMATION', '');
