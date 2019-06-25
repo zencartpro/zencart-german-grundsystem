b28347bfb494324922ec6482cae5813010405a3c
webchills <office@webchills.com>
2019-06-25 08:06:37 +0200

    Versandart Selbstabholung vollständig multilanguage

--- UPLOAD/includes/languages/english/modules/shipping/storepickup.php
+++ UPLOAD/includes/languages/english/modules/shipping/storepickup.php
@@ -1,22 +1,21 @@
 <?php
 /**
  * @package languageDefines
- * @copyright Copyright 2003-2016 Zen Cart Development Team
+ * @copyright Copyright 2003-2019 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
- * @version $Id: storepickup.php 655 2012-02-22 08:45:57Z webchills $
+ * @version $Id: storepickup.php 656 2019-06-25 07:45:57Z webchills $
  */
 
 define('MODULE_SHIPPING_STOREPICKUP_TEXT_TITLE', 'Store Pickup');
 define('MODULE_SHIPPING_STOREPICKUP_TEXT_DESCRIPTION', 'Customer In Store Pick-up');
 define('MODULE_SHIPPING_STOREPICKUP_TEXT_WAY', 'Walk In');
 
-
-// The following is for defining multiple locations/methods on a per-language basis. It is only used if the shopper has selected a language other than the store's default.
-// The content of the MODULE_SHIPPING_STOREPICKUP_MULTIPLE_WAYS definition should be the same as the multiple locations in the shipping module's settings in your admin, just with names changed.
-// Typical formats are:
-// "Location One, 5.00; Location Two, 3.50; Location Three, 0.00"
-// "Location One, Location Two, Location Three"
-// or leave it blank to simply use the same text as defined in the Admin, regardless of language
-// TIP: This should really be left blank for the default language, otherwise the Admin settings field is never used.
+//Hier haben Sie die Möglichkeit, die Ortsangaben für die Abholung je nach Sprache eigens zu definieren. Das Setting in der Administration bei der Selbstabholung greift nur für die Standardsprache des Shops.
+//Beispiele:
+// Demogasse 17 in 1010 Wien;Beispielweg 15 in 8020 Graz<br/>
+// Demogasse 17 in 1010 Wien,4.00;Beispielweg 15 in 8020 Graz,5.00<br/>
+// Demogasse 17 in 1010 Wien,4.00;Beispielweg 15 in 8020 Graz,0.00<br/>
+// Wenn Sie den Eintrag leer lassen, dann werden die unter Module > Versandarten > Selbstabholung eingetragenen Werte verwendet
+// HINWEIS: Wenn Sie nur eine Sprache verwenden, oder keine multilinguale Darstellung dieser Ortsangaben brauchen, dann tragen Sie hier NICHTS ein, es werden sonst nie die Admineinstellungen verwendet. Was Sie hier eintragen überschreibt Ihre Konfiguration im Versandmodul Selbstabholung!
 define('MODULE_SHIPPING_STOREPICKUP_MULTIPLE_WAYS', "");
