bb40e22d63942a824c1718894a7a67017f38b969
webchills <office@webchills.com>
2020-02-10 07:50:51 +0100

    fehlendes ENTRY_TELEPHONE in englischem Sprachfile

--- UPLOAD/includes/languages/english/contact_us.php
+++ UPLOAD/includes/languages/english/contact_us.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * @package languageDefines
- * @copyright Copyright 2003-2019 Zen Cart Development Team
+ * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: contact_us.php 729 2011-08-09 15:49:16Z hugo13 $
+ * @version $Id: contact_us.php 730 2020-02-10 07:49:16Z webchills $
  */
 
 define('HEADING_TITLE', 'Contact Us');
@@ -14,6 +14,7 @@ define('EMAIL_SUBJECT', 'Website Inquiry from ' . STORE_NAME);
 
 define('ENTRY_NAME', 'Full Name:');
 define('ENTRY_EMAIL', 'Email Address:');
+define('ENTRY_TELEPHONE', 'Telephone Number:');
 define('ENTRY_ENQUIRY', 'Message:');
 
 define('SEND_TO_TEXT','Send Email To:');
