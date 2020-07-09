6678e3154e20f46b33ef077960d167791f1951ee
webchills <office@webchills.com>
2020-07-09 11:36:57 +0200

    Neuer Mehrfach Kategorie Manager

--- UPLOAD/admin/includes/languages/german/products_to_categories.php
+++ UPLOAD/admin/includes/languages/german/products_to_categories.php
@@ -1,18 +1,18 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2003-2019 Zen Cart Development Team
+ * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: products_to_categories.php 2019-10-15 11:37:14Z webchills $
+ * @version $Id: products_to_categories.php 2020-07-09 11:37:14Z webchills $
  */
 
-define('HEADING_TITLE', 'Artikel in mehreren Kategorien anzeigen - Link Manager ...');
+define('HEADING_TITLE', 'Artikel in mehreren Kategorien anzeigen');
 define('HEADING_TITLE2', 'Kategorien / Artikel');
 define('TEXT_INFO_PRODUCTS_TO_CATEGORIES_AVAILABLE', 'Kategorien mit verlinkbaren Artikeln ...');
+
 define('TABLE_HEADING_PRODUCTS_ID', 'Artikel ID');
 define('TABLE_HEADING_PRODUCT', 'Artikelname');
-define('TABLE_HEADING_MODEL', 'Bezeichnung');
 define('TABLE_HEADING_ACTION', 'Aktion');
 define('TEXT_INFO_HEADING_EDIT_PRODUCTS_TO_CATEGORIES', 'ARTIKEL ZU KATEGORIE INFORMATIONEN EDITIEREN');
 define('TEXT_PRODUCTS_ID', 'Artikel ID ');
@@ -20,27 +20,30 @@ define('TEXT_PRODUCTS_NAME', 'Artikel: ');
 define('TEXT_PRODUCTS_MODEL', 'Artikelnummer: ');
 define('TEXT_PRODUCTS_PRICE', 'Preis: ');
 define('BUTTON_UPDATE_CATEGORY_LINKS', 'Kategorie Links aktualisieren');
-define('BUTTON_NEW_PRODUCTS_TO_CATEGORIES', 'Artikel verlinken');
-define('TEXT_SET_PRODUCTS_TO_CATEGORIES_LINKS', 'Setze Artikel - Kategorie Links für: ');
+define('BUTTON_NEW_PRODUCTS_TO_CATEGORIES', 'anderen Artikel wählen nach ID#');
+define('BUTTON_CATEGORY_LISTING', 'Kategorie Liste');
+define('TEXT_SET_PRODUCTS_TO_CATEGORIES_LINKS', 'Zeige Artikel - Kategorie Links für: ');
 define('TEXT_INFO_LINKED_TO_COUNT', '&nbsp;&nbsp;Aktuelle Anzahl verlinkter Kategorien: ');
-define('TEXT_INFO_PRODUCTS_TO_CATEGORIES_LINKER_INTRO',
-    'Artikel in mehreren Kategorien anzeigen - Link Manager wurde entwickelt um schnell einen Artikel mit ein oder mehreren anderen Kategorien verlinken zu können.<br />Es können auch alle Artikel einer Kategorie mit einer anderen Kategorie verlinkt werden. Selbstverständlich können bestehende Links mit dieses Tool wieder gelöscht werden. (siehe Information nächster Punkt)');
-define('TEXT_INFO_PRODUCTS_TO_CATEGORIES_LINKER',
-    'Zur Preisberechnung muss jeder Artikel einer Hauptkategorie zugewiesen sein, unabhängig davon mit wievielen anderen Kategorien dieser verlinkt ist. Verwenden Sie dazu das Dropdown Feld "Hauptkategorie".<br />
-Der Artikel ist aktuell folgenden Kategorien zugewiesen (siehe Checkbox). Einfach Checkbox neben Kategorienamen selektieren bzw. deselektiern um Verlinkung hinzu zu fügen bzw. zu löschen.<br />
-Zum Speichern den Button ' . BUTTON_UPDATE_CATEGORY_LINKS . ' drücken.<br />'
-    );
-define('HEADER_CATEGORIES_GLOBAL_CHANGES', 'Globale Kategorie-Link Änderungen und Hauptkategorie-ID Reset');
-define('TEXT_SET_MASTER_CATEGORIES_ID', '<strong>ACHTUNG:</strong> Hauptkategorie-ID änderen bevor verlinkte Kategorien verändert werden!');
+
+define('HEADER_CATEGORIES_GLOBAL_CHANGES', 'Global Category Tools');
+
+define('TEXT_INFO_PRODUCTS_TO_CATEGORIES_LINKER_INTRO', 'This product is currently linked to the categories selected below.<br>To add/remove links, select/deselect the checkboxes as required and then click on the ' . BUTTON_UPDATE_CATEGORY_LINKS . ' button.<br />Further product/category actions are available using the ' . HEADER_CATEGORIES_GLOBAL_CHANGES . ' below.');
+
+define('TEXT_INFO_MASTER_CATEGORY_CHANGE','A product has a Master Category ID (for pricing purposes) that can be considered as the category where the product actually resides. Additionally, a product can be linked (copied) to any number of other categories.<br>The Master Category ID can be changed by using this Master Category dropdown, that offers the currently linked categories as possible alternatives.<br>To set the Master Category ID to <strong>any</strong> category, use the "Move" option on the category listing page.');
+
+define('TEXT_SET_MASTER_CATEGORIES_ID', '<strong>WARNING:</strong> You must set the MASTER CATEGORIES ID before changing Linked Categories');
 
 // copy category to category linked
+define('TEXT_INFO_COPY_ALL_PRODUCTS_TO_CATEGORY_LINKED_HEADING', 'Link (copy) Products from one Category to another Category');
 define('TEXT_INFO_COPY_ALL_PRODUCTS_TO_CATEGORY_LINKED', '<strong>Kopiere ALLE Artikel einer Kategorie als VERLINKTE Artikel in eine andere ...</strong><br />z.B. 8 und 22 bedeutet das ALLE Artikel in Kategorie 8 zu Kategorie 22 verlinkt werden');
 define('TEXT_INFO_COPY_ALL_PRODUCTS_TO_CATEGORY_FROM_LINKED', 'Selektiere ALLE Artikel einer Kategorie: ');
 define('TEXT_INFO_COPY_ALL_PRODUCTS_TO_CATEGORY_TO_LINKED', 'Mit Kategorie verlinken: ');
 define('BUTTON_COPY_CATEGORY_LINKED', 'Kopiere Artikel als verlinke Artikel ');
 define('WARNING_PRODUCTS_LINK_TO_CATEGORY_REMOVED', 'WARNUNG: Artikel wurde zurückgesetzt und ist nicht mehr Teil dieser Kategorie  ...');
-define('WARNING_COPY_LINKED', 'WARNUNG: ');
+
 define('WARNING_COPY_ALL_PRODUCTS_TO_CATEGORY_FROM_LINKED', 'Ungültige Kategorie um Artikel aus Kategorie zu verlinken: ');
+define('WARNING_CATEGORY_TARGET_NOT_EXIST','<strong>TARGET</strong> Category ID#%u invalid (does not exist)');
+define('WARNING_CATEGORY_IDS_DUPLICATED', 'Warning: same Category IDs (#%u)');
 define('WARNING_COPY_ALL_PRODUCTS_TO_CATEGORY_TO_LINKED', 'Ungültige Kategorie um Artikel in Kategorie zu verlinken: ');
 define('WARNING_NO_CATEGORIES_ID', 'WARNUNG: Keine Kategorie ausgewählt ... keine Änderung gemacht');
 define('SUCCESS_COPY_LINKED', 'Erfolgreiche Aktualisierung der verlinkten Artikel ... ');
@@ -50,17 +53,15 @@ define('WARNING_COPY_ALL_PRODUCTS_TO_CATEGORY_TO_LINKED_MISSING', 'WARNUNG: Ung�
 define('WARNING_COPY_FROM_IN_TO_LINKED', '<strong>WARNUNG: Keine Änderungen durchgeführt - Artikel bereits verlinkt ... </strong>');
 
 // remove category to category linked
+define('TEXT_INFO_REMOVE_ALL_PRODUCTS_TO_CATEGORY_LINKED_HEADING', 'Remove Linked Products from a Category');
 define('TEXT_INFO_REMOVE_ALL_PRODUCTS_TO_CATEGORY_LINKED', '<strong>Entferne ALLE VERLINKTEN Artikel einer Kategorie ...</strong><br />z.B. Bei 8 und 22 werden ALLE Artikel-Links zu Kategorie 22 in Kategorie 8 entfernt');
 define('TEXT_INFO_REMOVE_ALL_PRODUCTS_TO_CATEGORY_FROM_LINKED', 'Selektiere Alle Artikel einer Kategorie: ');
 define('TEXT_INFO_REMOVE_ALL_PRODUCTS_TO_CATEGORY_TO_LINKED', 'Entferne Link zu Kategorie: ');
-define('BUTTON_REMOVE_CATEGORY_LINKED', 'Entferne verlinkte Artikel');
-define('WARNING_REMOVE_LINKED', 'WARNUNG: ');
-define('WARNING_REMOVE_ALL_PRODUCTS_TO_CATEGORY_FROM_LINKED', 'Nicht möglich Artikel aus folgender Kategorie zu verlinken: ');
-define('WARNING_REMOVE_ALL_PRODUCTS_TO_CATEGORY_TO_LINKED', 'Nicht möglich Artikel in diese Kategorie zu verlinken: ');
-define('SUCCESS_REMOVE_LINKED', 'Verlinkte Artikel erfolgreich entfernt ... ');
-define('SUCCESS_REMOVE_ALL_PRODUCTS_TO_CATEGORY_FROM_LINKED', 'Verlinkte Artikel aus Kategorie löschen: ');
-define('SUCCESS_REMOVE_ALL_PRODUCTS_TO_CATEGORY_TO_LINKED', 'Verlinkte Artikel in diese Kategorie löschen: ');
-define('WARNING_REMOVE_FROM_IN_TO_LINKED', '<strong>WARNUNG: Keine Änderungen gemacht, keine Artikel verlinkt ... </strong>');
+define('BUTTON_REMOVE_CATEGORY_LINKED', 'Remove Linked Products');
+
+define('SUCCESS_REMOVE_LINKED', '%1$u linked product(s) removed from Category ID#%2$u');
+
+define('WARNING_REMOVE_FROM_IN_TO_LINKED', 'WARNING: No changes made: no products in TARGET Category ID#%1$u are linked from REF Category ID#%2$u');
 define('WARNING_MASTER_CATEGORIES_ID_CONFLICT', '<strong>ACHTUNG: HAUPTKATEGORIE-ID KONFLIKT!! </strong>');
 define('TEXT_INFO_MASTER_CATEGORIES_ID_CONFLICT', '<strong>Hauptkategorie ID ist: </strong>');
 define('TEXT_INFO_MASTER_CATEGORIES_ID_PURPOSE', 'Anmerkung: Die Hauptkategorie wird zur Preisberechnung bei verlinkten Artikeln verwendet. z.B. Sonderangebote <br />');
@@ -74,10 +75,11 @@ define('TEXT_PRODUCTS_ID_NOT_REQUIRED', 'Anmerkung: Eine Artikel-ID wird nicht u
 
 // reset all products to new master_categories_id
 // copy category to category linked
+define('TEXT_INFO_RESET_ALL_PRODUCTS_TO_CATEGORY_MASTER_HEADING', 'Reset the Master Category ID for ALL Products in a Category');
 define('TEXT_INFO_RESET_ALL_PRODUCTS_TO_CATEGORY_MASTER', '<strong>ALLE Artikel in der ausgewählten Kategorie sollen diese als Hauptkategorie verwenden ...</strong><br />z.B: Kategorie 22 zurücksetzen bedeutet, dass ALLE Produkte in Kategorie 22, diese als HauptKategorie-ID verwenden');
 define('TEXT_INFO_RESET_ALL_PRODUCTS_TO_CATEGORY_FROM_MASTER', 'Rücksetzen der Hauptkategorie-ID für ALLE Artikel in Kategorie: ');
 define('BUTTON_RESET_CATEGORY_MASTER', 'Hauptkategorie-ID zurücksetzen');
-define('WARNING_RESET_ALL_PRODUCTS_TO_CATEGORY_FROM_MASTER', 'WARNUNG: Unzulässige Kategorie ausgewählt ...');
+
 define('SUCCESS_RESET_ALL_PRODUCTS_TO_CATEGORY_FROM_MASTER', 'Erfolgreiche Aktualisierung der Hauptkategorie-ID für alle Artikel der Kategorie: ');
 
 define('TEXT_CATEGORIES_NAME', 'Kategorie Name');
