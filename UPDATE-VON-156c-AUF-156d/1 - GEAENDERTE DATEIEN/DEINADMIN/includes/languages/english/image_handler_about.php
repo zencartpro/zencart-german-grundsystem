d3ba2d1a4906d1cd706cc8dd5da319ddb7238331
Jörg Schönberger <office@webchills.at>
2021-12-29 12:56:23 +0100

    1.5.7 beta

--- UPDATE-VON-156c-AUF-156d/1 - GEAENDERTE DATEIEN/DEINADMIN/includes/languages/english/image_handler_about.php
+++
@@ -1,6 +0,0 @@
-<div class="aboutbox">
-    <h2>Image Handler<sup>5</sup> für Zen Cart 1.5.6 deutsch</h2>
-    <p></p>
-    <p>Um sich mit der Funktionsweise vertraut zu machen und Ihre Bilder optimal zu gestalten, lesen Sie bitte die Hinweise zum Bildmanagement in der Onlinedokumentation der deutschen Zen Cart Version:</p>
-    <p><a href="https://www.zen-cart-pro.at/documentation.html#section-7" target="_blank"><b>www.zen-cart-pro.at/documentation.html#section-7</b></a></p>
-</div>
