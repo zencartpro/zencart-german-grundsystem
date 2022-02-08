90bcb37ba29bc43a88014be4645778990f9cfb58
Jörg Schönberger <office@webchills.at>
2022-02-08 08:32:59 +0100

    Neue Übersetzungen für Attributnamen Manager

--- UPLOAD/admin/includes/languages/german/options_name_manager.php
+++ UPLOAD/admin/includes/languages/german/options_name_manager.php
@@ -1,108 +1,108 @@
 <?php
-
- * @copyright Copyright 2003-2023 Zen Cart Development Team
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: option_name_manager.php 2021-12-03 15:58:16Z webchills $
+ * @version $Id: option_name_manager.php 2022-02-08 08:12:16Z webchills $
  */
 
-define('HEADING_TITLE', 'Option Name Manager');
-define('TEXT_ATTRIBUTES_CONTROLLER', 'Attributes Controller');
+define('HEADING_TITLE', 'Attribut Namen');
+define('TEXT_ATTRIBUTES_CONTROLLER', 'Attributmanager');
 
-define('TEXT_WARNING_TEXT_OPTION_NAME_RESTORED', 'Warning: The Option Value TEXT ID#0 was found to be missing from the database table "' . TABLE_PRODUCTS_OPTIONS_VALUES . '". This may have been due to an incorrectly coded plugin.<br>The value has been restored correctly.');
-define('TABLE_HEADING_PRODUCT', 'Product Name');
-define('TABLE_HEADING_OPTION_NAME', 'Option Name');
-define('TABLE_HEADING_OPTION_VALUE', 'Option Value');
-define('TABLE_HEADING_ACTION', 'Action');
+define('TEXT_WARNING_TEXT_OPTION_NAME_RESTORED', 'Warnung: Der Optionswert TEXT ID#0 wurde in der Datenbanktabelle "' . TABLE_PRODUCTS_OPTIONS_VALUES . '" nicht gefunden. Dies könnte auf ein falsch codiertes Plugin zurückzuführen sein.<br> Der Wert wurde korrekt wiederhergestellt.');
+define('TABLE_HEADING_PRODUCT', 'Artikelname');
+define('TABLE_HEADING_OPTION_NAME', 'Attribut Name');
+define('TABLE_HEADING_OPTION_VALUE', 'Attribut Merkmal');
+define('TABLE_HEADING_ACTION', 'Aktion');
 
-define('TEXT_PRODUCT_OPTIONS_INFO','<strong>Note: Edit the Option Name for additional settings</strong>');
+define('TEXT_PRODUCT_OPTIONS_INFO','<strong>Hinweis: Bearbeiten Sie den Optionsnamen für zusätzliche Einstellungen</strong>');
 
-define('TEXT_WARNING_OF_DELETE', 'This Option Name is used by the product(s) listed below: it cannot be deleted until all the Option Values (attributes) associated with this Option Name have been removed from these products (this may be done using the Global Tools below)');
-define('TEXT_OK_TO_DELETE', 'This Option Name is not used by any product - it is safe to delete it.<br><strong>Warning:</strong> this will delete both the Option Name AND all the Option Values associated with that Option Name.');
+define('TEXT_WARNING_OF_DELETE', 'Dieser Attributname wird von dem/den unten aufgeführten Artikel(n) verwendet: Er kann erst gelöscht werden, wenn alle Attributmerkmale, die mit diesem Attributnamen verbunden sind, aus diesen Artikeln entfernt wurden (dies kann mit den unten aufgeführten globalen Tools erledigt werden)');
+define('TEXT_OK_TO_DELETE', 'Dieser Attributname wird von keinem Artikel verwendet - es ist sicher, ihn zu löschen.<br><strong>Warnung:</strong> Dies löscht sowohl den Attributnamen als auch alle Attributmerkmale, die mit diesem Attributnamen verbunden sind.');
 
 define('TEXT_OPTION_ID', 'Option ID');
-define('TEXT_OPTION_NAME', 'Option Name');
+define('TEXT_OPTION_NAME', 'Attribut Name');
 
-define('TEXT_WARNING_DUPLICATE_OPTION_NAME','Option ID#%1$u: Duplicate Option Name Added: "%2$s" (%3$s)');
+define('TEXT_WARNING_DUPLICATE_OPTION_NAME','Option ID#%1$u: Doppelter Attributname hinzugefügt: "%2$s" (%3$s)');
 
-define('TEXT_ORDER_BY','Order by');
-define('TEXT_SORT_ORDER','Sort Order');
+define('TEXT_ORDER_BY','Sortiere nach');
+define('TEXT_SORT_ORDER','Sortierung');
 
-define('TABLE_HEADING_OPTION_TYPE', 'Option Type');
-define('TABLE_HEADING_OPTION_NAME_SIZE','Size');
+define('TABLE_HEADING_OPTION_TYPE', 'Optionstyp');
+define('TABLE_HEADING_OPTION_NAME_SIZE','Größe');
 define('TABLE_HEADING_OPTION_NAME_MAX','Max');
 
-define('TEXT_OPTION_NAME_COMMENTS','Comment (displayed next to Option Name)');
-define('TEXT_OPTION_ATTRIBUTE_IMAGES_PER_ROW', 'Attribute Images per Row');
-define('TEXT_OPTION_ATTRIBUTE_IMAGES_STYLE', 'Attribute Image Layout Style (for Checkbox/Radio Buttons only)');
-define('TEXT_OPTION_ATTRIBUTE_LAYOUTS_EXAMPLE', 'View Examples');
-define('TEXT_OPTION_ATTRIBUTE_IMAGES_STYLE_0', '0 - Selection + text, Images below Options');
-define('TEXT_OPTION_ATTRIBUTE_IMAGES_STYLE_1', '1 - Select + Image + Option inline');
-define('TEXT_OPTION_ATTRIBUTE_IMAGES_STYLE_2', '2 - Select + Option + Image wrapped');
-define('TEXT_OPTION_ATTRIBUTE_IMAGES_STYLE_3', '3 - Select + Image + Option wrapped');
-define('TEXT_OPTION_ATTRIBUTE_IMAGES_STYLE_4', '4 - Image + Option + Select as column');
-define('TEXT_OPTION_ATTRIBUTE_IMAGES_STYLE_5', '5 - Select + Image + Option as column');
+define('TEXT_OPTION_NAME_COMMENTS','Kommentar (angezeigt beim Attributnamen)');
+define('TEXT_OPTION_ATTRIBUTE_IMAGES_PER_ROW', 'Attribut Bilder pro Reihe');
+define('TEXT_OPTION_ATTRIBUTE_IMAGES_STYLE', 'Attribut Bild Layout Stil (nur für Checkbox/Radio Buttons)');
+define('TEXT_OPTION_ATTRIBUTE_LAYOUTS_EXAMPLE', 'Beispiele zeigen');
+define('TEXT_OPTION_ATTRIBUTE_IMAGES_STYLE_0', '0 - Auswahl + Text, Bilder unter den Optionen');
+define('TEXT_OPTION_ATTRIBUTE_IMAGES_STYLE_1', '1 - Auswahl + Bild + Option inline');
+define('TEXT_OPTION_ATTRIBUTE_IMAGES_STYLE_2', '2 - Auswahl + Option + Bild wrapped');
+define('TEXT_OPTION_ATTRIBUTE_IMAGES_STYLE_3', '3 - Auswahl + Bild + Option wrapped');
+define('TEXT_OPTION_ATTRIBUTE_IMAGES_STYLE_4', '4 - Bild + Option + Auswahl als Spalte');
+define('TEXT_OPTION_ATTRIBUTE_IMAGES_STYLE_5', '5 - Auswahl + Bild + Option als Spalte');
 //text attributes only
-define('TEXT_OPTION_NAME_ROWS', 'Rows');
-define('TEXT_OPTION_NAME_SIZE','Display Size');
-define('TEXT_OPTION_NAME_MAX','Maximum Length');
-define('TEXT_OPTION_TYPE_TEXT_ATTRIBUTE_INFO', 'Note: ' . TEXT_OPTION_NAME_ROWS . ', ' . TEXT_OPTION_NAME_SIZE . ' and ' . TEXT_OPTION_NAME_MAX . ' are for Option Name Type "Text" only.');
-define('TEXT_INSERT_NEW_OPTION_NAME', 'Add a new Option Name');
+define('TEXT_OPTION_NAME_ROWS', 'Zeilen');
+define('TEXT_OPTION_NAME_SIZE','Anzeigegröße');
+define('TEXT_OPTION_NAME_MAX','Maximale Länge');
+define('TEXT_OPTION_TYPE_TEXT_ATTRIBUTE_INFO', 'Hinweis: ' . TEXT_OPTION_NAME_ROWS . ', ' . TEXT_OPTION_NAME_SIZE . ' und ' . TEXT_OPTION_NAME_MAX . ' sind nur für den Attributnamen Typ "Text".');
+define('TEXT_INSERT_NEW_OPTION_NAME', 'Neuen Attributnamen hinzufügen');
 
 // Global Tools
-define('TEXT_GLOBAL_TOOLS', 'Global Tools');
-define('TEXT_CLICK_TO_SHOW_HIDE', 'click to show/hide');
-define('TEXT_WARNING_BACKUP', 'Important: Always make a verified backup of your database before making global changes/using Global Tools');
-define('TEXT_SELECT_OPTION_TYPES_ALLOWED', 'Note that Global Tools cannot be used with option name types "Text" or "File".');
-define('TEXT_SELECT_PRODUCT', 'Select a Product');
-define('TEXT_SELECT_CATEGORY', 'Select a Category');
-define('TEXT_SELECT_OPTION', 'Select an Option Name');
+define('TEXT_GLOBAL_TOOLS', 'Globale Tools');
+define('TEXT_CLICK_TO_SHOW_HIDE', 'clicken zum Anzeigen/Ausblenden');
+define('TEXT_WARNING_BACKUP', 'Wichtig: Erstellen Sie immer eine überprüfte Sicherungs Ihrer Datenbank, bevor Sie globale Tools verwenden.');
+define('TEXT_SELECT_OPTION_TYPES_ALLOWED', 'Beachten Sie, dass Globale Tools nicht mit Attributnamen vom Typ "Text" oder "Datei" verwendet werden können.');
+define('TEXT_SELECT_PRODUCT', 'Wählen Sie einen Artikel');
+define('TEXT_SELECT_CATEGORY', 'Wählen Sie eine Kategorie');
+define('TEXT_SELECT_OPTION', 'Wählen Sie einen Attributnamen');
 define('TEXT_NAME', 'Name');
 
 // Add
-define('TEXT_INFO_OPTION_VALUES_ADD', '<strong>Note:</strong> for products that get updated (receive additional Option Values) using the <b>Add</b> tools, the sort order for the Option Values (attributes) will be reset to the <strong>default</strong> sort order for that Option name.');
+define('TEXT_INFO_OPTION_VALUES_ADD', '<strong>Hinweis:</strong> Für Artikel, die mit den <b>Hinzufügen</b> Tools aktualisiert werden (zusätzliche Attributmerkmale erhalten), wird die Sortierreihenfolge für die Attributmerkmale auf die <strong>Standardsortierreihenfolge</strong> für diesen Attributnamen zurückgesetzt.');
 
-define('TEXT_OPTION_VALUE_ADD_ALL', 'Update (add) all remaining Option Values to ALL products that use this Option Name');
-define('TEXT_INFO_OPTION_VALUE_ADD_ALL', 'For ALL products that are using the selected Option Name (and so have at least one Option Value assigned), add ALL the other Option Values associated with the Option Name.');
+define('TEXT_OPTION_VALUE_ADD_ALL', 'Aktualisieren (Hinzufügen) aller verbleibenden Attributmerkmale zu ALLEN Produkten, die diesen Attributnamen verwenden');
+define('TEXT_INFO_OPTION_VALUE_ADD_ALL', 'Für ALLE Artikel, die den ausgewählten Attributnamen verwenden (und denen somit mindestens ein Attributmerkmal zugewiesen ist), ALLE anderen Attributmerkmale hinzufügen, die mit dem Attributnamen verbunden sind');
 
-define('TEXT_OPTION_VALUE_ADD_PRODUCT', 'Update (add) all remaining Option Values to ONE product that is using this Option Name');
-define('TEXT_INFO_OPTION_VALUE_ADD_PRODUCT', 'For a product that is using the selected Option Name (and so has at least one Option Value assigned), add ALL the other Option Values associated with the Option Name.');
+define('TEXT_OPTION_VALUE_ADD_PRODUCT', 'Aktualisieren (Hinzufügen) aller verbleibenden Attributmerkmale zu EINEM Artikel, der diesen Attributnamen verwendet');
+define('TEXT_INFO_OPTION_VALUE_ADD_PRODUCT', 'Für einen Artikel, der den ausgewählten Attributnamen verwendet (und dem somit mindestens ein Attributmerkmal zugewiesen ist), ALLE anderen Attributmerkmale hinzufügen, die mit dem Attributnamen verbunden sind');
 
-define('TEXT_OPTION_VALUE_ADD_CATEGORY', 'Update (add) all remaining Option Values to ALL products in a Category that are using this Option Name');
-define('TEXT_INFO_OPTION_VALUE_ADD_CATEGORY', 'For products in ONE category only that are using the selected Option Name, add ALL the other Option Values associated with the Option Name.');
-define('TEXT_SHOW_CATEGORY_PATH', 'Show category path');
-define('TEXT_SHOW_CATEGORY_NAME', 'Show only category name');
+define('TEXT_OPTION_VALUE_ADD_CATEGORY', 'Aktualisieren (Hinzufügen) aller verbleibenden Attributmerkmale für ALLE Artikel in einer Kategorie, die diesen Attributnamen verwenden');
+define('TEXT_INFO_OPTION_VALUE_ADD_CATEGORY', 'Für Artikel in nur EINER Kategorie, die den ausgewählten Attributnamen verwenden, ALLE anderen Attributmerkmale hinzufügen, die mit dem Attributnamen verbunden sind');
+define('TEXT_SHOW_CATEGORY_PATH', 'Zeige Kategorie Pfad');
+define('TEXT_SHOW_CATEGORY_NAME', 'Zeige nur Kategorie Name');
 
 // messageStack
-define('SUCCESS_PRODUCT_OPTION_VALUE', 'Option Name "%1$s": Option Value "%2$s" added to product "%3$s".');
-define('SUCCESS_PRODUCT_OPTIONS_VALUES_SORT_ORDER', 'Option Name "%1$s": product "%2$s" Option Values updated to the default sort order for Option Name "%1$s".');
-define('SUCCESS_PRODUCTS_OPTIONS_VALUES', 'Option Name "%1$s": %2$u product(s) updated with additional Option Values.');
+define('SUCCESS_PRODUCT_OPTION_VALUE', 'Attributname "%1$s": Attributmerkmal "%2$s" hinzugefügt zu Artikel "%3$s".');
+define('SUCCESS_PRODUCT_OPTIONS_VALUES_SORT_ORDER', 'Attributname "%1$s": Artikel "%2$s" Attributmerkmale aktualisiert mit Standardsortierung für Attributname "%1$s".');
+define('SUCCESS_PRODUCTS_OPTIONS_VALUES', 'Attributname "%1$s": %2$u Artikel aktualisiert mit zusätzlichen Attributmerkmalen.');
 
-define('ERROR_PRODUCTS_OPTIONS_PRODUCTS', 'Warning: No matching product(s) found using Option Name "%s" (nothing was updated).');
-define('ERROR_PRODUCTS_OPTIONS_VALUES', 'Warning: All matching product(s) already have all Option Values for Option Name "%s" (nothing was updated).');
+define('ERROR_PRODUCTS_OPTIONS_PRODUCTS', 'Warnung: Keine passenden Artikel mit dem Attributnamen "%s" gefunden (nichts wurde aktualisiert).');
+define('ERROR_PRODUCTS_OPTIONS_VALUES', 'Warnung: Alle passenden Artikel haben bereits alle Attributmerkmale für Attributname "%s" (nichts wurde aktualisiert).');
 
 // Delete
-define('TEXT_COMMENT_OPTION_VALUE_DELETE_ALL', '<strong>NOTE:</strong> All Option Values will be deleted from matching/the selected product(s). This will not delete the Option Values defined for that Option Name.');
-define('TEXT_OPTION_VALUE_DELETE_ALL', 'Delete all Option Values from ALL products using this Option Name');
-define('TEXT_INFO_OPTION_VALUE_DELETE_ALL', 'For ALL products that are using the selected Option Name, remove all the Option Values/the Option Name.');
+define('TEXT_COMMENT_OPTION_VALUE_DELETE_ALL', '<strong>Hinweis:</strong> Alle Attributmerkmale werden für den/die ausgewählte(n) Artikel gelöscht. Dies löscht nicht die Attributmerkmale selbst, die für diesen Attributnamen definiert wurden.');
+define('TEXT_OPTION_VALUE_DELETE_ALL', 'Alle Attributmerkmale aus ALLEN Artikeln löschen, die diesen Attributnamen verwenden');
+define('TEXT_INFO_OPTION_VALUE_DELETE_ALL', 'Für ALLE Artikel, die den ausgewählten Attributnamen verwenden, alle Attributmerkmale/Attributnamen entfernen');
 
-define('TEXT_OPTION_VALUE_DELETE_PRODUCT', 'Delete all Option Values from ONE product using this Option Name');
-define('TEXT_INFO_OPTION_VALUE_DELETE_PRODUCT', 'For a product that is using the selected Option Name, remove ALL the Option Values/the Option Name.');
+define('TEXT_OPTION_VALUE_DELETE_PRODUCT', 'Alle Attributmerkmale aus EINEM Artikel mit diesem Attributnamen löschen');
+define('TEXT_INFO_OPTION_VALUE_DELETE_PRODUCT', 'Für einen Artikel, der den ausgewählten Attributnamen verwendet, ALLE Attributmerkmale/Attributnamen entfernen');
 
-define('TEXT_OPTION_VALUE_DELETE_CATEGORY', 'Delete all Option Values from ONE Category of products for this Option Name');
-define('TEXT_INFO_OPTION_VALUE_DELETE_CATEGORY', 'For products in ONE category only that are using the selected Option Name, remove all the Option Values/the Option Name.');
+define('TEXT_OPTION_VALUE_DELETE_CATEGORY', 'Alle Attributmerkmale aus EINER Kategorie von Artikel für diesen Attributnamen löschen');
+define('TEXT_INFO_OPTION_VALUE_DELETE_CATEGORY', 'Für Artikel in nur EINER Kategorie, die den ausgewählten Attributnamen verwenden, ALLE Attributmerkmale/Attributnamen entfernen');
 
 // messageStack
-define('SUCCESS_PRODUCT_OPTION_VALUES_DELETED', 'Option Name "%1$s": all Option Values deleted from product "%2$s".');
-define('SUCCESS_PRODUCTS_OPTIONS_VALUES_DELETED', 'Option Name "%1$s": all Option Values removed from %2$u product(s).');
+define('SUCCESS_PRODUCT_OPTION_VALUES_DELETED', 'Attributname "%1$s": alle Attributmerkmale entfernt von Artikel "%2$s".');
+define('SUCCESS_PRODUCTS_OPTIONS_VALUES_DELETED', 'Attributname "%1$s": alle Attributmerkmale entfernt von %2$u Artikel(n).');
 
 // Copy
-define('TEXT_OPTION_VALUE_COPY_ALL', 'Copy all Option Values to another Option Name');
-define('TEXT_INFO_OPTION_VALUE_COPY_ALL', 'All Option Values from the selected Option Name will be copied (added) to another Option Name.');
-define('TEXT_SELECT_OPTION_FROM', 'Copy from Option Name: ');
-define('TEXT_SELECT_OPTION_TO', 'Copy to Option Name: ');
+define('TEXT_OPTION_VALUE_COPY_ALL', 'Kopiere alle Attributmerkmale zu einem anderen Attributnamen');
+define('TEXT_INFO_OPTION_VALUE_COPY_ALL', 'Alle Attributmerkmale des ausgewählten Attributnamens werden zu einem anderen Attributnamen kopiert (hinzugefügt).');
+define('TEXT_SELECT_OPTION_FROM', 'Kopiere von Attributname: ');
+define('TEXT_SELECT_OPTION_TO', 'Kopiere zu Attributname: ');
 
-define('SUCCESS_OPTION_VALUE_COPIED', 'Option Value "%6$s" ID#%5$u copied from Option Name "%2$s" ID#%1$u to Option Name "%4$s" ID#%3$u.');
-define('SUCCESS_OPTION_VALUES_COPIED', '%5$u Option Value(s) copied from Option Name "%2$s" ID#%1$u to Option Name "%4$s" ID#%3$u.');
-define('ERROR_OPTION_VALUES_COPIED', 'Error: Cannot copy Option Values to the same Option Name ("%2$s" ID#%1$u to "%4$s" ID#%3$u)!');
-define('ERROR_OPTION_VALUES_NONE', 'Error: Option Name "%2$s" ID#%1$u has no Option Values defined (nothing to copy)!');
+define('SUCCESS_OPTION_VALUE_COPIED', 'Attributmerkmal "%6$s" ID#%5$u kopiert von Attributname "%2$s" ID#%1$u zu Attributname "%4$s" ID#%3$u.');
+define('SUCCESS_OPTION_VALUES_COPIED', '%5$u Attributmerkmal(e) kopiert von Attributname "%2$s" ID#%1$u zu Attributname "%4$s" ID#%3$u.');
+define('ERROR_OPTION_VALUES_COPIED', 'Fehler: Kann Attributmerkmale nicht um selben Attributnamen kopieren ("%2$s" ID#%1$u zu "%4$s" ID#%3$u)!');
+define('ERROR_OPTION_VALUES_NONE', 'Fehler: Attributname "%2$s" ID#%1$u hat keine Attributmerkmale definiert (es gibt nichts zu kopieren)!');
