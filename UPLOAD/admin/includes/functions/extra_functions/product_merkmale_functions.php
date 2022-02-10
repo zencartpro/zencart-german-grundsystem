1dd22b8a2e3bbd16cbb96985e4d3188e8481ac60
Jörg Schönberger <office@webchills.at>
2022-02-10 22:13:23 +0100

    product_merkmale_functions modernisiert

--- UPLOAD/admin/includes/functions/extra_functions/product_merkmale_functions.php
+++ UPLOAD/admin/includes/functions/extra_functions/product_merkmale_functions.php
@@ -1,24 +1,21 @@
 <?php
 /**
- * @package admin
+ * Zen Cart German Specific
  * @copyright Copyright 2003-2022 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: product_merkmale_functions.php 2012-06-13 19:53:47 webchills $
+ * @version $Id: product_merkmale_functions.php 2022-02-10 22:05:47 webchills $
 */
 
-   function zen_get_products_merkmale($product_id, $language = '') {
-    global $db;
+   function zen_get_products_merkmale($product_id, $language_id) {
+    global $db;    
 
-    if (empty($language)) $language = $_SESSION['languages_id'];
-
-    $product_query = "select products_merkmale
+    $merkmale = $db->Execute("select products_merkmale
                       from " . TABLE_PRODUCTS_DESCRIPTION . "
                       where products_id = '" . (int)$product_id . "'
-                      and language_id = '" . (int)$language . "'";
-
-    $product = $db->Execute($product_query);
-
-    return $product->fields['products_merkmale'];
-  }
+                      and language_id = '" . (int)$language_id . "'");
+		      
+    return $merkmale->fields['products_merkmale'];
+   } 
+ 
