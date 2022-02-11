91529129b3af9209660d98c21c3d1bd39be67983
Jörg Schönberger <office@webchills.at>
2022-02-11 19:03:24 +0100

    veraltetes ot_netto aktualisiert

--- UPLOAD/admin/includes/languages/german/store_manager.php
+++ UPLOAD/admin/includes/languages/german/store_manager.php
@@ -1,12 +1,12 @@
 <?php
 /**
 
- * @copyright Copyright 2003-2022 Zen Cart Development Team
- * Zen Cart German Version - www.zen-cart-pro.at
- * @copyright Portions Copyright 2003 osCommerce
- * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: store_manager.php 2021-12-03 15:36:31Z webchills $
- */
+* @copyright Copyright 2003-2022 Zen Cart Development Team
+* Zen Cart German Version - www.zen-cart-pro.at
+* @copyright Portions Copyright 2003 osCommerce
+* @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
+* @version $Id: store_manager.php 2021-12-03 15:36:31Z webchills $
+*/
 define('HEADING_TITLE', 'Shopmanager');
 define('TABLE_CONFIGURATION_TABLE', 'Suche KONSTANTE Definitionen');
 
@@ -26,18 +26,15 @@ define('TEXT_INFO_PRODUCTS_VIEWED_UPDATE', '<strong>Setze ALLE meist gesehenen A
 define('TEXT_INFO_PRODUCTS_ORDERED_UPDATE', '<strong>ALLE meist gekauften Artikel zurücksetzen</strong><br />Anzahl bestellter Artikel auf 0 setzen: ');
 define('TEXT_INFO_MASTER_CATEGORIES_ID_UPDATE', '<strong>Setze ALLE Artikelhauptkategorie IDs</strong><br />zur Verwendung für verlinkte Artikel und Bepreisung zurück: ');
 
-  define('TEXT_NEW_ORDERS_ID', 'Neue Bestellnummer');
-  define('TEXT_INFO_SET_NEXT_ORDER_NUMBER', '<strong>Legen Sie hier Ihre nächste Bestellnummer fest</strong><br />HINWEIS: Ihre neue Bestellnummer muss größer sein als die bereits bestehenden Bestellungen in Ihrer Datenbank.');
-  define('TEXT_MSG_NEXT_ORDER', 'Ihre nächste Bestellnummer wurde ist %s');
-  define('TEXT_MSG_NEXT_ORDER_MAX', 'Basierend auf den bestehenden Bestellungen ist Ihre nächste Bestellnummer momentan: %s');
-  define('TEXT_MSG_NEXT_ORDER_TOO_LARGE', 'Aufgrund des Datenbanklimits können Sie keine Bestellnummer höher als 2000000000 einstellen. Bitte wählen Sie einen kleineren Wert.');
+define('TEXT_NEW_ORDERS_ID', 'Neue Bestellnummer');
+define('TEXT_INFO_SET_NEXT_ORDER_NUMBER', '<strong>Legen Sie hier Ihre nächste Bestellnummer fest</strong><br />HINWEIS: Ihre neue Bestellnummer muss größer sein als die bereits bestehenden Bestellungen in Ihrer Datenbank.');
+define('TEXT_MSG_NEXT_ORDER', 'Ihre nächste Bestellnummer wurde ist %s');
+define('TEXT_MSG_NEXT_ORDER_MAX', 'Basierend auf den bestehenden Bestellungen ist Ihre nächste Bestellnummer momentan: %s');
+define('TEXT_MSG_NEXT_ORDER_TOO_LARGE', 'Aufgrund des Datenbanklimits können Sie keine Bestellnummer höher als 2000000000 einstellen. Bitte wählen Sie einen kleineren Wert.');
 
 define('TEXT_CONFIGURATION_CONSTANT', '<strong>Suche KONSTANTE oder Sprachdateidefinitionen</strong>');
 define('TEXT_CONFIGURATION_KEY', 'Schlüssel oder Name:');
 define('TEXT_INFO_CONFIGURATION_UPDATE', '<strong>HINWEIS:</strong> KONSTANTEN sind immer in Großbuchstaben geschrieben.<br />Die Suche in Sprachdateien kann eine Alternative sein, wenn in den Datenbanktabellen nichts gefunden wurde.');
-
-
-
 
 define('TABLE_TITLE_KEY', '<strong>Schlüssel:</strong>');
 define('TABLE_TITLE_TITLE', '<strong>Titel:</strong>');
@@ -54,7 +51,7 @@ define('TEXT_LANGUAGE_LOOKUP_CURRENT_ADMIN', 'Alle Hauptsprachdateien - Admin ('
 define('TEXT_LANGUAGE_LOOKUP_CURRENT_ADMIN_LANGUAGE', 'Alle derzeit ausgewählten Sprachdateien - Admin (' . DIR_WS_ADMIN . DIR_WS_LANGUAGES . $_SESSION['language'] . '/*.php)');
 define('TEXT_LANGUAGE_LOOKUP_CURRENT_ALL', 'Alle derzeit ausgewählten Sprachdateien - Webshop/Admin');
 
-define('TEXT_INFO_NO_EDIT_AVAILABLE', 'Keine Bearbeitung erhältlich');
+define('TEXT_INFO_NO_EDIT_AVAILABLE', 'Keine Bearbeitung verfügbar');
 define('TEXT_INFO_CONFIGURATION_HIDDEN', ' oder VERSTECKT');
 
 define('TEXT_INFO_DATABASE_OPTIMIZE', '<strong>Datenbank optimieren</strong> um vergeudeten Raum von gelöschten Aufzeichnungen zu entfernen.<br>Das kann monatlich oder wöchentlich auf einer frequentierten Datenbank erforderlich sein.<br>(Am besten während der nichtaktiven Zeiten die Optimierung laufen lassen.)');
--- UPLOAD/includes/modules/order_total/ot_netto.php
+++ UPLOAD/includes/modules/order_total/ot_netto.php
@@ -1,38 +1,46 @@
 <?php
 /**
  * ot_netto order-total module
- * Zen Cart German Specific
- * @package orderTotal
+ * Zen Cart German Specific 
  * @copyright Copyright 2003-2022 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: ot_netto.php 2022-02-10 09:10:16Z webchills $
+ * @version $Id: ot_netto.php 2022-02-11 18:46:16Z webchills $
  */
+class ot_netto extends base
+{
+    public    $code,
+              $title,
+              $description,
+              $sort_order,
+              $output;
+    protected $_check;
 
-  class ot_netto {
-    var $title, $output;
-
-    function __construct() {
+    public function __construct() 
+    {
+      global $order, $currencies;
       $this->code = 'ot_netto';
       $this->title = MODULE_ORDER_TOTAL_NETTO_TITLE;
       $this->description = MODULE_ORDER_TOTAL_NETTO_DESCRIPTION; 
       $this->enabled = (defined('MODULE_ORDER_TOTAL_NETTO_STATUS') && MODULE_ORDER_TOTAL_NETTO_STATUS == 'True'); 
-      $this->sort_order = defined('MODULE_ORDER_TOTAL_NETTO_SORT_ORDER') ? MODULE_ORDER_TOTAL_NETTO_SORT_ORDER : null;
-
+      $this->sort_order = defined('MODULE_ORDER_TOTAL_NETTO_SORT_ORDER') ? (int)MODULE_ORDER_TOTAL_NETTO_SORT_ORDER : null;
+        if (null === $this->sort_order) {
+            return false;
+        }
       $this->output = array();
     }
 
-    function process() {
+    public function process() 
+    {
       global $order, $currencies;
       $Tax_total = 0; 
 
       reset($order->info['tax_groups']);
-      while (list($key, $value) = each($order->info['tax_groups'])) {
+      foreach($order->info['tax_groups'] as $key => $value) {      
         // sum all tax values to calculate total tax:
         if ($value > 0) $Tax_total += $value;
       }
-      
       // subtract total tax from total invoice amount to calculate net amount:
       $Netto = $order->info['total']-$Tax_total;
       
@@ -42,21 +50,25 @@
                         'value' => $Netto);
     }
 
-    function check() {
+    public function check() 
+    {
     global $db;
       if (!isset($this->_check)) {
-        $check_query = $db->Execute("select configuration_value from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_ORDER_TOTAL_NETTO_STATUS'");
+            $check_query = $db->Execute("SELECT configuration_value FROM " . TABLE_CONFIGURATION . " WHERE configuration_key = 'MODULE_ORDER_TOTAL_NETTO_STATUS' LIMIT 1");
         $this->_check = $check_query->RecordCount();
       }
 
       return $this->_check;
     }
 
-    function keys() {
+    public function keys() 
+    {
+
       return array('MODULE_ORDER_TOTAL_NETTO_STATUS', 'MODULE_ORDER_TOTAL_NETTO_SORT_ORDER');
     }
 
-    function install() {
+    public function install() 
+    {
     global $db;
       $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Display net amount?', 'MODULE_ORDER_TOTAL_NETTO_STATUS', 'true', 'Do you want to display the net amount?', '6', '1','zen_cfg_select_option(array(\'true\', \'false\'), ', now())");
       $db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_ORDER_TOTAL_NETTO_SORT_ORDER', '299', 'Sort order of display.', '6', '6', now())");
@@ -65,7 +77,8 @@
       $db->Execute("insert into " . TABLE_CONFIGURATION_LANGUAGE   . " (configuration_title, configuration_key, configuration_language_id, configuration_description, date_added) values ('Anzeigereihenfolge', 'MODULE_ORDER_TOTAL_NETTO_SORT_ORDER', '43', 'Anzeigereihenfolge', now())");
     }
 
-    function remove() {
+    public function remove() 
+    {
     global $db;
       $db->Execute("delete from " . TABLE_CONFIGURATION . " where configuration_key in ('" . implode("', '", $this->keys()) . "')");
        // www.zen-cart-pro.at german admin languages_id == delete all
