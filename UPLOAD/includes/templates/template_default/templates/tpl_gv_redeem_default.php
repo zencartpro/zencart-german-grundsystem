ffbf45ed4d395a71e2405177afe1c1ad6088d3f1
Jörg Schönberger <office@webchills.at>
2022-03-25 09:55:13 +0100

    removed unused $_GET['goback']

--- UPLOAD/includes/templates/template_default/templates/tpl_gv_redeem_default.php
+++ UPLOAD/includes/templates/template_default/templates/tpl_gv_redeem_default.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Page Template
- *
+ * Zen Cart German Specific
  * Display information related to GV redemption (could be redemption details, or an error message)
  *
  * @package templateSystem
@@ -9,7 +9,7 @@
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: tpl_gv_redeem_default.php 2011-08-09 15:49:16Z hugo13 $
+ * @version $Id: tpl_gv_redeem_default.php 2022-03-25 09:47:16Z webchills $
  */
 ?>
 <div class="centerColumn" id="gvRedeemDefault">
@@ -20,6 +20,6 @@
 
 <div id="gvRedeemDefaultMainContent" class="content"><?php echo TEXT_INFORMATION; ?></div>
 
-<div class="buttonRow forward"><?php echo '<a href="' . ($_GET['goback'] == 'true' ? zen_href_link(FILENAME_GV_FAQ) : zen_href_link(FILENAME_DEFAULT)) . '">' . zen_image_button(BUTTON_IMAGE_CONTINUE, BUTTON_CONTINUE_ALT) . '</a>'; ?></div>
+<div class="buttonRow forward"><?php echo '<a href="' . zen_href_link(FILENAME_DEFAULT) . '">' . zen_image_button(BUTTON_IMAGE_CONTINUE, BUTTON_CONTINUE_ALT) . '</a>'; ?></div>
 
 </div>
