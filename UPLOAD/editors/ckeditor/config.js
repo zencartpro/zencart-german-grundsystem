8733e53cf67e2c4ab3a2fedc101426e74fc5c31c
Jörg Schönberger <office@webchills.at>
2022-04-27 21:13:42 +0200

    prevent ckeditor from converting umlauts

--- UPLOAD/editors/ckeditor/config.js
+++ UPLOAD/editors/ckeditor/config.js
@@ -1,6 +1,7 @@
 /**
- * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2022, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ * modified for Zen Cart German
  */
 
 CKEDITOR.editorConfig = function( config ) {
@@ -9,4 +10,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.uiColor = '#AADC6E';
 	// we want to allow custom classes
 	config.extraAllowedContent = '*(*)';
+	// we want utf-8
+	config.entities_latin = false;
+	config.entities = false;
 };
