89bcbe66ffd1029df471376da61874e6aa86aa5b
Jörg Schönberger <office@webchills.at>
2022-12-08 18:58:43 +0100

    Update PHPMailer to 6.7.1

--- UPLOAD/includes/languages/german/responsive_classic/header.php
+++ UPLOAD/includes/languages/german/responsive_classic/header.php
@@ -5,8 +5,7 @@
  * @copyright Copyright 2003-2022 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
- * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- 
+ * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0 
  * @version $Id: header.php 2022-03-11 16:43:14Z webchills $
  */
 
