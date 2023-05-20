f66a9a53541d3d4d765a199bd6ae6b59799df531
Jörg Schönberger <office@webchills.at>
2023-05-20 08:55:48 +0200

    Update Font Awesome to 6.4.0

--- UPLOAD/includes/templates/responsive_classic/common/html_header.php
+++ UPLOAD/includes/templates/responsive_classic/common/html_header.php
@@ -9,7 +9,7 @@
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: html_header.php 2023-05-15 16:22:39Z webchills $
+ * @version $Id: html_header.php 2023-05-20 08:22:39Z webchills $
  */
 if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
@@ -152,7 +152,7 @@ if (in_array($current_page_base,explode(",",'popup_image,popup_image_additional'
 }
 ?>
   <script type="text/javascript">document.documentElement.className = 'no-fouc';</script>
-  <link rel="stylesheet" href="extras/fontawesome/6.2.1/css/all.css" />
+  <link rel="stylesheet" href="extras/fontawesome/6.4.0/css/all.css" />
 <?php
   $zco_notifier->notify('NOTIFY_HTML_HEAD_END', $current_page_base);
 ?>
