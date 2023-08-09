1f20a62d53df788141fb76aa742ca94be2951b30
Jörg Schönberger <office@webchills.at>
2023-08-09 08:03:23 +0200

    Update manufacturers.php

--- UPLOAD/admin/includes/languages/german/manufacturers.php
+++ UPLOAD/admin/includes/languages/german/manufacturers.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Zen Cart German Specific
- * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Copyright 2003-2023 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: manufacturers.php 2022-03-02 19:52:16Z webchills $
+ * @version $Id: manufacturers.php 2023-07-03 09:52:16Z webchills $
  */
 
 define('HEADING_TITLE','Hersteller');
