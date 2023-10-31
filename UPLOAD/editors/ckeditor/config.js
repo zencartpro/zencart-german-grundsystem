7bdb3b89972fa19455eccaeb786488a27452ebca
Jörg Schönberger <office@webchills.at>
2023-10-31 16:17:09 +0100

    Update CKEditor to 4.22.1

--- UPLOAD/editors/ckeditor/config.js
+++ UPLOAD/editors/ckeditor/config.js
@@ -1,16 +1,22 @@
 /**
  * @license Copyright (c) 2003-2023, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
- * modified for Zen Cart German
+ * modified for Zen Cart German - 2023-10-31 webchills
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
+	// For complete reference see:
+	// https://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-	config.height = 400;
+	// By default, the editor width equals the width of its container element in the page, while its height is set to 200 pixels.
+	//config.width = '100%';
+	config.height = 200;
+	// if you prefer 400px set to 400 instead
+	//config.height = 400;
 
 
-	config.uiColor = '#D8D6CD'; //#D8D6CD
+	config.uiColor = '#D8D6CD';
+
 	// The toolbar button arrangement, optimized for two toolbar rows.
 	config.toolbar = [
 		{ name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
@@ -38,15 +44,13 @@ CKEDITOR.editorConfig = function( config ) {
 	config.fontSize_sizes =  '8/8px;9/9px;10/10px;11/11px;12/12px;13/13px;14/14px;15/15px;16/16px;17/17px;18/18px;19/19px;20/20px;22/22px;24/24px;26/26px;28/28px;36/36px;48/48px;72/72px';
 
 
-	// Set the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
-	// we want to allow custom classes
-config.allowedContent = true;
+
+  config.allowedContent = true;
 	config.extraAllowedContent = '*(*)';
 	// we want utf-8
 	config.entities_latin = false;
-	config.entities = false;
-  config.skin = 'moono'; // kama, moono, moono-lisa
+	config.entities = false;  
+
 };
 CKEDITOR.on( 'instanceReady', function( ev ) {
     ev.editor.dataProcessor.writer.selfClosingEnd = '>';
