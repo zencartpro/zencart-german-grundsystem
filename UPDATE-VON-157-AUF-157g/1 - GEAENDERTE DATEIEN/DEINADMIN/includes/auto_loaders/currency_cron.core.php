bfa63c275b693552dcdadc2d33741faffb457a2c
Jörg Schönberger <office@webchills.at>
2024-01-12 14:31:30 +0100

    Load functions/admin_access for currency_cron

--- UPDATE-VON-157-AUF-157g/1 - GEAENDERTE DATEIEN/DEINADMIN/includes/auto_loaders/currency_cron.core.php
+++ UPDATE-VON-157-AUF-157g/1 - GEAENDERTE DATEIEN/DEINADMIN/includes/auto_loaders/currency_cron.core.php
@@ -1,52 +1,86 @@
 <?php
 /**
  * Zen Cart German Specific (158 code in 157)
- * @copyright Copyright 2003-2023 Zen Cart Development Team
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: currency_cron.core.php 2023-10-23 17:12:36Z webchills $
+ * @version $Id: currency_cron.core.php 2024-01-12 14:29:36Z webchills $
  */
-if (!defined('USE_PCONNECT')) define('USE_PCONNECT', 'false');
+if (!defined('USE_PCONNECT')) {
+    define('USE_PCONNECT', 'false');
+}
 /**
  * autoloader array for admin currency_cron.php
  */
-  $autoLoadConfig[0][] = array('autoType'=>'require',
-                               'loadFile'=> DIR_FS_CATALOG . DIR_WS_INCLUDES .  'version.php');
-  $autoLoadConfig[0][] = array('autoType'=>'class',
-                               'loadFile'=>'class.notifier.php');
-  $autoLoadConfig[0][] = array('autoType'=>'classInstantiate',
-                               'className'=>'notifier',
-                               'objectName'=>'zco_notifier');
-  $autoLoadConfig[0][] = array('autoType'=>'class',
-                               'loadFile'=>'sniffer.php');
-  $autoLoadConfig[0][] = array('autoType'=>'class',
-                               'loadFile'=>'object_info.php',
-                               'classPath'=>DIR_WS_CLASSES);
-  $autoLoadConfig[20][] = array('autoType'=>'init_script',
-                                'loadFile'=> 'init_db_config_read.php');
-  $autoLoadConfig[30][] = array('autoType'=>'classInstantiate',
-                                'className'=>'sniffer',
-                                'objectName'=>'sniffer');
-  $autoLoadConfig[40][] = array('autoType'=>'init_script',
-                                'loadFile'=> 'init_general_funcs.php');
-  $autoLoadConfig[60][] = array('autoType'=>'require',
-                                'loadFile'=> DIR_FS_CATALOG . DIR_WS_FUNCTIONS . 'sessions.php');
-  $autoLoadConfig[70][] = array('autoType'=>'init_script',
-                                'loadFile'=> 'init_languages.php');
-  $autoLoadConfig[90][] = array('autoType'=>'require',
-                                'loadFile'=> DIR_FS_CATALOG . DIR_WS_FUNCTIONS . 'functions_exchange_rates.php');
-  $autoLoadConfig[120][] = array('autoType'=>'init_script',
-                                 'loadFile'=> 'init_special_funcs.php');
-//   $autoLoadConfig[140][] = array('autoType'=>'init_script',
-//                                  'loadFile'=> 'init_errors.php');
-  $autoLoadConfig[170][] = array('autoType'=>'init_script',
-                                 'loadFile'=> 'init_admin_history.php');
+$autoLoadConfig[0][] = [
+    'autoType' => 'require',
+    'loadFile' => DIR_FS_CATALOG . DIR_WS_INCLUDES . 'version.php',
+];
+$autoLoadConfig[0][] = [
+    'autoType' => 'class',
+    'loadFile' => 'class.notifier.php',
+];
+$autoLoadConfig[0][] = [
+    'autoType' => 'classInstantiate',
+    'className' => 'notifier',
+    'objectName' => 'zco_notifier',
+];
+$autoLoadConfig[0][] = [
+    'autoType' => 'class',
+    'loadFile' => 'sniffer.php',
+];
+$autoLoadConfig[0][] = [
+    'autoType' => 'class',
+    'loadFile' => 'object_info.php',
+    'classPath' => DIR_WS_CLASSES,
+];
+$autoLoadConfig[20][] = [
+    'autoType' => 'init_script',
+    'loadFile' => 'init_db_config_read.php',
+];
+$autoLoadConfig[30][] = [
+    'autoType' => 'classInstantiate',
+    'className' => 'sniffer',
+    'objectName' => 'sniffer',
+];
+$autoLoadConfig[35][] = [
+    'autoType' => 'require',
+    'loadFile' => DIR_WS_FUNCTIONS . 'admin_access.php',
+];
+$autoLoadConfig[40][] = [
+    'autoType' => 'init_script',
+    'loadFile' => 'init_general_funcs.php',
+];
+$autoLoadConfig[60][] = [
+    'autoType' => 'require',
+    'loadFile' => DIR_FS_CATALOG . DIR_WS_FUNCTIONS . 'sessions.php',
+];
+$autoLoadConfig[65][] = [
+    'autoType' => 'init_script',
+    'loadFile' => 'init_languages.php',
+];
+$autoLoadConfig[90][] = [
+    'autoType' => 'require',
+    'loadFile' => DIR_FS_CATALOG . DIR_WS_FUNCTIONS . 'functions_exchange_rates.php',
+];
+$autoLoadConfig[120][] = [
+    'autoType' => 'init_script',
+    'loadFile' => 'init_special_funcs.php',
+];
+$autoLoadConfig[170][] = [
+    'autoType' => 'init_script',
+    'loadFile' => 'init_admin_history.php',
+];
 
-  $autoLoadConfig[1][] = array('autoType'=>'class',
-                               'loadFile'=>'class.admin.zcObserverLogEventListener.php',
-                               'classPath'=>DIR_WS_CLASSES);
-  $autoLoadConfig[40][] = array('autoType'=>'classInstantiate',
-                               'className'=>'zcObserverLogEventListener',
-                               'objectName'=>'zcObserverLogEventListener');
+$autoLoadConfig[1][] = [
+    'autoType' => 'class',
+    'loadFile' => 'class.admin.zcObserverLogEventListener.php',
+    'classPath' => DIR_WS_CLASSES,
+];
+$autoLoadConfig[40][] = [
+    'autoType' => 'classInstantiate',
+    'className' => 'zcObserverLogEventListener',
+    'objectName' => 'zcObserverLogEventListener',
+];
 
