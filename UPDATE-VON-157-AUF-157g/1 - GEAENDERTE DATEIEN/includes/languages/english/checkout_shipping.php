be4c86631798b0dea27c3e249ebec0700f16da15
Jörg Schönberger <office@webchills.at>
2024-01-21 11:50:49 +0100

    Refine language in checkout_shipping when no methods available

--- UPDATE-VON-157-AUF-157g/1 - GEAENDERTE DATEIEN/includes/languages/english/checkout_shipping.php
+++ UPDATE-VON-157-AUF-157g/1 - GEAENDERTE DATEIEN/includes/languages/english/checkout_shipping.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Zen Cart German Specific (158 code in 157)
- * @copyright Copyright 2003-2023 Zen Cart Development Team
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: checkout_shipping.php 2023-10-29 08:49:16Z webchills $
+ * @version $Id: checkout_shipping.php 2024-01-21 11:45:16Z webchills $
  */
 
 define('NAVBAR_TITLE_1', 'Checkout - Step 1');
@@ -14,15 +14,15 @@ define('NAVBAR_TITLE_2', 'Shipping Method');
 define('HEADING_TITLE', 'Step 1 of 3 - Delivery Information');
 
 
-define('TEXT_CHOOSE_SHIPPING_DESTINATION', 'Your order will be shipped to the address at the left or you may change the shipping address by clicking the <em>Change Address</em> button.');
+define('TEXT_CHOOSE_SHIPPING_DESTINATION', 'Your order will be shipped to the address shown or you may change the shipping address by clicking the <em>Change Address</em> button.');
 define('TITLE_SHIPPING_ADDRESS', 'Delivery Information:');
 
 
 define('TEXT_CHOOSE_SHIPPING_METHOD', 'Please select the preferred shipping method to use on this order.');
 define('TITLE_PLEASE_SELECT', 'Please Select');
-define('TEXT_ENTER_SHIPPING_INFORMATION', 'This is currently the only shipping method available to use on this order.');
-define('TITLE_NO_SHIPPING_AVAILABLE', 'Not Available At This Time');
-define('TEXT_NO_SHIPPING_AVAILABLE','<span class="alert">Sorry, we are not shipping to your region at this time.</span><br>Please contact us for alternate arrangements.');
+define('TEXT_ENTER_SHIPPING_INFORMATION', 'This is currently the only shipping method we have available for this order and this shipping address.');
+define('TITLE_NO_SHIPPING_AVAILABLE', 'No Online Shipping Methods For This Address And Your Cart');
+define('TEXT_NO_SHIPPING_AVAILABLE','<span class="alert">Sorry, we have no online options for shipping this order to the address shown.</span><br>Please contact us for alternate arrangements.');
 
 
 
--- UPLOAD/includes/languages/german/checkout_shipping.php
+++ UPLOAD/includes/languages/german/checkout_shipping.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Zen Cart German Specific (158 code in 157)
- * @copyright Copyright 2003-2023 Zen Cart Development Team
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: checkout_shipping.php 2023-10-29 08:49:16Z webchills $
+ * @version $Id: checkout_shipping.php 2024-01-21 11:49:16Z webchills $
  */
 
 define('NAVBAR_TITLE_1','Bestellung');
@@ -14,15 +14,15 @@ define('NAVBAR_TITLE_2','Versandart wählen');
 define('HEADING_TITLE','Schritt 1 von 3 : Lieferinformationen');
 
 
-define('TEXT_CHOOSE_SHIPPING_DESTINATION','Ihre Bestellung wird an die links angezeigte Anschrift geliefert. Sie können die Lieferanschrift ändern, wenn Sie auf den Button <em>Adresse ändern</em> klicken.');
+define('TEXT_CHOOSE_SHIPPING_DESTINATION','Ihre Bestellung wird an die angezeigte Anschrift geliefert. Sie können die Lieferanschrift ändern, wenn Sie auf den Button <em>Adresse ändern</em> klicken.');
 define('TITLE_SHIPPING_ADDRESS','Lieferanschrift:');
 
 
 define('TEXT_CHOOSE_SHIPPING_METHOD','Bitte wählen Sie die Versandart für Ihre Bestellung.');
 define('TITLE_PLEASE_SELECT','Bitte wählen Sie');
-define('TEXT_ENTER_SHIPPING_INFORMATION','Dies ist zur Zeit die einzige Versandart.');
-define('TITLE_NO_SHIPPING_AVAILABLE', 'Zur Zeit nicht verfügbar');
-define('TEXT_NO_SHIPPING_AVAILABLE','<span class="alert">Entschuldigung, aber wir können nicht in Ihre Region versenden .</span><br>Bitte setzen Sie sich mit uns in Verbindung, um Alternativen zu suchen.');
+define('TEXT_ENTER_SHIPPING_INFORMATION','Dies ist zur Zeit die einzige Versandart, die wir für eine Bestellung an diese Lieferadresse anbieten können.');
+define('TITLE_NO_SHIPPING_AVAILABLE', 'Für diese Adresse und diesen Warenkorbinhalt haben wir derzeit keine Versandarten verfügbar.');
+define('TEXT_NO_SHIPPING_AVAILABLE','<span class="alert">Entschuldigung, aber an diese Adresse können wir derzeit nicht versenden .</span><br>Bitte setzen Sie sich mit uns in Verbindung, um Alternativen zu suchen.');
 
 
 
