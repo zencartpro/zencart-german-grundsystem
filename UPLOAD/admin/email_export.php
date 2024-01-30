11bad3bb4cde57503dd3b2270dea42d12efb0f5c
Jörg Schönberger <office@webchills.at>
2024-01-30 20:03:31 +0100

    use admin_html_head in email_export

--- UPLOAD/admin/email_export.php
+++ UPLOAD/admin/email_export.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Zen Cart German Specific
- * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: email_export.php 2022-04-17 16:52:17Z webchills $
+ * @version $Id: email_export.php 2024-01-30 20:01:17Z webchills $
  */
 
 require('includes/application_top.php');
@@ -219,25 +219,11 @@ if (!defined('DIR_FS_EMAIL_EXPORT')) define('DIR_FS_EMAIL_EXPORT',DIR_FS_CATALOG
 <!doctype html>
 <html <?php echo HTML_PARAMS; ?>>
   <head>
-    <meta charset="<?php echo CHARSET; ?>">
-    <title><?php echo TITLE; ?></title>
-    <link rel="stylesheet" href="includes/stylesheet.css">
-    <link rel="stylesheet" href="includes/cssjsmenuhover.css" media="all" id="hoverJS">
-    <script src="includes/menu.js"></script>
-    <script src="includes/general.js"></script>
+    <?php require DIR_WS_INCLUDES . 'admin_html_head.php'; ?>
 
-    <script>
-      function init() {
-          cssjsmenu('navbar');
-          if (document.getElementById) {
-              var kill = document.getElementById('hoverJS');
-              kill.disabled = true;
-          }
-      }
-    </script>
     
   </head>
-<body onLoad="init()" >
+<body>
       <!-- header //-->
       <?php require(DIR_WS_INCLUDES . 'header.php'); ?>
       <!-- header_eof //-->
@@ -267,10 +253,9 @@ if (!defined('DIR_FS_EMAIL_EXPORT')) define('DIR_FS_EMAIL_EXPORT',DIR_FS_CATALOG
           
     </form>
  <!-- body_text_eof //-->
-      </div>
-      <!-- body_eof //-->
-      <!-- footer //-->
-  <?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
-      <!-- footer_eof //-->
-    </body>
-  </html>
+</div>
+<!-- body_eof //-->
+<?php require DIR_WS_INCLUDES . 'footer.php'; ?>
+</body>
+</html>
+<?php require DIR_WS_INCLUDES . 'application_bottom.php'; ?>
