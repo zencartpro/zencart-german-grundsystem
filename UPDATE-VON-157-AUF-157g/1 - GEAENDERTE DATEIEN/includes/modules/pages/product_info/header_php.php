f12e69e40ba33d9da80e53b5097427aa31f92015
Jörg Schönberger <office@webchills.at>
2024-02-01 11:51:07 +0100

    Force product_info pages to use proper product-type-handler page

---
+++ UPDATE-VON-157-AUF-157g/1 - GEAENDERTE DATEIEN/includes/modules/pages/product_info/header_php.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * product_info header_php.php
+ *
+ * Zen Cart German Specific (158 code in 157)
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
+ * Zen Cart German Version - www.zen-cart-pro.at
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
+ * @version $Id: header_php.php 2024-02-01 11:46:16Z webchills $
+ */
+
+// This should be first line of the script:
+$zco_notifier->notify('NOTIFY_HEADER_START_PRODUCT_INFO');
+
+require(DIR_WS_MODULES . zen_get_module_directory('require_languages.php'));
+
+$product_info = zen_get_product_details($products_id_current = (!empty($_GET['products_id']) ? (int)$_GET['products_id'] : 0));
+if ($current_page !== $product_info->fields['type_handler'] . '_info') {
+    zen_redirect(zen_href_link($product_info->fields['type_handler'] . '_info', zen_get_all_get_params()));
+}
+
+zen_product_set_header_response($products_id_current, $product_info);
+
+// ensure navigation snapshot is set in order to "go back" in case must-be-logged-in-for-price is enabled
+if (!zen_is_logged_in()) {
+    $_SESSION['navigation']->set_snapshot();
+}
+
+// This should be last line of the script:
+$zco_notifier->notify('NOTIFY_HEADER_END_PRODUCT_INFO');
--- UPLOAD/includes/modules/pages/document_product_info/header_php.php
+++ UPLOAD/includes/modules/pages/document_product_info/header_php.php
@@ -2,12 +2,12 @@
 /**
  * document_product header_php.php 
  *
- 
- * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * Zen Cart German Specific (158 code in 157)
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: header_php.php 2021-11-29 15:23:16Z webchills $
+ * @version $Id: header_php.php 2024-02-01 11:44:16Z webchills $
  */
 
 // This should be first line of the script:
@@ -16,6 +16,9 @@ $zco_notifier->notify('NOTIFY_HEADER_START_DOCUMENT_PRODUCT_INFO');
 require(DIR_WS_MODULES . zen_get_module_directory('require_languages.php'));
 
 $product_info = zen_get_product_details($products_id_current = (!empty($_GET['products_id']) ? (int)$_GET['products_id'] : 0));
+if ($current_page !== $product_info->fields['type_handler'] . '_info') {
+    zen_redirect(zen_href_link($product_info->fields['type_handler'] . '_info', zen_get_all_get_params()));
+}
 
 zen_product_set_header_response($products_id_current, $product_info);
 
