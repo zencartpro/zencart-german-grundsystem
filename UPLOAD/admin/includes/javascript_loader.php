f64db8b52e51a7861915ea0b75abbf9f0eb42872
Jörg Schönberger <office@webchills.at>
2024-02-16 16:42:33 +0100

    back_to_top button added to admin section

--- UPLOAD/admin/includes/javascript_loader.php
+++ UPLOAD/admin/includes/javascript_loader.php
@@ -3,11 +3,11 @@
  * This file is inserted at the start of the body tag, just above the header menu, and loads most of the admin javascript components
  *
  * Zen Cart German Specific (158 code in 157 / zencartpro adaptations)
- * @copyright Copyright 2003-2023 Zen Cart Development Team
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: javascript_loader.php 2023-12-12 19:27:32Z webchills $
+ * @version $Id: javascript_loader.php 2024-02-16 16:18:32Z webchills $
  */
 ?>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha384-1H217gwSVyLSIfaLxHbE7dRb3v4mYCKbpQvzx0cegeju1MVsGrX5xXxAvs/HgeFs" crossorigin="anonymous"></script>
@@ -15,7 +15,7 @@
 <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js" integrity="sha256-lSjKY0/srUM9BE3dPm+c4fBo1dky2v27Gdjm2uoZaL0=" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
 <!--<script src="includes/javascript/bootstrap.min.js"></script>-->
-
+<script src="includes/javascript/backtotop.js"></script>
 <script src="includes/javascript/datepicker-de.js"></script>
 <script>
 // init datepicker defaults with localization
