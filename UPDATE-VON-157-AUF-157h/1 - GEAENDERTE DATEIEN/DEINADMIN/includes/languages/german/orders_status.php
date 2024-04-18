3dd81d38f2fcb50129582bd9d701e6ace8283c93
Jörg Schönberger <office@webchills.at>
2024-04-18 13:26:23 +0200

    Fix small typo in german orders_status language file

--- UPDATE-VON-157-AUF-157h/1 - GEAENDERTE DATEIEN/DEINADMIN/includes/languages/german/orders_status.php
+++ UPDATE-VON-157-AUF-157h/1 - GEAENDERTE DATEIEN/DEINADMIN/includes/languages/german/orders_status.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Zen Cart German Specific (158 code in 157)
- * @copyright Copyright 2003-2023 Zen Cart Development Team
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: orders_status.php 2023-10-28 18:49:16Z webchills $
+ * @version $Id: orders_status.php 2024-04-18 13:23:16Z webchills $
  */
 
 define('HEADING_TITLE','Bestellstatus');
@@ -13,8 +13,7 @@ define('TABLE_HEADING_ORDERS_STATUS','Bestellstatus');
 
 define('TABLE_HEADING_ORDERS_STATUS_ID', 'Bestellstatus ID');
 
-
-define('TEXT_INFO_SORT_ORDER', 'Sortierungr: ');
+define('TEXT_INFO_SORT_ORDER', 'Sortierreihenfolge:');
 
 define('TEXT_INFO_ORDERS_STATUS_NAME','Bestellstatus:');
 define('TEXT_INFO_INSERT_INTRO','Tragen Sie bitte den neuen Bestellstatus mit den dazu notwendigen Daten ein');
