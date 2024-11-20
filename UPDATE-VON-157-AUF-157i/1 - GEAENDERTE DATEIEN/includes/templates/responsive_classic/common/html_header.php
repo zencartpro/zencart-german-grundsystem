3a115fb5a4929270aae1b01ca37fea2f732c4c6a
Jörg Schönberger <office@webchills.at>
2024-11-20 12:39:20 +0100

    Font Awesome updated to 6.7.0

--- UPDATE-VON-157-AUF-157i/1 - GEAENDERTE DATEIEN/includes/templates/responsive_classic/common/html_header.php
+++ UPDATE-VON-157-AUF-157i/1 - GEAENDERTE DATEIEN/includes/templates/responsive_classic/common/html_header.php
@@ -5,11 +5,11 @@
  *
  * outputs the html header. i,e, everything that comes before the \</head\> tag
  * 
- * @copyright Copyright 2003-2023 Zen Cart Development Team
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: html_header.php 2023-11-18 08:22:39Z webchills $
+ * @version $Id: html_header.php 2024-11-20 12:25:39Z webchills $
  */
 if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
@@ -159,7 +159,7 @@ if (in_array($current_page_base,explode(",",'popup_image,popup_image_additional'
 }
 ?>
   <script type="text/javascript">document.documentElement.className = 'no-fouc';</script>
-  <link rel="stylesheet" href="extras/fontawesome/6.4.0/css/all.css" />
+  <link rel="stylesheet" href="extras/fontawesome/6.7.0/css/all.css" />
 <?php
   $zco_notifier->notify('NOTIFY_HTML_HEAD_END', $current_page_base);
 ?>
