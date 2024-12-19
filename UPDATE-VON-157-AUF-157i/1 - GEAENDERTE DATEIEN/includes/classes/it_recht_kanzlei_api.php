ec27bf0259e1abb28d7cad50492388eb5cca1cbf
Jörg Schönberger <office@webchills.at>
2024-12-19 21:47:42 +0100

    temp fix for deprecated logs for it_recht_kanzlei_api
    has to be rewritten in 2.1.0

--- UPDATE-VON-157-AUF-157i/1 - GEAENDERTE DATEIEN/includes/classes/it_recht_kanzlei_api.php
+++ UPDATE-VON-157-AUF-157i/1 - GEAENDERTE DATEIEN/includes/classes/it_recht_kanzlei_api.php
@@ -1,17 +1,17 @@
 <?php
 /**
  * @package IT Recht Kanzlei
- * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: it_recht_kanzlei_api.php 2022-10-14 17:30:51Z webchills $
+ * @version $Id: it_recht_kanzlei_api.php 2024-12-19 21:44:51Z webchills $
  */
 
 if (!defined('IS_ADMIN_FLAG')) {
   die('Illegal Access');
 }
-
+#[\AllowDynamicProperties]
 class it_recht_kanzlei {
   
   public $modulversion = IT_RECHT_KANZLEI_MODUL_VERSION;
