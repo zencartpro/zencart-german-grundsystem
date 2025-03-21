183368cd2154125f5456da3595094cf016b2db7e
Jörg Schönberger <office@webchills.at>
2025-03-21 18:58:00 +0100

    version stamping 1.5.7j

--- UPLOAD/zc_install/sql/install/mysql_zencart.sql
+++ UPLOAD/zc_install/sql/install/mysql_zencart.sql
@@ -2,11 +2,11 @@
 # * Zen Cart German Specific (158 code in 157 / zencartpro adaptations)
 # * Main Zen Cart SQL Load for MySQL databases
 # * @access private
-# * @copyright Copyright 2003-2024 Zen Cart Development Team
+# * @copyright Copyright 2003-2025 Zen Cart Development Team
 # * Zen Cart German Version - www.zen-cart-pro.at
 # * @copyright Portions Copyright 2003 osCommerce
 # * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
-# * @version $Id: mysql_zencart.sql 2024-08-16 14:55:16Z webchills $
+# * @version $Id: mysql_zencart.sql 2025-03-21 18:55:16Z webchills $
 #
 
 ############ IMPORTANT INSTRUCTIONS ###############
@@ -5369,5 +5369,5 @@ INSERT INTO configuration_language (configuration_title, configuration_key, conf
 
 
 REPLACE INTO product_type_layout_language (configuration_title , configuration_key , languages_id, configuration_description, last_modified, date_added)
-VALUES ('20240818', 'LANGUAGE_VERSION', '43', 'Datum der deutschen Übersetzungen', now(), now());
+VALUES ('20250323', 'LANGUAGE_VERSION', '43', 'Datum der deutschen Übersetzungen', now(), now());
 ##### End of SQL setup for Zen Cart German.
