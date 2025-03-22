168e7faf08940bcb47627f65574f826c62d6d61e
Jörg Schönberger <office@webchills.at>
2025-03-22 13:57:32 +0100

    info files updated for 1.5.7j

--- UPDATE-VON-157i-AUF-157j/3 - DATENBANKAKTUALISIERUNG/DEINADMIN/includes/auto_loaders/config.157j_update.php
+++ UPDATE-VON-157i-AUF-157j/3 - DATENBANKAKTUALISIERUNG/DEINADMIN/includes/auto_loaders/config.157j_update.php
@@ -1,16 +1,16 @@
 <?php
 /**
 * !!!! Nach erfolgreicher Aktualisierung, löschen Sie diese Datei sofort wieder vom Server !!!!!
-* @copyright Copyright 2003-2024 Zen Cart Development Team
+* @copyright Copyright 2003-2025 Zen Cart Development Team
 * @copyright Portions Copyright 2003 osCommerce
 * Zen Cart German Version - www.zen-cart-pro.at
 * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
-* @version $Id: config.157i_update.php 2024-08-17 09:28:51Z webchills $
+* @version $Id: config.157j_update.php 2025-03-22 13:28:51Z webchills $
 */
 if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 } 
 $autoLoadConfig[200][] = array(
     'autoType'  => 'init_script',
-    'loadFile'  => 'init_157i_update.php'
+    'loadFile'  => 'init_157j_update.php'
 );
