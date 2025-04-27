6226e4f6292e20ef4aa8be8dd156c9e129a6cac3
Jörg Schönberger <office@webchills.at>
2025-04-27 11:00:17 +0200

    fix typo in 157j update package

--- UPDATE-VON-157-AUF-157j/3 - DATENBANKAKTUALISIERUNG/DEINADMIN/includes/auto_loaders/config.157j_update.php
+++ UPDATE-VON-157-AUF-157j/3 - DATENBANKAKTUALISIERUNG/DEINADMIN/includes/auto_loaders/config.157j_update.php
@@ -5,12 +5,12 @@
 * @copyright Portions Copyright 2003 osCommerce
 * Zen Cart German Version - www.zen-cart-pro.at
 * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
-* @version $Id: config.157i_update.php 2024-08-18 07:21:51Z webchills $
+* @version $Id: config.157j_update.php 2024-08-18 07:21:51Z webchills $
 */
 if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 } 
 $autoLoadConfig[200][] = array(
     'autoType'  => 'init_script',
-    'loadFile'  => 'init_157i_update.php'
+    'loadFile'  => 'init_157j_update.php'
 );
