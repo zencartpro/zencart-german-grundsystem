f51f0136cd1e72813c23aff017b8d56363af95aa
Jörg Schönberger <office@webchills.at>
2026-04-03 09:34:05 +0200

    Update CKEditor to 5

--- UPLOAD/admin/includes/ckeditor.php
+++ UPLOAD/admin/includes/ckeditor.php
@@ -1,52 +1,261 @@
 <?php
 /**
+ * CKEditor 5
+ *
+ * Custom config can be set up in /editors/ckeditor5/config.js
+ *
  * Zen Cart German Specific (zencartpro adaptations)
- * @copyright Copyright 2003-2023 Zen Cart Development Team
+ * @copyright Copyright 2003-2026 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2010 Kuroi Web Design
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: ckeditor.php 2023-10-31 10:11:32Z webchills $
+ * @version $Id: ckeditor.php 2026-03-04 07:36:32Z webchills $
+ *
+ * @var language $lng
+ *
+ * Ref: https://ckeditor.com/docs/ckeditor5/latest/getting-started/installation/quick-start.html#installing-ckeditor-5-from-cdn
+ * Ref: https://github.com/ckeditor/ckeditor5/releases
  */
+
+// To use a newer version, set the number here. See https://github.com/ckeditor/ckeditor5/releases for latest.
+const CKEDITOR_VERSION = '43.3.1';
+
 if (!defined('IS_ADMIN_FLAG')) {
-  die('Illegal Access');
+    die('Illegal Access');
 }
-// prepare list of languages supported by this website, so we can tell CKEditor
-$var = zen_get_languages();
-$jsLanguageLookupArray = "var lang = new Array;\n";
-foreach ($var as $key)
-{
-  $jsLanguageLookupArray .= "        lang[" . $key['id'] . "] = '" . $key['code'] . "';\n";
+
+// for compatibility with pre-ZC-v2.0.0 where class is PSR-autoloaded
+if (!isset($lng)) {
+    if (!class_exists('language')) {
+        include(DIR_FS_CATALOG . DIR_WS_CLASSES . 'language.php');
+    }
+    $lng = new language;
+}
+// Get an array of languages: [1=>'en', 2=>'fr', etc] to match up textarea ID suffix to know which language the editor should use for that field.
+if (method_exists($lng, 'get_language_list')) {
+    $langArray = $lng->get_language_list();
+} else {
+    // fallback for compatibility with pre-ZC-v2.0.0
+    $langArray = [];
+    foreach ($lng->catalog_languages as $lang) {
+        $langArray[$lang['id']] = $lang['code'];
+    }
 }
 ?>
 
-<script src="../<?php echo DIR_WS_EDITORS ?>ckeditor/ckeditor.js"></script>
+<link title="CKEditorCSS" rel="stylesheet" href="https://cdn.ckeditor.com/ckeditor5/<?= CKEDITOR_VERSION ?>/ckeditor5.css">
+<style>
+    /* for Bootstrap compatibility to prevent interfering with tables */
+    .ck-content .table {
+        width: auto;
+    }
+</style>
+<script title="CKEditor5CDN" type="importmap">
+    {
+        "imports": {
+            "ckeditor5": "https://cdn.ckeditor.com/ckeditor5/<?= CKEDITOR_VERSION ?>/ckeditor5.js",
+            "ckeditor5/": "https://cdn.ckeditor.com/ckeditor5/<?= CKEDITOR_VERSION ?>/"
+        }
+    }
+</script>
+<script title="CKEditor-custom-config" src="<?= (function_exists('zen_catalog_base_link') ? zen_catalog_base_link() : '/') . DIR_WS_EDITORS . 'ckeditor5/config.js' ?>"></script>
+<script title="CKEditor-initialize" type="module">
+    const langArray = <?= json_encode($langArray) ?>;
+    const defaultLang = '<?= DEFAULT_LANGUAGE ?>';
+    const sessionLangId = '<?= $_SESSION['languages_id'] ?>';
+    const sessionLangCode = '<?= $_SESSION['languages_code'] ?>';
 
-<script title="ckEditor-Initialize">
-    jQuery(document).ready(function() {
-        <?php echo $jsLanguageLookupArray; ?>
-        // Activate on every textarea field that has the editorHook class and does not have the noEditor class
-        jQuery('textarea.editorHook').each(function() {
-            if (!jQuery(this).hasClass('noEditor'))
-            {
-                // handle multi-language variants of fields
-                index = jQuery(this).attr('name').match(/\d+/);
-                if (index == null) index = <?php echo $_SESSION['languages_id'] ?>;
+    // Get list of textarea fields that have the editorHook class and do not have the noEditor class
+    const editorElements = document.querySelectorAll('.editorHook:not(.noEditor)');
 
-                CKEDITOR.replace(jQuery(this).attr('name'),
-                    {
-                        customConfig: '<?php echo (function_exists('zen_catalog_base_link') ? zen_catalog_base_link() : '/') . DIR_WS_EDITORS . 'ckeditor/config.js'; ?>',
-                        language: lang[index]
-                    });
+    import {
+        Alignment,
+        Autoformat,
+        AutoImage,
+        AutoLink,
+        BlockQuote,
+        Bold,
+        ClassicEditor,
+        Clipboard,
+        Code,
+        CodeBlock,
+        Essentials,
+        FindAndReplace,
+        Font,
+        GeneralHtmlSupport,
+        Heading,
+        HorizontalLine,
+        HtmlEmbed,
+        Image,
+        ImageCaption,
+        ImageInsert,
+        ImageResize,
+        ImageStyle,
+        ImageToolbar,
+        Indent,
+        IndentBlock,
+        Italic,
+        LinkImage,
+        List,
+        MediaEmbed,
+        Paragraph,
+        PasteFromMarkdownExperimental,
+        PasteFromOffice,
+        RemoveFormat,
+        SelectAll,
+        ShowBlocks,
+        SourceEditing,
+        SpecialCharacters,
+        SpecialCharactersEssentials,
+        Strikethrough,
+        Style,
+        Subscript,
+        Superscript,
+        Table,
+        TableCellProperties,
+        TableColumnResize,
+        TableProperties,
+        TableToolbar,
+        TextPartLanguage,
+        Underline,
+        Undo
+    } from 'ckeditor5';
+
+    <?php
+    // import translations needed
+    foreach ($langArray as $langCode) {
+        echo "import " . $langCode . "Translation from 'ckeditor5/translations/" . $langCode . ".js';\n";
+    }
+    ?>
+    const uiLanguages = {
+        <?php
+    foreach ($langArray as $langCode) {
+        echo '"' . $langCode . '": ' . $langCode . 'Translation,';
+    }
+    ?>
+    };
+
+
+    const editorConfig = {
+        plugins: [
+            Essentials, Font, Bold, Italic, Underline, Strikethrough, Subscript, Superscript, Code,
+            Clipboard, PasteFromOffice, Autoformat, PasteFromMarkdownExperimental, FindAndReplace,
+            CodeBlock, Heading, Paragraph, Undo, BlockQuote, Indent, IndentBlock, List, SelectAll,
+            HorizontalLine,
+            Alignment, GeneralHtmlSupport, HtmlEmbed, Style, SourceEditing, MediaEmbed, TextPartLanguage, ShowBlocks,
+            SpecialCharacters, SpecialCharactersEssentials, RemoveFormat,
+            Table, TableToolbar, TableProperties, TableCellProperties, TableColumnResize,
+            Image, AutoImage, ImageInsert, ImageToolbar, ImageCaption, ImageStyle, ImageResize, LinkImage
+        ],
+        toolbar: {
+            items: [
+                'undo', 'redo', 'findAndReplace',
+                '|', 'link', 'insertImage', 'mediaEmbed', 'insertTable',
+                '|', 'heading', 'fontsize', /* 'fontfamily', */ 'specialCharacters',
+                '|', 'showBlocks',
+                // 'sourceEditing' may or may not be desired. Do not use if admins might paste content from untrusted sources
+                'sourceEditing',
+                // using htmlEmbed poses a security risk: do not use if Admins might paste content from untrusted sources; A sanitizer should be configured if htmlEmbed is enabled.
+                // 'htmlEmbed',
+                '-', 'bold', 'italic', 'underline', 'strikethrough',
+                // 'superscript', 'subscript',
+                'code',
+                '|', 'removeFormat',
+                '|', 'numberedList', 'bulletedList', 'indent', 'outdent', 'blockQuote', 'alignment', 'horizontalLine',
+                '|', 'fontColor', 'fontBackgroundColor',
+            ],
+            shouldNotGroupWhenFull: true
+        },
+
+        image: {
+            insert: {
+                integrations: ['url'/*, 'upload', 'assetManager'*/],
+
+                // If "type" setting is omitted, the editor defaults to 'block'.
+                type: 'auto'
+            },
+            // image toolbar layout:
+            toolbar: [
+                'imageStyle:block',
+                'imageStyle:side',
+                '|',
+                'toggleImageCaption',
+                'imageTextAlternative',
+                '|',
+                'linkImage',
+                '|',
+                'imageStyle:inline',
+                'imageStyle:block',
+                'imageStyle:wrapText',
+            ],
+        },
+
+        link: {
+            defaultProtocol: 'https://',
+            allowedProtocols: [ 'https?', 'tel', 'sms', 'mailto'],
+            decorators: {
+                openInNewTab: {
+                    mode: 'manual',
+                    label: 'Open in a new tab',
+                    attributes: {
+                        target: '_blank',
+                        rel: 'noopener noreferrer'
+                    }
+                }
             }
-        });
-    });
+        },
+
+        table: {
+            contentToolbar: [
+                'tableColumn', 'tableRow', 'mergeTableCells',
+                'tableProperties', 'tableCellProperties',
+            ],
+        },
+    };
+
+
+// console.log(editorElements);
+    // Loop through the target elements and activate the Editor on each.
+    for (const editorElement of editorElements) {
+
+        // Determine language to use for this element's content and the UI
+        const contentLangCode = langArray[editorElement.name.match(/\d+/) ?? sessionLangId];
+        const langConfig = {
+            language: {
+                // ui: sessionLangCode,
+                content: contentLangCode,
+            }
+        };
+
+        const translationsConfig = {
+            translations: [
+                uiLanguages[contentLangCode],
+                // uiLanguages[sessionLangCode],
+            ]
+        };
+
+        // In case the override/custom config.js doesn't load or is not present, fallback to empty object.
+        let customConfig = {};
+        if (typeof myCKEditorConfig !== 'undefined') {
+            customConfig = myCKEditorConfig;
+        }
+
+        // Combine configs: First use the master config above, then any custom overrides from /editors/ckeditor5/config.js, and then override language for specific fields
+        const currentEditorConfig = {...editorConfig, ...translationsConfig, ...customConfig, ...langConfig};
+//console.log(currentEditorConfig);
+
+        // Instantiate this editor instance, before looping to the next one on the page.
+        ClassicEditor
+            .create(editorElement, currentEditorConfig)
+            .then(editor => {
+                window.editor = editor;
+            })
+            .catch(error => {
+                // console.error(error.stack);
+                console.error(error);
+            });
+    }
 </script>
 
 <?php
-// Other options:
-// - Edit your /editors/ckeditor/config.js file to control the toolbar buttons, add additional plugins, control the UI color, etc
-//
-// Advanced:
-// https://ckeditor.com/docs/ckeditor4/latest/features/styles.html#the-stylesheet-parser-plugin
-// - set custom styles in the /editors/ckeditor/styles.js file
-// - import a template-specific stylesheet from catalog-side, using config.contentsCss setting, so dialogs "look like catalog-side" in admin editor
+// - Edit your /editors/ckeditor5/config.js file to control the toolbar buttons, add additional plugins, etc
--- UPLOAD/editors/ckeditor/config.js
+++ UPLOAD/editors/ckeditor/config.js
@@ -1,7 +1,7 @@
 /**
  * @license Copyright (c) 2003-2023, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
- * modified for Zen Cart German - 2024-07-01 webchills
+ * modified for Zen Cart German - 2026-04-03 webchills
  */
 
 CKEDITOR.editorConfig = function( config ) {
@@ -10,16 +10,9 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// By default, the editor width equals the width of its container element in the page, while its height is set to 200 pixels.
 	//config.width = '100%';
-	config.height = 200;
-	// if you prefer 400px set to 400 instead
+	config.height = 400;
+	// if you prefer 200px set to 200 instead
 	//config.height = 400;
-	CKEDITOR.plugins.addExternal('youtube', '/plugins/youtube/');
-
-   config.extraPlugins = 'youtube';
-   config.youtube_responsive = true;
-   config.youtube_related = false;
-   config.youtube_privacy = true;
-   config.youtube_autoplay = false;
 
 	config.uiColor = '#EEEEEE';
 
@@ -50,9 +43,8 @@ CKEDITOR.editorConfig = function( config ) {
 	config.fontSize_sizes =  '8/8px;9/9px;10/10px;11/11px;12/12px;13/13px;14/14px;15/15px;16/16px;17/17px;18/18px;19/19px;20/20px;22/22px;24/24px;26/26px;28/28px;36/36px;48/48px;72/72px';
 
 	
-   config.iframe_attributes = {	
-    sandbox: 'allow-scripts allow-same-origin'	
-  }
+	// Set the most common block elements.
+	config.format_tags = 'p;h1;h2;h3;pre';
 
   config.allowedContent = true;
 	config.extraAllowedContent = '*(*)';
@@ -60,7 +52,9 @@ CKEDITOR.editorConfig = function( config ) {
 	config.entities_latin = false;
 	config.entities = false;  
 	config.versionCheck = false;
+ 	config.extraPlugins = 'font,justify,colorbutton,iframe';
 
+	config.skin = 'moono'; // kama, moono, moono-lisa
 };
 CKEDITOR.on( 'instanceReady', function( ev ) {
     ev.editor.dataProcessor.writer.selfClosingEnd = '>';
