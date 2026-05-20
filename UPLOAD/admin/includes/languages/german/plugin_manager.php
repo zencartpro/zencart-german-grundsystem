a60e343c364c4b4fce1e89a32639256014632513
Jörg Schönberger <office@webchills.at>
2026-05-20 14:39:38 +0200

    Fix typos in plugin_manager language file

--- UPLOAD/admin/includes/languages/german/plugin_manager.php
+++ UPLOAD/admin/includes/languages/german/plugin_manager.php
@@ -1,18 +1,18 @@
 <?php
 /**
- * Zen Cart German Specific (158 code in 157)
- * @copyright Copyright 2003-2023 Zen Cart Development Team
+ * Zen Cart German Specific (210 code in 157)
+ * @copyright Copyright 2003-2026 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: plugin_manager.php 2023-10-28 16:54:16Z webchills $
+ * @version $Id: plugin_manager.php 2026-05-20 14:54:16Z webchills $
  */
 define('HEADING_TITLE', 'Plugin Manager');
 
 define('TABLE_HEADING_KEY', 'Plugin Key');
 define('TABLE_HEADING_NAME', 'Name');
 
-define('TABLE_HEADING_FILE_SPACE', 'File Space');
+define('TABLE_HEADING_FILE_SPACE', 'Dateigröße');
 define('TABLE_HEADING_VERSION_INSTALLED', 'Version');
 define('TEXT_INSTALL', 'Installieren');
 define('TEXT_UNINSTALL', 'Deinstallieren');
@@ -25,7 +25,7 @@ define('TEXT_INFO_DESCRIPTION', 'Plugin Beschreibung:');
 define('TEXT_INFO_CLEANUP', 'Unerwünschte Plugin-Versionsverzeichnisse entfernen');
 define('TEXT_INFO_SELECT_CLEAN', 'Wählen Sie die Versionen, die Sie entfernen wollen');
 define('TEXT_INSTALLED_ENABLED', 'Installiert und aktiviert');
-define('TEXT_INSTALLED_DISABLED', 'Installiert und deaktiviert)');
+define('TEXT_INSTALLED_DISABLED', 'Installiert und deaktiviert');
 define('TEXT_ALL_STATUSES' , 'Alle');
 define('TEXT_NOT_INSTALLED', 'nicht installiert');
 define('TEXT_INSTALL_SUCCESS', 'Plugin erfolgreich installiert');
@@ -34,7 +34,7 @@ define('TEXT_UPGRADE_SUCCESS' , 'Plugin erfolgreich aktualisiert');
 define('TEXT_DISABLE_SUCCESS', 'Plugin erfolgreich deaktiviert');
 define('TEXT_ENABLE_SUCCESS', 'Plugin erfolgreich aktiviert');
 define('TEXT_CLEANUP_SUCCESS' , 'Verzeichnisse erfolgreich entfernt');
-define('TEXT_CLEANUP_ERROR' , 'Bitte daterechte überprüfen (chmod). Einige Verzeichnisse wurden nicht entfernt');
+define('TEXT_CLEANUP_ERROR' , 'Bitte Dateirechte überprüfen (chmod). Einige Verzeichnisse wurden nicht entfernt');
 define('TEXT_UPGRADE_AVAILABLE', 'Update verfügbar');
 define('TEXT_VERSION_INSTALLED', 'Installierte Version: %s');
 define('TEXT_PLUGIN_AUTHOR' , '<strong>Autor:</strong> %s');
@@ -47,7 +47,7 @@ define('TEXT_CONFIRM_ENABLE', 'Wollen Sie dieses Plugin wirklich aktivieren?');
 define('TEXT_INFO_UPGRADE', 'Wählen Sie die Version, auf die Sie aktualisieren wollen.');
 define('TEXT_INFO_UPGRADE_CONFIRM', 'Update Version %s');
 define('TEXT_INFO_UPGRADE_WARNING', 'Warnung:');
-define('TEXT_INFO_CONFIRM_CLEAN', 'Bestätigen Sie die Verzeichnisse, die aufgeräunt/entfernt werden sollen');
+define('TEXT_INFO_CONFIRM_CLEAN', 'Bestätigen Sie die Verzeichnisse, die aufgeräumt/entfernt werden sollen');
 define('TEXT_LABEL_STATUS', 'Status: ');
 define('ERROR_NOT_FOUND_IN_SQL_FUNCTIONS_MAP' , 'SQL Abfrage prüfen. Eine sql function map wurde nicht gefunden für : ');
 define('ERROR_INVALID_SYNTAX', 'Die Tabelle konnte nicht identifiziert werden, da die Syntax nicht korrekt ist in: ');
