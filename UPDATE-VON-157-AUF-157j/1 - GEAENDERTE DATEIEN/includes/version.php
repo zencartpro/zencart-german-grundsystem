4f454361422c1d59494f3ea13546acdde9e9b92d
WebConcept One <info@webdesign-aus-braunschweig.de>
2026-05-30 13:20:53 +0200

    version

--- UPDATE-VON-157-AUF-157j/1 - GEAENDERTE DATEIEN/includes/version.php
+++ UPDATE-VON-157-AUF-157j/1 - GEAENDERTE DATEIEN/includes/version.php
@@ -16,7 +16,7 @@
 
 define('PROJECT_VERSION_NAME', 'Zen Cart');
 define('PROJECT_VERSION_MAJOR', '1');
-define('PROJECT_VERSION_MINOR', '5.7i');
+define('PROJECT_VERSION_MINOR', '5.7j');
 define('PROJECT_VERSION_PATCH1', '');
 define('PROJECT_VERSION_PATCH2', '');
 define('PROJECT_VERSION_PATCH1_SOURCE', '');
