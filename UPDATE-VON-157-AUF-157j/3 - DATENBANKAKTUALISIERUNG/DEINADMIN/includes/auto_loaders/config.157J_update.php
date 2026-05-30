bab3650ccd44324f5f4b7e9c6e33b8d891fb1b98
WebConcept One <info@webdesign-aus-braunschweig.de>
2026-05-30 12:00:59 +0200

    Zielversion falsch

--- UPDATE-VON-157-AUF-157j/3 - DATENBANKAKTUALISIERUNG/DEINADMIN/includes/auto_loaders/config.157J_update.php
+++ UPDATE-VON-157-AUF-157j/3 - DATENBANKAKTUALISIERUNG/DEINADMIN/includes/auto_loaders/config.157J_update.php
@@ -5,12 +5,12 @@
 * @copyright Portions Copyright 2003 osCommerce
 * Zen Cart German Version - www.zen-cart-pro.at
 * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
-* @version $Id: config.157i_update.php 2024-08-18 07:21:51Z webchills $
+* @version $Id: config.157J_update.php 2026-30-01 07:21:51Z webconcept.one $
 */
 if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 } 
 $autoLoadConfig[200][] = array(
     'autoType'  => 'init_script',
-    'loadFile'  => 'init_157i_update.php'
+    'loadFile'  => 'init_157j_update.php'
 );
